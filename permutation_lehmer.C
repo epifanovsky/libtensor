81d6ab7cf53de293d15bad76871cdd54d320a6e9
Evgeny Epifanovsky <epif@q-chem.com>
2009-01-24 00:44:31 +0000

    Initial implementation of permutation_lehmer

---
+++ permutation_lehmer.C
@@ -0,0 +1,11 @@
+#include "permutation_lehmer.h"
+
+namespace libtensor {
+
+permutation_lehmer::multable permutation_lehmer::m_multable(6);
+
+permutation_lehmer::multable::multable(const size_t maxorder) {
+}
+
+} // namespace libtensor
+
