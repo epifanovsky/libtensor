2af1d401f7e821984e023935f04e956f340bcda9
Evgeny Epifanovsky <epif@q-chem.com>
2009-02-17 22:52:35 +0000

    Event model in the test suite implemented

--- tests/libtensor_tests.C
+++ tests/libtensor_tests.C
@@ -4,12 +4,33 @@
 using namespace libtensor;
 using libtest::test_exception;
 
-int main(int argc, char **argv) {
-	libtensor_suite suite;
-	try {
-		suite.run_all_tests();
-	} catch(test_exception e) {
+class suite_handler : public libtest::suite_event_handler {
+public:
+	virtual void on_suite_start(const char *suite) {
+	}
+
+	virtual void on_suite_end(const char *suite) {
+	}
+
+	virtual void on_test_start(const char *test) {
+		printf("Test %s ... ", test);
+	}
+
+	virtual void on_test_end_success(const char *test) {
+		printf("done.\n");
+	}
+
+	virtual void on_test_end_exception(const char *test,
+		const test_exception &e) {
+		printf("FAIL!\n");
 		printf("%s\n", e.what());
 	}
+};
+
+int main(int argc, char **argv) {
+	suite_handler handler;
+	libtensor_suite suite;
+	suite.set_handler(&handler);
+	suite.run_all_tests();
 }
 
