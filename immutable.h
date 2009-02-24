0edab05734d84dcf00e202c23dd656905a7f0e58
Evgeny Epifanovsky <epif@q-chem.com>
2009-02-24 19:40:53 +0000

    docs for immutable

--- immutable.h
+++ immutable.h
@@ -6,7 +6,7 @@
 
 namespace libtensor {
 
-/**	\brief Enables %immutabilit
+/**	\brief Enables %immutability
 
 	\ingroup libtensor
 **/
@@ -27,16 +27,16 @@ public:
 	//!	\name Immutability
 	//@{
 
-	/**	\brief Checks if the object is immutable
+	/**	\brief Checks if the object is %immutable
 
-		Returns true if the object is immutable, false otherwise.
+		Returns true if the object is %immutable, false otherwise.
 	**/
 	bool is_immutable() const;
 
-	/**	\brief Sets the object status as immutable.
+	/**	\brief Sets the object status as %immutable.
 
-		Sets the object status as immutable. If the object has already
-		been set immutable, it stays immutable.
+		Sets the object status as %immutable. If the object has already
+		been set %immutable, it stays %immutable.
 	**/
 	void set_immutable();
 
