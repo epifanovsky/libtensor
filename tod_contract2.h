46675674f0634a83a3d8cacaaa34fbc695b5a1e4
Evgeny Epifanovsky <epif@q-chem.com>
2009-03-26 03:04:44 +0000

    tod_contract2: modified template parameters

--- tod_contract2.h
+++ tod_contract2.h
@@ -9,6 +9,11 @@ namespace libtensor {
 
 /**	\brief Contracts two tensors (double)
 
+	\param N Order of the first %tensor (a) less the contraction degree
+	\param M Order of the second %tensor (b) less the contraction degree
+	\param K Contraction degree (the number of indexes over which the
+		tensors are contracted)
+
 	This operation contracts %tensor T1 permuted as P1 with %tensor T2
 	permuted as P2 over n last indexes. The result is permuted as Pres
 	and written or added to the resulting %tensor.
@@ -21,25 +26,30 @@ namespace libtensor {
 
 	\ingroup libtensor_tod
 **/
-template<size_t NC, size_t NA, size_t NB>
-class tod_contract2 : public tod_additive<NC> {
+template<size_t N, size_t M, size_t K>
+class tod_contract2 : public tod_additive<N+M> {
 private:
-	size_t m_ncontr; //!< Number of indexes to contract over
-	tensor_i<NA,double> &m_t1; //!< First tensor
-	tensor_i<NB,double> &m_t2; //!< Second tensor
-	permutation<NA> m_p1; //!< Permutation of the first tensor
-	permutation<NB> m_p2; //!< Permutation of the second tensor
+	tensor_i<N+K,double> &m_ta; //!< First tensor (a)
+	tensor_i<M+K,double> &m_tb; //!< Second tensor (b)
+	permutation<N+K> m_pa; //!< Permutation of the first %tensor (a)
+	permutation<M+K> m_pb; //!< Permutation of the second %tensor (b)
+	permutation<N+M> m_pc; //!< Permutation of the result (c)
 
 public:
 	//!	\name Construction and destruction
 	//@{
 
 	/**	\brief Initializes the operation
+
+		\param ta Tensor a
+		\param pa Permutation of %tensor a
+		\param tb Tensor b
+		\param pb Permutation of %tensor b
+		\param pc Permutation of the resulting %tensor c
 	**/
-	tod_contract2(const size_t n, tensor_i<NA,double> &t1,
-		const permutation<NA> &p1, tensor_i<NB,double> &t2,
-		const permutation<NB> &p2, const permutation<NC> &pres)
-		throw(exception);
+	tod_contract2(tensor_i<N+K,double> &ta, const permutation<N+K> &pa,
+		tensor_i<M+K,double> &tb, const permutation<M+K> &pb,
+		const permutation<N+M> &pc) throw(exception);
 
 	/**	\brief Virtual destructor
 	**/
@@ -54,41 +64,48 @@ public:
 
 	//!	\name Implementation of tod_additive
 	//@{
-	virtual void perform(tensor_i<NC,double> &t) throw(exception);
-	virtual void perform(tensor_i<NC,double> &t, double c) throw(exception);
+	virtual void perform(tensor_i<N+M,double> &t) throw(exception);
+	virtual void perform(tensor_i<N+M,double> &t, double c)
+		throw(exception);
 	//@}
 };
 
-template<size_t NC, size_t NA, size_t NB>
-tod_contract2<NC,NA,NB>::tod_contract2(const size_t n, tensor_i<NA,double> &t1,
-	const permutation<NA> &p1, tensor_i<NB,double> &t2,
-	const permutation<NB> &p2, const permutation<NC> &pres)
-	throw(exception) : m_ncontr(n), m_t1(t1), m_t2(t2), m_p1(p1), m_p2(p2) {
+template<size_t N, size_t M, size_t K>
+tod_contract2<N,M,K>::tod_contract2(
+	tensor_i<N+K,double> &ta, const permutation<N+K> &pa,
+	tensor_i<M+K,double> &tb, const permutation<M+K> &pb,
+	const permutation<N+M> &pc) throw(exception) :
+		m_ta(ta), m_pa(pa), m_tb(tb), m_pb(pb), m_pc(pc) {
 }
 
-template<size_t NC, size_t NA, size_t NB>
-tod_contract2<NC,NA,NB>::~tod_contract2() {
+template<size_t N, size_t M, size_t K>
+tod_contract2<N,M,K>::~tod_contract2() {
 }
 
-template<size_t NC, size_t NA, size_t NB>
-void tod_contract2<NC,NA,NB>::prefetch() throw(exception) {
-	tensor_ctrl<NA,double> ctrl_t1(m_t1);
-	tensor_ctrl<NB,double> ctrl_t2(m_t2);
-	ctrl_t1.req_prefetch();
-	ctrl_t2.req_prefetch();
+template<size_t N, size_t M, size_t K>
+void tod_contract2<N,M,K>::prefetch() throw(exception) {
+	tensor_ctrl<N+K,double> ctrl_ta(m_ta);
+	tensor_ctrl<M+K,double> ctrl_tb(m_tb);
+	ctrl_ta.req_prefetch();
+	ctrl_tb.req_prefetch();
 }
 
-template<size_t NC, size_t NA, size_t NB>
-void tod_contract2<NC,NA,NB>::perform(tensor_i<NC,double> &t) throw(exception) {
-	dimensions<NA> dims_t1(m_t1.get_dims());
-	dimensions<NB> dims_t2(m_t2.get_dims());
-	dims_t1.permute(m_p1);
-	dims_t2.permute(m_p2);
+template<size_t N, size_t M, size_t K>
+void tod_contract2<N,M,K>::perform(tensor_i<N+M,double> &t) throw(exception) {
+	dimensions<N+K> dims_ta(m_ta.get_dims());
+	dimensions<M+K> dims_tb(m_tb.get_dims());
+	dims_ta.permute(m_pa);
+	dims_tb.permute(m_pb);
 }
 
-template<size_t NC, size_t NA, size_t NB>
-void tod_contract2<NC,NA,NB>::perform(tensor_i<NC,double> &t, const double c)
+template<size_t N, size_t M, size_t K>
+void tod_contract2<N,M,K>::perform(tensor_i<N+M,double> &t, const double c)
 	throw(exception) {
+	char cls[32], meth[128];
+	snprintf(cls, 32, "tod_contract2<%lu,%lu,%lu>", N, M, K);
+	snprintf(meth, 128, "perform(tensor_i<%lu,double>&, const double)",
+		N+M);
+	throw_exc(cls, meth, "Contraction not implemented");
 }
 
 } // namespace libtensor
