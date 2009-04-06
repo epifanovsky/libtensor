5d0b72110fa1a0d71f5945a76da8b19adc893e01
Evgeny Epifanovsky <epif@q-chem.com>
2009-04-06 17:17:38 +0000

    added letter index expressions to libtensor.h

--- libtensor.h
+++ libtensor.h
@@ -1,3 +1,6 @@
+#ifndef LIBTENSOR_H
+#define LIBTENSOR_H
+
 #include "defs.h"
 #include "exception.h"
 
@@ -10,6 +13,9 @@
 #include "index_space.h"
 #include "index_space_product.h"
 
+#include "letter.h"
+#include "letter_expr.h"
+
 #include "scalar.h"
 
 #include "tensor.h"
@@ -21,4 +27,6 @@
 #include "block_tensor_ctrl.h"
 #include "direct_block_tensor.h"
 #include "direct_block_tensor_operation.h"
+
+#endif // LIBTENSOR_H
 
