9db93be3980f0413c6947c7c32b7fd8ffb267b48
Evgeny Epifanovsky <epif@q-chem.com>
2009-05-14 04:23:45 +0000

    one more tod_contract2 test

--- tests/tod_contract2_test.C
+++ tests/tod_contract2_test.C
@@ -12,10 +12,18 @@ typedef libvmm::std_allocator<double> allocator;
 void tod_contract2_test::perform() throw(libtest::test_exception) {
 	srand48(time(NULL));
 
+	test_ij_ipqr_jpqr(3, 4, 5, 6, 7);
+
+	test_ij_pqir_pqjr(3, 4, 5, 6, 7);
+	test_ij_pqir_pqjr(3, 3, 3, 3, 3);
+	test_ij_pqir_pqjr(3, 1, 3, 1, 2);
+	test_ij_pqir_pqjr(3, 3, 1, 1, 2);
+
 	//test_ijkl_ijp_klp(1, 1, 1, 1, 1); // This one fails
 	test_ijkl_ijp_klp(3, 4, 5, 6, 7);
 	test_ijkl_ijp_klp(5, 6, 3, 4, 7);
 	test_ijkl_ijp_klp(1, 100, 1, 100, 100);
+
 }
 
 void tod_contract2_test::test_ij_ipqr_jpqr(size_t ni, size_t nj, size_t np,
@@ -88,6 +96,80 @@ void tod_contract2_test::test_ij_ipqr_jpqr(size_t ni, size_t nj, size_t np,
 	char testname[128];
 	snprintf(testname, 128,
 		"tod_contract2_test::test_ij_ipqr_jpqr"
+		"(%lu, %lu, %lu, %lu, %lu)", ni, nj, np, nq, nr);
+	compare_ref(testname, tc, tc_ref, cij_max*1e-15);
+}
+
+void tod_contract2_test::test_ij_pqir_pqjr(size_t ni, size_t nj,
+	size_t np, size_t nq, size_t nr) throw(libtest::test_exception) {
+
+	// c_{ij} = \sum_{pqr} a_{pqir} b_{pqjr}
+
+	index<4> ia1, ia2; ia2[0]=np-1; ia2[1]=nq-1; ia2[2]=ni-1; ia2[3]=nr-1;
+	index<4> ib1, ib2; ib2[0]=np-1; ib2[1]=nq-1; ib2[2]=nj-1; ib2[3]=nr-1;
+	index<2> ic1, ic2; ic2[0]=ni-1; ic2[1]=nj-1;
+	index_range<4> ira(ia1,ia2); dimensions<4> dima(ira);
+	index_range<4> irb(ib1,ib2); dimensions<4> dimb(irb);
+	index_range<2> irc(ic1,ic2); dimensions<2> dimc(irc);
+	size_t sza = dima.get_size(), szb = dimb.get_size(),
+		szc = dimc.get_size();
+
+	tensor<4, double, allocator> ta(dima); tensor_ctrl<4, double> tca(ta);
+	tensor<4, double, allocator> tb(dimb); tensor_ctrl<4, double> tcb(tb);
+	tensor<2, double, allocator> tc(dimc); tensor_ctrl<2, double> tcc(tc);
+	tensor<2, double, allocator> tc_ref(dimc);
+	tensor_ctrl<2, double> tcc_ref(tc);
+	double *dta = tca.req_dataptr();
+	double *dtb = tcb.req_dataptr();
+	double *dtc = tcc_ref.req_dataptr();
+
+	// Fill in random input
+
+	for(size_t i=0; i<sza; i++) dta[i]=drand48();
+	for(size_t i=0; i<szb; i++) dtb[i]=drand48();
+
+	// Generate reference data
+
+	index<4> ia, ib; index<2> ic;
+	double cij_max = 0.0;
+	for(size_t i=0; i<ni; i++) {
+	for(size_t j=0; j<nj; j++) {
+		ic[0]=i; ic[1]=j;
+		double cij = 0.0;
+		for(size_t p=0; p<np; p++) {
+		for(size_t q=0; q<nq; q++) {
+		for(size_t r=0; r<nr; r++) {
+			ia[0]=p; ia[1]=q; ia[2]=i; ia[3]=r;
+			ib[0]=p; ib[1]=q; ib[2]=j; ib[3]=r;
+			cij += dta[dima.abs_index(ia)]*dtb[dimb.abs_index(ib)];
+		}
+		}
+		}
+		dtc[dimc.abs_index(ic)] = cij;
+		if(fabs(cij) > cij_max) cij_max = fabs(cij);
+	}
+	}
+
+	tca.ret_dataptr(dta); dta = NULL; ta.set_immutable();
+	tcb.ret_dataptr(dtb); dtb = NULL; tb.set_immutable();
+	tcc_ref.ret_dataptr(dtc); dtc = NULL; tc_ref.set_immutable();
+
+	// Invoke the contraction routine
+
+	permutation<2> permc;
+	contraction2<1, 1, 3> contr(permc);
+	contr.contract(0, 0);
+	contr.contract(1, 1);
+	contr.contract(3, 3);
+
+	tod_contract2<1, 1, 3> op(contr, ta, tb);
+	op.perform(tc);
+
+	// Compare against the reference
+
+	char testname[128];
+	snprintf(testname, 128,
+		"tod_contract2_test::test_ij_pqir_pqjr"
 		"(%lu, %lu, %lu, %lu, %lu)", ni, nj, np, nq, nr);
 	compare_ref(testname, tc, tc_ref, cij_max*1e-15);
 }
