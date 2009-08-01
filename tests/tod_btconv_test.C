24369bf0b48061624622ae9dd007746789454225
Evgeny Epifanovsky <epif@q-chem.com>
2009-08-01 14:28:10 +0000

    tod_btconv implementation + tests

--- tests/tod_btconv_test.C
+++ tests/tod_btconv_test.C
@@ -1,9 +1,239 @@
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <sstream>
 #include <libtensor.h>
+#include <libvmm.h>
 #include "tod_btconv_test.h"
 
 namespace libtensor {
 
 void tod_btconv_test::perform() throw(libtest::test_exception) {
+
+	srand48(time(NULL));
+	test_1();
+	test_2();
+}
+
+void tod_btconv_test::test_1() throw(libtest::test_exception) {
+
+	static const char *testname = "tod_btconv_test::test_1()";
+
+	typedef libvmm::std_allocator<double> allocator_t;
+	typedef tensor<2, double, allocator_t> tensor_t;
+	typedef tensor_ctrl<2, double> tensor_ctrl_t;
+	typedef block_tensor<2, double, default_symmetry<2, double>,
+		allocator_t> block_tensor_t;
+	typedef block_tensor_ctrl<2, double> block_tensor_ctrl_t;
+
+	try {
+
+	index<2> i1, i2;
+	i2[0] = 10; i2[1] = 10;
+	dimensions<2> dims(index_range<2>(i1, i2));
+	block_index_space<2> bis(dims);
+	bis.split(0, 5);
+	bis.split(1, 5);
+
+	block_tensor_t bt(bis);
+	block_tensor_ctrl_t btctrl(bt);
+
+	tensor_t t(dims), t_ref(dims);
+	tensor_ctrl_t tctrl(t), tctrl_ref(t_ref);
+
+	double *pt = tctrl.req_dataptr();
+	double *pt_ref = tctrl_ref.req_dataptr();
+
+	//	Fill in random input, generate reference
+
+	size_t sz = dims.get_size();
+	for(size_t i = 0; i < sz; i++) {
+		pt[i] = drand48();
+		pt_ref[i] = 0.0;
+	}
+
+	tctrl.ret_dataptr(pt); pt = NULL;
+	tctrl_ref.ret_dataptr(pt_ref); pt_ref = NULL;
+
+	t_ref.set_immutable();
+	bt.set_immutable();
+
+	//	Invoke the operation
+
+	tod_btconv<2> op(bt);
+	op.perform(t);
+
+	//	Compare the result against the reference
+
+	compare_ref(testname, t, t_ref, 0.0);
+
+	} catch(exception &exc) {
+		fail_test(testname, __FILE__, __LINE__, exc.what());
+	}
+}
+
+void tod_btconv_test::test_2() throw(libtest::test_exception) {
+
+	static const char *testname = "tod_btconv_test::test_2()";
+
+	typedef libvmm::std_allocator<double> allocator_t;
+	typedef tensor<2, double, allocator_t> tensor_t;
+	typedef tensor_ctrl<2, double> tensor_ctrl_t;
+	typedef block_tensor<2, double, default_symmetry<2, double>,
+		allocator_t> block_tensor_t;
+	typedef block_tensor_ctrl<2, double> block_tensor_ctrl_t;
+
+	try {
+
+	index<2> i1, i2;
+	i2[0] = 10; i2[1] = 10;
+	dimensions<2> dims(index_range<2>(i1, i2));
+	block_index_space<2> bis(dims);
+	bis.split(0, 5);
+	bis.split(1, 5);
+
+	block_tensor_t bt(bis);
+	block_tensor_ctrl_t btctrl(bt);
+
+	tensor_t t(dims), t_ref(dims);
+	tensor_ctrl_t tctrl(t), tctrl_ref(t_ref);
+
+	double *pt = tctrl.req_dataptr();
+	double *pt_ref = tctrl_ref.req_dataptr();
+
+	//	Fill in random input, generate reference
+
+	size_t sz = dims.get_size();
+	for(size_t i = 0; i < sz; i++) {
+		pt[i] = drand48();
+		pt_ref[i] = 0.0;
+	}
+
+	index<2> i_00, ii;
+	index<2> istart = bis.get_block_start(i_00);
+	dimensions<2> dims_00 = bis.get_block_dims(i_00);
+	tensor_i<2, double> &blk_00 = btctrl.req_block(i_00);
+	tensor_ctrl_t tctrl_00(blk_00);
+	double *p_00 = tctrl_00.req_dataptr();
+	do {
+		index<2> iii(istart);
+		for(size_t j = 0; j < 2; j++) iii[j] += ii[j];
+		pt_ref[dims.abs_index(iii)] = p_00[dims_00.abs_index(ii)] =
+			drand48();
+	} while(dims_00.inc_index(ii));
+	tctrl_00.ret_dataptr(p_00);
+	btctrl.ret_block(i_00);
+
+	tctrl.ret_dataptr(pt); pt = NULL;
+	tctrl_ref.ret_dataptr(pt_ref); pt_ref = NULL;
+
+	t_ref.set_immutable();
+	bt.set_immutable();
+
+	//	Invoke the operation
+
+	tod_btconv<2> op(bt);
+	op.perform(t);
+
+	//	Compare the result against the reference
+
+	compare_ref(testname, t, t_ref, 0.0);
+
+	} catch(exception &exc) {
+		fail_test(testname, __FILE__, __LINE__, exc.what());
+	}
+}
+
+void tod_btconv_test::test_3() throw(libtest::test_exception) {
+
+	static const char *testname = "tod_btconv_test::test_3()";
+
+	typedef libvmm::std_allocator<double> allocator_t;
+	typedef tensor<2, double, allocator_t> tensor_t;
+	typedef tensor_ctrl<2, double> tensor_ctrl_t;
+	typedef block_tensor<2, double, default_symmetry<2, double>,
+		allocator_t> block_tensor_t;
+	typedef block_tensor_ctrl<2, double> block_tensor_ctrl_t;
+
+	try {
+
+	index<2> i1, i2;
+	i2[0] = 10; i2[1] = 10;
+	dimensions<2> dims(index_range<2>(i1, i2));
+	block_index_space<2> bis(dims);
+	bis.split(0, 5);
+	bis.split(1, 5);
+
+	block_tensor_t bt(bis);
+	block_tensor_ctrl_t btctrl(bt);
+
+	tensor_t t(dims), t_ref(dims);
+	tensor_ctrl_t tctrl(t), tctrl_ref(t_ref);
+
+	double *pt = tctrl.req_dataptr();
+	double *pt_ref = tctrl_ref.req_dataptr();
+
+	//	Fill in random input, generate reference
+
+	size_t sz = dims.get_size();
+	for(size_t i = 0; i < sz; i++) {
+		pt[i] = drand48();
+		pt_ref[i] = 0.0;
+	}
+
+	index<2> i_11, ii;
+	i_11[0] = 1; i_11[1] = 1;
+	index<2> istart = bis.get_block_start(i_11);
+	dimensions<2> dims_11 = bis.get_block_dims(i_11);
+	tensor_i<2, double> &blk_11 = btctrl.req_block(i_11);
+	tensor_ctrl_t tctrl_11(blk_11);
+	double *p_11 = tctrl_11.req_dataptr();
+	do {
+		index<2> iii(istart);
+		for(size_t j = 0; j < 2; j++) iii[j] += ii[j];
+		pt_ref[dims.abs_index(iii)] = p_11[dims_11.abs_index(ii)] =
+			drand48();
+	} while(dims_11.inc_index(ii));
+	tctrl_11.ret_dataptr(p_11);
+	btctrl.ret_block(i_11);
+
+	tctrl.ret_dataptr(pt); pt = NULL;
+	tctrl_ref.ret_dataptr(pt_ref); pt_ref = NULL;
+
+	t_ref.set_immutable();
+	bt.set_immutable();
+
+	//	Invoke the operation
+
+	tod_btconv<2> op(bt);
+	op.perform(t);
+
+	//	Compare the result against the reference
+
+	compare_ref(testname, t, t_ref, 0.0);
+
+	} catch(exception &exc) {
+		fail_test(testname, __FILE__, __LINE__, exc.what());
+	}
+}
+
+template<size_t N>
+void tod_btconv_test::compare_ref(const char *test,
+	tensor_i<N, double> &t, tensor_i<N, double> &t_ref, double thresh)
+	throw(libtest::test_exception) {
+
+	tod_compare<N> cmp(t, t_ref, thresh);
+	if(!cmp.compare()) {
+		std::ostringstream ss1, ss2;
+		ss2 << "Result does not match reference at element "
+			<< cmp.get_diff_index() << ": "
+			<< cmp.get_diff_elem_1() << " (act) vs. "
+			<< cmp.get_diff_elem_2() << " (ref), "
+			<< cmp.get_diff_elem_1() - cmp.get_diff_elem_2()
+			<< " (diff) in " << test;
+		fail_test("tod_btconv_test::compare_ref()",
+			__FILE__, __LINE__, ss2.str().c_str());
+	}
 
 }
 
--- tod/tod_btconv.h
+++ tod/tod_btconv.h
@@ -1,6 +1,7 @@
 #ifndef LIBTENSOR_TOD_BTCONV_H
 #define LIBTENSOR_TOD_BTCONV_H
 
+#include <list>
 #include "defs.h"
 #include "exception.h"
 #include "core/block_tensor_i.h"
@@ -8,6 +9,7 @@
 #include "core/tensor_i.h"
 #include "core/tensor_ctrl.h"
 #include "btod/block_symop_double.h"
+#include "tod/processor.h"
 
 namespace libtensor {
 
@@ -19,7 +21,55 @@ namespace libtensor {
 template<size_t N>
 class tod_btconv {
 private:
-	block_tensor_i<N, double> m_bt; //!< Source block %tensor
+	static const char *k_clazz; //!< Class name
+
+private:
+	struct registers {
+		const double *m_ptra;
+		double *m_ptrb;
+	};
+
+	struct loop_list_node;
+	typedef std::list<loop_list_node> loop_list_t;
+	typedef processor<loop_list_t, registers> processor_t;
+	typedef processor_op_i<loop_list_t, registers> processor_op_i_t;
+
+	struct loop_list_node {
+	public:
+		size_t m_weight;
+		size_t m_inca, m_incb;
+		processor_op_i_t *m_op;
+		loop_list_node()
+			: m_weight(0), m_inca(0), m_incb(0), m_op(NULL) { }
+		loop_list_node(size_t weight, size_t inca, size_t incb)
+			: m_weight(weight), m_inca(inca), m_incb(incb),
+			m_op(NULL) { }
+		processor_op_i_t *op() const { return m_op; }
+	};
+
+	class op_loop : public processor_op_i_t {
+	private:
+		size_t m_len, m_inca, m_incb;
+	public:
+		op_loop(size_t len, size_t inca, size_t incb)
+			: m_len(len), m_inca(inca), m_incb(incb) { }
+		virtual void exec(processor_t &proc, registers &regs)
+			throw(exception);
+	};
+
+	class op_dcopy : public processor_op_i_t {
+	private:
+		size_t m_len, m_inca, m_incb;
+		double m_c;
+	public:
+		op_dcopy(size_t len, size_t inca, size_t incb, double c)
+			: m_len(len), m_inca(inca), m_incb(incb), m_c(c) { }
+		virtual void exec(processor_t &proc, registers &regs)
+			throw(exception);
+	};
+
+private:
+	block_tensor_i<N, double> &m_bt; //!< Source block %tensor
 
 public:
 	//!	\name Construction and destruction
@@ -36,7 +86,17 @@ public:
 	void perform(tensor_i<N, double> &t) throw(exception);
 
 	//@}
+
+private:
+	void copy_block(double *optr, const dimensions<N> &odims,
+		const index<N> &ooffs, const double *iptr,
+		const dimensions<N> &idims, const permutation<N> &iperm,
+		double icoeff) throw(exception);
 };
+
+
+template<size_t N>
+const char *tod_btconv<N>::k_clazz = "tod_btconv<N>";
 
 
 template<size_t N>
@@ -52,13 +112,106 @@ tod_btconv<N>::~tod_btconv() {
 template<size_t N>
 void tod_btconv<N>::perform(tensor_i<N, double> &t) throw(exception) {
 
+	static const char *method = "perform(tensor_i<N, double>&)";
+
 	const block_index_space<N> &bis = m_bt.get_bis();
 	if(!bis.get_dims().equals(t.get_dims())) {
-		throw_exc("tod_btconv<N>", "perform()",
-			"Incompatible dimensions");
+		throw bad_parameter("libtensor", k_clazz, method,
+			__FILE__, __LINE__,
+			"Incorrect dimensions of the output tensor.");
 	}
 
 	block_tensor_ctrl<N, double> btctrl(m_bt);
+	tensor_ctrl<N, double> tctrl(t);
+
+	double *ptr_out = tctrl.req_dataptr();
+	size_t sz = t.get_dims().get_size();
+	for(register size_t i = 0; i < sz; i++) ptr_out[i] = 0.0;
+
+	orbit_iterator<N, double> oi = btctrl.req_orbits();
+	while(!oi.end()) {
+		tensor_i<N, double> &blk = btctrl.req_block(oi.get_index());
+		tensor_ctrl<N, double> blk_ctrl(blk);
+		const double *ptr_in = blk_ctrl.req_const_dataptr();
+
+		block_iterator<N, double> bi = oi.get_blocks();
+		while(!bi.end()) {
+			const block_symop<N, double> &symop = bi.get_symop();
+			index<N> offset_out =
+				bis.get_block_start(bi.get_index());
+			copy_block(ptr_out, t.get_dims(), offset_out,
+				ptr_in, blk.get_dims(), symop.m_perm,
+				symop.m_coeff);
+			bi.next();
+		}
+
+		blk_ctrl.ret_dataptr(ptr_in);
+		btctrl.ret_block(oi.get_index());
+		oi.next();
+	}
+
+	tctrl.ret_dataptr(ptr_out);
+}
+
+template<size_t N>
+void tod_btconv<N>::copy_block(double *optr, const dimensions<N> &odims,
+	const index<N> &ooffs, const double *iptr, const dimensions<N> &idims,
+	const permutation<N> &iperm, double icoeff) throw(exception) {
+
+	size_t ib[N];
+	for(size_t i = 0; i < N; i++) ib[i] = i;
+	iperm.apply(N, ib);
+
+	loop_list_t lst;
+	for(size_t i = 0; i < N; i++) {
+		size_t inca = idims.get_increment(i);
+		size_t incb = odims.get_increment(ib[i]);
+		loop_list_node node(idims[i], inca, incb);
+		if(i < N-1) {
+			node.m_op = new op_loop(idims[i], inca, incb);
+		} else {
+			node.m_op = new op_dcopy(idims[i], inca, incb, icoeff);
+		}
+		lst.push_back(node);
+	}
+
+	registers regs;
+	regs.m_ptra = iptr;
+	regs.m_ptrb = optr + odims.abs_index(ooffs);
+	processor_t proc(lst, regs);
+	proc.process_next();
+
+	for(typename loop_list_t::iterator i = lst.begin();
+		i != lst.end(); i++) {
+
+		delete i->m_op;
+		i->m_op = NULL;
+	}
+}
+
+template<size_t N>
+void tod_btconv<N>::op_loop::exec(processor_t &proc, registers &regs)
+	throw(exception) {
+
+	const double *ptra = regs.m_ptra;
+	double *ptrb = regs.m_ptrb;
+
+	for(size_t i=0; i<m_len; i++) {
+		regs.m_ptra = ptra;
+		regs.m_ptrb = ptrb;
+		proc.process_next();
+		ptra += m_inca;
+		ptrb += m_incb;
+	}
+}
+
+template<size_t N>
+void tod_btconv<N>::op_dcopy::exec(processor_t &proc, registers &regs)
+	throw(exception) {
+	cblas_dcopy(m_len, regs.m_ptra, m_inca, regs.m_ptrb, m_incb);
+	if(m_c != 1.0) {
+		cblas_dscal(m_len, m_c, regs.m_ptrb, m_incb);
+	}
 }
 
 } // namespace libtensor
