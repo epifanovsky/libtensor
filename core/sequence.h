02ef9d226db3f2b5d4527e65701fff4e1071a142
Evgeny Epifanovsky <epif@q-chem.com>
2009-12-06 01:01:51 +0000

    merged r382, r383 from branches/2.0

--- core/sequence.h
+++ core/sequence.h
@@ -47,7 +47,7 @@ public:
 	T at(size_t pos) const throw(out_of_bounds);
 
 	/**	\brief Permutes the sequence
-		\param p Permutation.
+		\param perm Permutation.
 	 **/
 	void permute(const permutation<N> &perm);
 
@@ -92,8 +92,84 @@ protected:
 };
 
 
+/**	\brief Sequence of objects (specialized for zero length)
+	\tparam T Object type.
+
+	This zero-length sequence is a stub that contains no objects. However,
+	it may be sometimes required to compile some templates that happen to
+	have zero order.
+
+	\sa sequence<N>
+
+	\ingroup libtensor_core
+ **/
+template<typename T>
+class sequence<0, T> {
+public:
+	static const char *k_clazz; //!< Class name
+
+public:
+	//!	\name Construction and destruction
+	//@{
+
+	sequence(const T t);
+	sequence(const sequence<0, T> &seq);
+
+	//@}
+
+	//!	\name Access to %sequence elements, manipulations
+	//@{
+
+	/**	\brief Always throws an exception
+	 **/
+	T &at(size_t pos) throw(out_of_bounds);
+
+	/**	\brief Always throws an exception
+	 **/
+	T at(size_t pos) const throw(out_of_bounds);
+
+	/**	\brief Does nothing
+	 **/
+	void permute(const permutation<0> &perm);
+
+	//@}
+
+	//!	\name Overloaded operators
+	//@{
+
+	/**	\brief Always throws an exception
+	 **/
+	T &operator[](size_t pos) throw(out_of_bounds);
+
+	/**	\brief Always throws an exception
+	 **/
+	T operator[](size_t pos) const throw(out_of_bounds);
+
+	//@}
+
+protected:
+	//!	\name Protected access to %sequence elements
+	//@{
+
+	/**	\brief Always throws an exception
+	 **/
+	T &at_nochk(size_t pos);
+
+	/**	\brief Always throws an exception
+	 **/
+	T at_nochk(size_t pos) const;
+
+	//@}
+
+};
+
+
 template<size_t N, typename T>
 const char *sequence<N, T>::k_clazz = "sequence<N, T>";
+
+
+template<typename T>
+const char *sequence<0, T>::k_clazz = "sequence<0, T>";
 
 
 template<size_t N, typename T>
@@ -168,6 +244,70 @@ template<size_t N, typename T>
 inline T sequence<N, T>::at_nochk(size_t pos) const {
 
 	return m_seq[pos];
+}
+
+
+template<typename T>
+inline sequence<0, T>::sequence(const T t) {
+
+}
+
+
+template<typename T>
+inline sequence<0, T>::sequence(const sequence<0, T> &seq) {
+
+}
+
+
+template<typename T>
+inline T &sequence<0, T>::at(size_t pos) throw(out_of_bounds) {
+
+	throw out_of_bounds("libtensor", k_clazz, "at(size_t)",
+		__FILE__, __LINE__, "pos");
+}
+
+
+template<typename T>
+inline T sequence<0, T>::at(size_t pos) const throw(out_of_bounds) {
+
+	throw out_of_bounds("libtensor", k_clazz, "at(size_t) const",
+		__FILE__, __LINE__, "pos");
+}
+
+
+template<typename T>
+inline void sequence<0, T>::permute(const permutation<0> &perm) {
+
+}
+
+
+template<typename T>
+inline T &sequence<0, T>::operator[](size_t pos) throw(out_of_bounds) {
+
+	return at(pos);
+}
+
+
+template<typename T>
+inline T sequence<0, T>::operator[](size_t pos) const throw(out_of_bounds) {
+
+	return at(pos);
+}
+
+
+template<typename T>
+inline T &sequence<0, T>::at_nochk(size_t pos) {
+
+	throw out_of_bounds("libtensor", k_clazz, "at_nochk(size_t) const",
+		__FILE__, __LINE__, "pos");
+}
+
+
+template<typename T>
+inline T sequence<0, T>::at_nochk(size_t pos) const {
+
+	throw out_of_bounds("libtensor", k_clazz, "at_nochk(size_t) const",
+		__FILE__, __LINE__, "pos");
 }
 
 
