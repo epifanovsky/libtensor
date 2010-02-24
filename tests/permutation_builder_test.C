7147b7e2cd8d0f46c44a4432136d5e2d7a9ee59b
Evgeny Epifanovsky <epif@q-chem.com>
2010-02-24 05:20:16 +0000

    improved permutation_builder

--- tests/permutation_builder_test.C
+++ tests/permutation_builder_test.C
@@ -3,7 +3,20 @@
 
 namespace libtensor {
 
+
 void permutation_builder_test::perform() throw(libtest::test_exception) {
+
+	test_1();
+	test_2();
+	test_3();
+}
+
+
+void permutation_builder_test::test_1() throw(libtest::test_exception) {
+
+	static const char *testname = "permutation_builder_test::test_1()";
+
+	try {
 
 	char seq_ab[] = { 'a', 'b' };
 	char seq_ba[] = { 'b', 'a' };
@@ -11,16 +24,26 @@ void permutation_builder_test::perform() throw(libtest::test_exception) {
 	permutation_builder<2> pb1(seq_ab, seq_ab);
 	permutation<2> p1;
 	if(!pb1.get_perm().equals(p1)) {
-		fail_test("permutation_builder_test::perform()", __FILE__,
-			__LINE__, "Test (ab, ab) failed");
+		fail_test(testname, __FILE__, __LINE__, "Test (ab, ab) failed");
 	}
 
 	permutation_builder<2> pb2(seq_ab, seq_ba);
 	permutation<2> p2; p2.permute(0, 1);
 	if(!pb2.get_perm().equals(p2)) {
-		fail_test("permutation_builder_test::perform()", __FILE__,
-			__LINE__, "Test (ab, ba) failed");
+		fail_test(testname, __FILE__, __LINE__, "Test (ab, ba) failed");
 	}
+
+	} catch(exception &e) {
+		fail_test(testname, __FILE__, __LINE__, e.what());
+	}
+}
+
+
+void permutation_builder_test::test_2() throw(libtest::test_exception) {
+
+	static const char *testname = "permutation_builder_test::test_2()";
+
+	try {
 
 	char seq_abcd[] = { 'a', 'b', 'c', 'd' };
 	char seq_acdb[] = { 'a', 'c', 'd', 'b' };
@@ -31,16 +54,45 @@ void permutation_builder_test::perform() throw(libtest::test_exception) {
 	p3.apply(seq_abcd_1);
 	for(size_t i = 0; i < 4; i++) {
 		if(seq_abcd_1[i] != seq_abcd[i]) {
-			fail_test("permutation_builder_test::perform()",
-				__FILE__, __LINE__,
+			fail_test(testname, __FILE__, __LINE__,
 				"Wrong reference in (abcd, acdb).");
 		}
 	}
 	if(!pb3.get_perm().equals(p3)) {
-		fail_test("permutation_builder_test::perform()", __FILE__,
-			__LINE__, "Test (abcd, acdb) failed");
+		fail_test(testname, __FILE__, __LINE__,
+			"Test (abcd, acdb) failed");
 	}
 
+	} catch(exception &e) {
+		fail_test(testname, __FILE__, __LINE__, e.what());
+	}
 }
+
+
+/**	\test Uses a permutation [abcd->acdb] and a map [abcd->bacd].
+		The mapped permutation is [abcd->cbda] and the expected
+		result is [0123->3102].
+ **/
+void permutation_builder_test::test_3() throw(libtest::test_exception) {
+
+	static const char *testname = "permutation_builder_test::test_3()";
+
+	try {
+
+	char seq_abcd[] = { 'a', 'b', 'c', 'd' };
+	char seq_acdb[] = { 'a', 'c', 'd', 'b' };
+	permutation<4> map; map.permute(0, 1);
+	permutation_builder<4> pb3(seq_abcd, seq_acdb, map);
+	permutation<4> p3; p3.permute(2, 3).permute(0, 2);
+	if(!pb3.get_perm().equals(p3)) {
+		fail_test(testname, __FILE__, __LINE__,
+			"Test (abcd, acdb) with (a->b, b->a) failed");
+	}
+
+	} catch(exception &e) {
+		fail_test(testname, __FILE__, __LINE__, e.what());
+	}
+}
+
 
 } // namespace libtensor
