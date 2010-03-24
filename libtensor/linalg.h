b651fa1570eec16994b399ff52dc424fb62635cb
Dmitry Zuev <dzuev@usc.edu>
2010-03-24 22:47:16 +0000

    Support for GSL for Windows

--- libtensor/linalg.h
+++ libtensor/linalg.h
@@ -9,11 +9,15 @@
 #ifdef USE_MKL
 #include "linalg/blas_mkl.h"
 #else // USE_MKL
+#ifdef USE_GSL
+#include "linalg/blas_gsl.h"
+#else // USE_GSL
 #ifdef USE_CBLAS
 #include "linalg/blas_cblas.h"
 #else // USE_CBLAS
 #error "No BLAS implementation specified"
 #endif // USE_CBLAS
+#endif // USE_GSL
 #endif // USE_MKL
 
 #ifdef USE_MKL
