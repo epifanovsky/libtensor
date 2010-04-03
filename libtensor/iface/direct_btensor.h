ce0512280637a34018ffd05ca219497986e1fbb6
Evgeny Epifanovsky <epif@q-chem.com>
2010-04-03 06:09:05 +0000

    some tests for direct_btensor

--- libtensor/iface/direct_btensor.h
+++ libtensor/iface/direct_btensor.h
@@ -4,6 +4,7 @@
 #include <libvmm/libvmm.h>
 #include "../defs.h"
 #include "../exception.h"
+#include "../core/block_tensor_ctrl.h"
 #include "../core/direct_block_tensor.h"
 #include "btensor_i.h"
 #include "labeled_btensor.h"
@@ -38,6 +39,7 @@ private:
 	letter_expr<N> m_label;
 	ptrs_t m_ptrs;
 	direct_block_tensor<N, T, typename Traits::allocator_t> m_bt;
+	block_tensor_ctrl<N, T> m_ctrl;
 
 public:
 	//!	\name Construction and destruction
@@ -62,19 +64,15 @@ public:
 	 **/
 	labeled_btensor<N, T, false> operator()(const letter_expr<N> &expr);
 
-	//!	\name Implementation of tensor_i<N, T>
-	//@{
-
-	const dimensions<N> &get_dims() const;
-
-	//@}
 
 	//!	\name Implementation of block_tensor_i<N, T>
-	//!{
+	//@{
 
-	const block_index_space<N> &get_bis() const;
+	/**	\copydoc block_tensor_i<N, T>::get_bis()
+	 **/
+	virtual const block_index_space<N> &get_bis() const;
 
-	//!}
+	//@}
 
 protected:
 
@@ -106,18 +104,21 @@ private:
 template<size_t N, typename T, typename Traits> template<typename Core>
 direct_btensor<N, T, Traits>::direct_btensor(const letter_expr<N> &label,
 	const labeled_btensor_expr::expr<N, T, Core> &expr) :
+
 	m_label(label), m_ptrs(mk_func(m_label, expr)),
-	m_bt(m_ptrs.m_pfunc->get_bto()) {
+	m_bt(m_ptrs.m_pfunc->get_bto()), m_ctrl(m_bt) {
 
 }
 
 
 template<size_t N, typename T, typename Traits>
 direct_btensor<N, T, Traits>::~direct_btensor() {
+
 	delete m_ptrs.m_pfunc;
 	delete m_ptrs.m_peval;
 	delete m_ptrs.m_pexpr;
 }
+
 
 template<size_t N, typename T, typename Traits> template<typename Core>
 typename direct_btensor<N, T, Traits>::ptrs_t
@@ -144,87 +145,100 @@ direct_btensor<N, T, Traits>::mk_func(const letter_expr<N> &label,
 	return ptrs;
 }
 
+
 template<size_t N, typename T, typename Traits>
 labeled_btensor<N, T, false> direct_btensor<N, T, Traits>::operator()(
 	const letter_expr<N> &expr) {
+
 	return labeled_btensor<N, T, false>(*this, expr);
 }
 
-template<size_t N, typename T, typename Traits>
-const dimensions<N> &direct_btensor<N, T, Traits>::get_dims() const {
-	throw_exc("direct_btensor<N, T, Traits>", "get_dims()",
-		"Not implemented");
-}
 
 template<size_t N, typename T, typename Traits>
 const block_index_space<N> &direct_btensor<N, T, Traits>::get_bis() const {
-	throw_exc("direct_btensor<N, T, Traits>", "get_bis()",
-		"Not implemented");
+
+	return m_bt.get_bis();
 }
+
 
 template<size_t N, typename T, typename Traits>
 const symmetry<N, T> &direct_btensor<N, T, Traits>::on_req_symmetry()
 	throw(exception) {
 
-	throw_exc("direct_btensor<N, T, Traits>", "on_req_symmetry()", "NIY");
+	return m_ctrl.req_symmetry();
 }
+
 
 template<size_t N, typename T, typename Traits>
 void direct_btensor<N, T, Traits>::on_req_sym_add_element(
 	const symmetry_element_i<N, T> &elem) throw(exception) {
 
-	throw_exc("direct_btensor<N, T, Traits>", "on_req_sym_add_element()", "NIY");
+	m_ctrl.req_sym_add_element(elem);
 }
+
 
 template<size_t N, typename T, typename Traits>
 void direct_btensor<N, T, Traits>::on_req_sym_remove_element(
 	const symmetry_element_i<N, T> &elem) throw(exception) {
 
-	throw_exc("direct_btensor<N, T, Traits>", "on_req_sym_remove_element()", "NIY");
+	m_ctrl.req_sym_remove_element(elem);
 }
+
 
 template<size_t N, typename T, typename Traits>
 bool direct_btensor<N, T, Traits>::on_req_sym_contains_element(
 	const symmetry_element_i<N, T> &elem) throw(exception) {
 
-	throw_exc("direct_btensor<N, T, Traits>", "on_req_sym_contains_element()", "NIY");
+	return m_ctrl.req_sym_contains_element(elem);
 }
+
 
 template<size_t N, typename T, typename Traits>
 void direct_btensor<N, T, Traits>::on_req_sym_clear_elements()
 	throw(exception) {
-	throw_exc("direct_btensor<N, T, Traits>", "on_req_sym_clear_elements()", "NIY");
+
+	m_ctrl.req_sym_clear_elements();
 }
+
 
 template<size_t N, typename T, typename Traits>
 tensor_i<N, T> &direct_btensor<N, T, Traits>::on_req_block(const index<N> &idx)
 	throw(exception) {
-	throw_exc("direct_btensor<N, T, Traits>",
-		"on_req_block(const index<N>&)", "Unhandled event");
+
+	return m_ctrl.req_block(idx);
 }
+
 
 template<size_t N, typename T, typename Traits>
 void direct_btensor<N, T, Traits>::on_ret_block(const index<N> &idx)
 	throw(exception) {
-	throw_exc("direct_btensor<N, T, Traits>", "on_ret_block()", "NIY");
+
+	m_ctrl.ret_block(idx);
 }
+
 
 template<size_t N, typename T, typename Traits>
 bool direct_btensor<N, T, Traits>::on_req_is_zero_block(const index<N> &idx)
 	throw(exception) {
-	throw_exc("direct_btensor<N, T, Traits>", "on_req_is_zero_block()", "NIY");
+
+	return m_ctrl.req_is_zero_block(idx);
 }
+
 
 template<size_t N, typename T, typename Traits>
 void direct_btensor<N, T, Traits>::on_req_zero_block(const index<N> &idx)
 	throw(exception) {
-	throw_exc("direct_btensor<N, T, Traits>", "on_req_zero_block()", "NIY");
+
+	m_ctrl.req_zero_block(idx);
 }
+
 
 template<size_t N, typename T, typename Traits>
 void direct_btensor<N, T, Traits>::on_req_zero_all_blocks() throw(exception) {
-	throw_exc("direct_btensor<N, T, Traits>", "on_req_zero_all_blocks()", "NIY");
+
+	m_ctrl.req_zero_all_blocks();
 }
+
 
 } // namespace libtensor
 
