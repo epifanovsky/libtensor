0ce6bb1364a591bfd5b05ccb341db55f1448ca46
Evgeny Epifanovsky <epif@q-chem.com>
2010-06-07 21:15:54 +0000

    tod_contract2 using loop_list_mul

--- libtensor/tod/loop_list_mul.C
+++ libtensor/tod/loop_list_mul.C
@@ -64,7 +64,7 @@ void loop_list_mul::match_l1(list_t &loop, double d) {
 		}
 	}
 	if(i1 != loop.end()) {
-		//~ std::cout << "ddot";
+//		std::cout << "ddot";
 		m_kernelname = "ddot";
 		i1->fn() = &loop_list_mul::fn_ddot;
 		m_ddot.m_d = d;
@@ -74,7 +74,7 @@ void loop_list_mul::match_l1(list_t &loop, double d) {
 		return;
 	}
 	if(i2 != loop.end() && k1b_min != 1) {
-		//~ std::cout << "daxpy_a";
+//		std::cout << "daxpy_a";
 		m_kernelname = "daxpy_a";
 		i2->fn() = &loop_list_mul::fn_daxpy_a;
 		m_daxpy_a.m_d = d;
@@ -85,7 +85,7 @@ void loop_list_mul::match_l1(list_t &loop, double d) {
 		return;
 	}
 	if(i3 != loop.end()) {
-		//~ std::cout << "daxpy_b";
+//		std::cout << "daxpy_b";
 		m_kernelname = "daxpy_b";
 		i3->fn() = &loop_list_mul::fn_daxpy_b;
 		m_daxpy_b.m_d = d;
@@ -143,7 +143,7 @@ void loop_list_mul::match_ddot_l2(list_t &loop, double d, size_t w1) {
 	}
 
 	if(i1 != loop.end()) {
-		//~ std::cout << " dgemv_n_a";
+//		std::cout << " dgemv_n_a";
 		m_kernelname = "dgemv_n_a";
 		i1->fn() = &loop_list_mul::fn_dgemv_n_a;
 		m_dgemv_n_a.m_d = d;
@@ -157,7 +157,7 @@ void loop_list_mul::match_ddot_l2(list_t &loop, double d, size_t w1) {
 	}
 
 	if(i2 != loop.end()) {
-		//~ std::cout << " dgemv_n_b";
+//		std::cout << " dgemv_n_b";
 		m_kernelname = "dgemv_n_b";
 		i2->fn() = &loop_list_mul::fn_dgemv_n_b;
 		m_dgemv_n_b.m_d = d;
@@ -223,7 +223,7 @@ void loop_list_mul::match_daxpy_a_l2(list_t &loop, double d, size_t w1,
 		}
 	}
 	if(i1 != loop.end() && !(k1 == 1 && i2 != loop.end())) {
-		//~ std::cout << " dgemv_t_a1";
+//		std::cout << " dgemv_t_a1";
 		m_kernelname = "dgemv_t_a";
 		i1->fn() = &loop_list_mul::fn_dgemv_t_a;
 		m_dgemv_t_a.m_d = d;
@@ -238,7 +238,7 @@ void loop_list_mul::match_daxpy_a_l2(list_t &loop, double d, size_t w1,
 		return;
 	}
 	if(i2 != loop.end()) {
-		//~ std::cout << " dgemv_t_a2";
+//		std::cout << " dgemv_t_a2";
 		m_kernelname = "dgemv_t_a";
 		i2->fn() = &loop_list_mul::fn_dgemv_t_a;
 		m_dgemv_t_a.m_d = d;
@@ -287,7 +287,7 @@ void loop_list_mul::match_daxpy_b_l2(list_t &loop, double d, size_t w1,
 		}
 	}
 	if(i1 != loop.end()) {
-		//~ std::cout << " dgemv_t_b";
+//		std::cout << " dgemv_t_b";
 		m_kernelname = "dgemv_t_b";
 		i1->fn() = &loop_list_mul::fn_dgemv_t_b;
 		m_dgemv_t_b.m_d = d;
@@ -342,7 +342,7 @@ void loop_list_mul::match_dgemv_n_a_l3(list_t &loop, double d, size_t w1,
 		}
 	}
 	if(i1 != loop.end()) {
-		//~ std::cout << " dgemm_nt_ba";
+//		std::cout << " dgemm_nt_ba";
 		m_kernelname = "dgemm_nt_ba";
 		i1->fn() = &loop_list_mul::fn_dgemm_nt_ba;
 		m_dgemm_nt_ba.m_d = d;
@@ -395,7 +395,7 @@ void loop_list_mul::match_dgemv_n_b_l3(list_t &loop, double d, size_t w1,
 		}
 	}
 	if(i1 != loop.end()) {
-		//~ std::cout << " dgemm_nt_ab";
+//		std::cout << " dgemm_nt_ab";
 		m_kernelname = "dgemm_nt_ab";
 		i1->fn() = &loop_list_mul::fn_dgemm_nt_ab;
 		m_dgemm_nt_ab.m_d = d;
@@ -449,7 +449,7 @@ void loop_list_mul::match_dgemv_t_a1_l3(list_t &loop, double d, size_t w1,
 		}
 	}
 	if(i1 != loop.end()) {
-		//~ std::cout << " dgemm_tt_ab";
+//		std::cout << " dgemm_tt_ab";
 		m_kernelname = "dgemm_tt_ab";
 		i1->fn() = &loop_list_mul::fn_dgemm_tt_ab;
 		m_dgemm_tt_ab.m_d = d;
@@ -504,7 +504,7 @@ void loop_list_mul::match_dgemv_t_a2_l3(list_t &loop, double d, size_t w1,
 			}
 		}
 		if(i1 != loop.end()) {
-			//~ std::cout << " dgemm_nn_ba";
+//			std::cout << " dgemm_nn_ba";
 			m_kernelname = "dgemm_nn_ba";
 			i1->fn() = &loop_list_mul::fn_dgemm_nn_ba;
 			m_dgemm_nn_ba.m_d = d;
@@ -544,7 +544,7 @@ void loop_list_mul::match_dgemv_t_a2_l3(list_t &loop, double d, size_t w1,
 		}
 	}
 	if(i2 != loop.end()) {
-		//~ std::cout << " dgemm_tn_ba";
+//		std::cout << " dgemm_tn_ba";
 		m_kernelname = "dgemm_tn_ba";
 		i2->fn() = &loop_list_mul::fn_dgemm_tn_ba;
 		m_dgemm_tn_ba.m_d = d;
@@ -598,7 +598,7 @@ void loop_list_mul::match_dgemv_t_b_l3(list_t &loop, double d, size_t w1,
 		}
 	}
 	if(i1 != loop.end()) {
-		//~ std::cout << " dgemm_tn_ab";
+//		std::cout << " dgemm_tn_ab";
 		m_kernelname = "dgemm_tn_ab";
 		i1->fn() = &loop_list_mul::fn_dgemm_tn_ab;
 		m_dgemm_tn_ab.m_d = d;
@@ -616,379 +616,109 @@ void loop_list_mul::match_dgemv_t_b_l3(list_t &loop, double d, size_t w1,
 
 void loop_list_mul::fn_ddot(registers &r) const {
 
-	*r.m_ptrb[0] += m_ddot.m_d * cblas_ddot(m_ddot.m_n, r.m_ptra[0], 1,
+	*r.m_ptrb[0] += m_ddot.m_d * blas_ddot(m_ddot.m_n, r.m_ptra[0], 1,
 		r.m_ptra[1], 1);
 }
 
 
 void loop_list_mul::fn_daxpy_a(registers &r) const {
 
-	double d = m_daxpy_a.m_d;
-	size_t n = m_daxpy_a.m_n;
-	size_t stepc = m_daxpy_a.m_stepc;
-
-	cblas_daxpy(n, *r.m_ptra[1] * d, r.m_ptra[0], 1, r.m_ptrb[0], stepc);
+	blas_daxpy(m_daxpy_a.m_n, *r.m_ptra[1] * m_daxpy_a.m_d, r.m_ptra[0], 1,
+		r.m_ptrb[0], m_daxpy_a.m_stepc);
 }
 
 
 void loop_list_mul::fn_daxpy_b(registers &r) const {
 
-	double d = m_daxpy_b.m_d;
-	size_t n = m_daxpy_b.m_n;
-	size_t stepc = m_daxpy_b.m_stepc;
-
-	cblas_daxpy(n, *r.m_ptra[0] * d, r.m_ptra[1], 1, r.m_ptrb[0], stepc);
+	blas_daxpy(m_daxpy_b.m_n, *r.m_ptra[0] * m_daxpy_b.m_d, r.m_ptra[1], 1,
+		r.m_ptrb[0], m_daxpy_b.m_stepc);
 }
 
 
 void loop_list_mul::fn_dgemv_n_a(registers &r) const {
 
-	double d = m_dgemv_n_a.m_d;
-	size_t rows = m_dgemv_n_a.m_rows;
-	size_t cols = m_dgemv_n_a.m_cols;
-	size_t lda = m_dgemv_n_a.m_lda;
-	size_t stepb = m_dgemv_n_a.m_stepb;
-
-//	tod_contract2<N, M, K>::start_timer("dgemv_n_a");
-	cblas_dgemv(CblasRowMajor, CblasNoTrans, rows, cols, d, r.m_ptra[0], lda,
-		r.m_ptra[1], stepb, 1.0, r.m_ptrb[0], 1);
-//	tod_contract2<N, M, K>::stop_timer("dgemv_n_a");
+	blas_dgemv(false, m_dgemv_n_a.m_rows, m_dgemv_n_a.m_cols,
+		m_dgemv_n_a.m_d, r.m_ptra[0], m_dgemv_n_a.m_lda, r.m_ptra[1],
+		m_dgemv_n_a.m_stepb, 1.0, r.m_ptrb[0], 1);
 }
 
 
 void loop_list_mul::fn_dgemv_t_a(registers &r) const {
 
-	double d = m_dgemv_t_a.m_d;
-	size_t rows = m_dgemv_t_a.m_rows;
-	size_t cols = m_dgemv_t_a.m_cols;
-	size_t lda = m_dgemv_t_a.m_lda;
-	size_t stepb = m_dgemv_t_a.m_stepb;
-	size_t stepc = m_dgemv_t_a.m_stepc;
-
-//	tod_contract2<N, M, K>::start_timer("dgemv_t_a");
-	cblas_dgemv(CblasRowMajor, CblasTrans, rows, cols, d, r.m_ptra[0], lda,
-		r.m_ptra[1], stepb, 1.0, r.m_ptrb[0], stepc);
-//	tod_contract2<N, M, K>::stop_timer("dgemv_t_a");
+	blas_dgemv(true, m_dgemv_t_a.m_rows, m_dgemv_t_a.m_cols,
+		m_dgemv_t_a.m_d, r.m_ptra[0], m_dgemv_t_a.m_lda, r.m_ptra[1],
+		m_dgemv_t_a.m_stepb, 1.0, r.m_ptrb[0], m_dgemv_t_a.m_stepc);
 }
 
 
 void loop_list_mul::fn_dgemv_n_b(registers &r) const {
 
-	double d = m_dgemv_n_b.m_d;
-	size_t rows = m_dgemv_n_b.m_rows;
-	size_t cols = m_dgemv_n_b.m_cols;
-	size_t ldb = m_dgemv_n_b.m_ldb;
-	size_t stepa = m_dgemv_n_b.m_stepa;
-
-//	tod_contract2<N, M, K>::start_timer("dgemv_n_b");
-	cblas_dgemv(CblasRowMajor, CblasNoTrans, rows, cols, d, r.m_ptra[1], ldb,
-		r.m_ptra[0], stepa, 1.0, r.m_ptrb[0], 1);
-//	tod_contract2<N, M, K>::stop_timer("dgemv_n_b");
+	blas_dgemv(false, m_dgemv_n_b.m_rows, m_dgemv_n_b.m_cols,
+		m_dgemv_n_b.m_d, r.m_ptra[1], m_dgemv_n_b.m_ldb, r.m_ptra[0],
+		m_dgemv_n_b.m_stepa, 1.0, r.m_ptrb[0], 1);
 }
 
 
 void loop_list_mul::fn_dgemv_t_b(registers &r) const {
 
-	double d = m_dgemv_t_b.m_d;
-	size_t rows = m_dgemv_t_b.m_rows;
-	size_t cols = m_dgemv_t_b.m_cols;
-	size_t ldb = m_dgemv_t_b.m_ldb;
-	size_t stepa = m_dgemv_t_b.m_stepa;
-	size_t stepc = m_dgemv_t_b.m_stepc;
-
-//	tod_contract2<N, M, K>::start_timer("dgemv_t_b");
-	cblas_dgemv(CblasRowMajor, CblasTrans, rows, cols, d, r.m_ptra[1], ldb,
-		r.m_ptra[0], stepa, 1.0, r.m_ptrb[0], stepc);
-//	tod_contract2<N, M, K>::stop_timer("dgemv_t_b");
+	blas_dgemv(true, m_dgemv_t_b.m_rows, m_dgemv_t_b.m_cols,
+		m_dgemv_t_b.m_d, r.m_ptra[1], m_dgemv_t_b.m_ldb, r.m_ptra[0],
+		m_dgemv_t_b.m_stepa, 1.0, r.m_ptrb[0], m_dgemv_t_b.m_stepc);
 }
 
 
 void loop_list_mul::fn_dgemm_nt_ab(registers &r) const {
 
-	double d = m_dgemm_nt_ab.m_d;
-	size_t rowsa = m_dgemm_nt_ab.m_rowsa;
-	size_t colsb = m_dgemm_nt_ab.m_colsb;
-	size_t colsa = m_dgemm_nt_ab.m_colsa;
-	size_t lda = m_dgemm_nt_ab.m_lda;
-	size_t ldb = m_dgemm_nt_ab.m_ldb;
-	size_t ldc = m_dgemm_nt_ab.m_ldc;
-
-//	tod_contract2<N, M, K>::start_timer("dgemm_nt_ab");
-	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans,
-		rowsa, colsb, colsa, d, r.m_ptra[0], lda, r.m_ptra[1], ldb,
-		1.0, r.m_ptrb[0], ldc);
-//	tod_contract2<N, M, K>::stop_timer("dgemm_nt_ab");
+	blas_dgemm(false, true, m_dgemm_nt_ab.m_rowsa, m_dgemm_nt_ab.m_colsb,
+		m_dgemm_nt_ab.m_colsa, m_dgemm_nt_ab.m_d, r.m_ptra[0],
+		m_dgemm_nt_ab.m_lda, r.m_ptra[1], m_dgemm_nt_ab.m_ldb, 1.0,
+		r.m_ptrb[0], m_dgemm_nt_ab.m_ldc);
 }
 
 
 void loop_list_mul::fn_dgemm_tn_ab(registers &r) const {
 
-	double d = m_dgemm_tn_ab.m_d;
-	size_t rowsa = m_dgemm_tn_ab.m_rowsa;
-	size_t colsb = m_dgemm_tn_ab.m_colsb;
-	size_t colsa = m_dgemm_tn_ab.m_colsa;
-	size_t lda = m_dgemm_tn_ab.m_lda;
-	size_t ldb = m_dgemm_tn_ab.m_ldb;
-	size_t ldc = m_dgemm_tn_ab.m_ldc;
-
-//	tod_contract2<N, M, K>::start_timer("dgemm_tn_ab");
-	cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans,
-		rowsa, colsb, colsa, d, r.m_ptra[0], lda, r.m_ptra[1], ldb,
-		1.0, r.m_ptrb[0], ldc);
-//	tod_contract2<N, M, K>::stop_timer("dgemm_tn_ab");
+	blas_dgemm(true, false, m_dgemm_tn_ab.m_rowsa, m_dgemm_tn_ab.m_colsb,
+		m_dgemm_tn_ab.m_colsa, m_dgemm_tn_ab.m_d, r.m_ptra[0],
+		m_dgemm_tn_ab.m_lda, r.m_ptra[1], m_dgemm_tn_ab.m_ldb, 1.0,
+		r.m_ptrb[0], m_dgemm_tn_ab.m_ldc);
 }
 
 
 void loop_list_mul::fn_dgemm_tt_ab(registers &r) const {
 
-	double d = m_dgemm_tt_ab.m_d;
-	size_t rowsa = m_dgemm_tt_ab.m_rowsa;
-	size_t colsb = m_dgemm_tt_ab.m_colsb;
-	size_t colsa = m_dgemm_tt_ab.m_colsa;
-	size_t lda = m_dgemm_tt_ab.m_lda;
-	size_t ldb = m_dgemm_tt_ab.m_ldb;
-	size_t ldc = m_dgemm_tt_ab.m_ldc;
-
-//	tod_contract2<N, M, K>::start_timer("dgemm_tt_ab");
-	cblas_dgemm(CblasRowMajor, CblasTrans, CblasTrans,
-		rowsa, colsb, colsa, d, r.m_ptra[0], lda, r.m_ptra[1], ldb,
-		1.0, r.m_ptrb[0], ldc);
-//	tod_contract2<N, M, K>::stop_timer("dgemm_tt_ab");
+	blas_dgemm(true, true, m_dgemm_tt_ab.m_rowsa, m_dgemm_tt_ab.m_colsb,
+		m_dgemm_tt_ab.m_colsa, m_dgemm_tt_ab.m_d, r.m_ptra[0],
+		m_dgemm_tt_ab.m_lda, r.m_ptra[1], m_dgemm_tt_ab.m_ldb, 1.0,
+		r.m_ptrb[0], m_dgemm_tt_ab.m_ldc);
 }
 
 
 void loop_list_mul::fn_dgemm_nn_ba(registers &r) const {
 
-	double d = m_dgemm_nn_ba.m_d;
-	size_t rowsb = m_dgemm_nn_ba.m_rowsb;
-	size_t colsa = m_dgemm_nn_ba.m_colsa;
-	size_t colsb = m_dgemm_nn_ba.m_colsb;
-	size_t ldb = m_dgemm_nn_ba.m_ldb;
-	size_t lda = m_dgemm_nn_ba.m_lda;
-	size_t ldc = m_dgemm_nn_ba.m_ldc;
-
-//	tod_contract2<N, M, K>::start_timer("dgemm_nn_ba");
-	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
-		rowsb, colsa, colsb, d, r.m_ptra[1], ldb, r.m_ptra[0], lda,
-		1.0, r.m_ptrb[1], ldc);
-//	tod_contract2<N, M, K>::stop_timer("dgemm_nn_ba");
+	blas_dgemm(false, false, m_dgemm_nn_ba.m_rowsb, m_dgemm_nn_ba.m_colsa,
+		m_dgemm_nn_ba.m_colsb, m_dgemm_nn_ba.m_d, r.m_ptra[1],
+		m_dgemm_nn_ba.m_ldb, r.m_ptra[0], m_dgemm_nn_ba.m_lda, 1.0,
+		r.m_ptrb[0], m_dgemm_nn_ba.m_ldc);
 }
 
 
 void loop_list_mul::fn_dgemm_nt_ba(registers &r) const {
 
-	double d = m_dgemm_nt_ba.m_d;
-	size_t rowsb = m_dgemm_nt_ba.m_rowsb;
-	size_t colsa = m_dgemm_nt_ba.m_colsa;
-	size_t colsb = m_dgemm_nt_ba.m_colsb;
-	size_t ldb = m_dgemm_nt_ba.m_ldb;
-	size_t lda = m_dgemm_nt_ba.m_lda;
-	size_t ldc = m_dgemm_nt_ba.m_ldc;
-
-//	tod_contract2<N, M, K>::start_timer("dgemm_nt_ba");
-	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans,
-		rowsb, colsa, colsb, d, r.m_ptra[1], ldb, r.m_ptra[0], lda,
-		1.0, r.m_ptrb[0], ldc);
-//	tod_contract2<N, M, K>::stop_timer("dgemm_nt_ba");
+	blas_dgemm(false, true, m_dgemm_nt_ba.m_rowsb, m_dgemm_nt_ba.m_colsa,
+		m_dgemm_nt_ba.m_colsb, m_dgemm_nt_ba.m_d, r.m_ptra[1],
+		m_dgemm_nt_ba.m_ldb, r.m_ptra[0], m_dgemm_nt_ba.m_lda, 1.0,
+		r.m_ptrb[0], m_dgemm_nt_ba.m_ldc);
 }
 
 
 void loop_list_mul::fn_dgemm_tn_ba(registers &r) const {
 
-	double d = m_dgemm_tn_ba.m_d;
-	size_t rowsb = m_dgemm_tn_ba.m_rowsb;
-	size_t colsa = m_dgemm_tn_ba.m_colsa;
-	size_t colsb = m_dgemm_tn_ba.m_colsb;
-	size_t ldb = m_dgemm_tn_ba.m_ldb;
-	size_t lda = m_dgemm_tn_ba.m_lda;
-	size_t ldc = m_dgemm_tn_ba.m_ldc;
-
-//	tod_contract2<N, M, K>::start_timer("dgemm_tn_ba");
-	cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans,
-		rowsb, colsa, colsb, d, r.m_ptra[1], ldb, r.m_ptra[0], lda,
-		1.0, r.m_ptrb[0], ldc);
-//	tod_contract2<N, M, K>::stop_timer("dgemm_tn_ba");
+	blas_dgemm(true, false, m_dgemm_tn_ba.m_rowsb, m_dgemm_tn_ba.m_colsa,
+		m_dgemm_tn_ba.m_colsb, m_dgemm_tn_ba.m_d, r.m_ptra[1],
+		m_dgemm_tn_ba.m_ldb, r.m_ptra[0], m_dgemm_tn_ba.m_lda, 1.0,
+		r.m_ptrb[0], m_dgemm_tn_ba.m_ldc);
 }
 
-
-/*
-void loop_list_add::match_l1(list_t &loop, double c) {
-
-	//	1. Find:
-	//	--------
-	//	w   a  b
-	//	w1  1  1  -->  b_i += a_i
-	//	--------       sz(i) = w1
-	//	               [daxpy]
-	//
-	//	2. Minimize k1a:
-	//
-	//	----------
-	//	w   a  b
-	//	w1  1  k1a  -->  b_i# += a_i
-	//	----------       sz(i) = w1, sz(#) = k1a
-	//	                 [daxpy]
-	//
-	//	3. Minimize k1b:
-	//
-	//	----------
-	//	w   a    b
-	//	w1  k1b  1  -->  b_i += a_i#
-	//	----------       sz(i) = w1, sz(#) = k1b
-	//	                 [daxpy]
-	//
-	//	4. Fallback [daxpy]
-	//
-	iterator_t i1 = loop.end(), i2 = loop.end(), i3 = loop.end();
-	size_t k1a_min = 0, k1b_min = 0;
-	for(iterator_t i = loop.begin(); i != loop.end(); i++) {
-		i->fn() = 0;
-		if(i->stepa(0) == 1) {
-			if(k1a_min == 0 || k1a_min > i->stepb(0)) {
-				i2 = i; k1a_min = i->stepb(0);
-			}
-			if(i->stepb(0) == 1) {
-				i1 = i;
-			}
-		} else {
-			if(i->stepb(0) == 1) {
-				if(k1b_min == 0 || k1b_min > i->stepa(0)) {
-					i3 = i; k1b_min = i->stepa(0);
-				}
-			}
-		}
-	}
-
-	if(i1 != loop.end()) {
-		i1->fn() = &loop_list_add::fn_daxpy;
-		m_daxpy.m_k = c;
-		m_daxpy.m_n = i1->weight();
-		m_daxpy.m_stepa = 1;
-		m_daxpy.m_stepb = 1;
-		loop.splice(loop.end(), loop, i1);
-		return;
-	}
-
-	if(i2 != loop.end()) {
-		i2->fn() = &loop_list_add::fn_daxpy;
-		m_daxpy.m_k = c;
-		m_daxpy.m_n = i2->weight();
-		m_daxpy.m_stepa = 1;
-		m_daxpy.m_stepb = i2->stepb(0);
-		//~ match_l2_a(loop, c, i2->weight());
-		loop.splice(loop.end(), loop, i2);
-		return;
-	}
-
-	if(i3 != loop.end()) {
-		i3->fn() = &loop_list_add::fn_daxpy;
-		m_daxpy.m_k = c;
-		m_daxpy.m_n = i3->weight();
-		m_daxpy.m_stepa = i3->stepa(0);
-		m_daxpy.m_stepb = 1;
-		//~ match_l2_b(loop, c, i3->weight());
-		loop.splice(loop.end(), loop, i3);
-		return;
-	}
-
-	iterator_t i4 = loop.begin();
-	i4->fn() = &loop_list_add::fn_daxpy;
-	m_daxpy.m_k = c;
-	m_daxpy.m_n = i4->weight();
-	m_daxpy.m_stepa = i4->stepa(0);
-	m_daxpy.m_stepb = i4->stepb(0);
-	loop.splice(loop.end(), loop, i4);
-}
-
-
-void loop_list_add::match_l2_a(list_t &loop, double c, size_t w1) {
-
-	//	Found pattern:
-	//	---------
-	//	w   a  b
-	//	w1  1  k1  -->  b_i# += a_i
-	//	---------       sz(i) = w1, sz(#) = k1
-	//	                [daxpy]
-
-	//	1. Find k2a = 1:
-	//	--------------
-	//	w   a       b
-	//	w1  1       k1
-	//	w2  k2a*w1  1   -->  b_ij += a_j$i
-	//	--------------       sz(i) = w1, sz(j) = w2, sz($) = k2a
-	//	                     [daxpby_trp]
-	iterator_t i1 = loop.end();
-	for(iterator_t i = loop.begin(); i != loop.end(); i++) {
-		if(i->stepb(0) != 1) continue;
-		if(i->stepa(0) != w1) continue;
-		i1 = i;
-		break;
-	}
-
-	if(i1 != loop.end()) {
-		i1->fn() = &loop_list_add::fn_daxpby_trp;
-		m_daxpby_trp.m_k = c;
-		m_daxpby_trp.m_ni = w1;
-		m_daxpby_trp.m_nj = i1->weight();
-		m_daxpby_trp.m_stepa = 1;
-		m_daxpby_trp.m_stepb = 1;
-		loop.splice(loop.end(), loop, i1);
-	}
-}
-
-
-void loop_list_add::match_l2_b(list_t &loop, double c, size_t w1) {
-
-}
-
-
-void loop_list_add::fn_daxpy(registers &r) const {
-
-	static const char *method = "fn_daxpy(registers&)";
-
-	if(m_daxpy.m_n == 0) return;
-
-#ifdef LIBTENSOR_DEBUG
-	if(r.m_ptra[0] + (m_daxpy.m_n - 1) * m_daxpy.m_stepa >=
-		r.m_ptra_end[0]) {
-		throw overflow(g_ns, k_clazz, method, __FILE__, __LINE__,
-			"source");
-	}
-	if(r.m_ptrb[0] + (m_daxpy.m_n - 1) * m_daxpy.m_stepb >=
-		r.m_ptrb_end[0]) {
-		throw overflow(g_ns, k_clazz, method, __FILE__, __LINE__,
-			"destination");
-	}
-#endif // LIBTENSOR_DEBUG
-
-	blas_daxpy(m_daxpy.m_n, m_daxpy.m_k, r.m_ptra[0], m_daxpy.m_stepa,
-		r.m_ptrb[0], m_daxpy.m_stepb);
-}
-
-
-void loop_list_add::fn_daxpby_trp(registers &r) const {
-
-	static const char *method = "fn_daxpby_trp(registers&)";
-
-	if(m_daxpby_trp.m_ni == 0 || m_daxpby_trp.m_nj) return;
-
-#ifdef LIBTENSOR_DEBUG
-	if(r.m_ptra[0] + (m_daxpby_trp.m_ni - 1) * m_daxpby_trp.m_stepa >=
-		r.m_ptra_end[0]) {
-		throw overflow(g_ns, k_clazz, method, __FILE__, __LINE__,
-			"source");
-	}
-	if(r.m_ptrb[0] + (m_daxpby_trp.m_nj - 1) * m_daxpby_trp.m_stepb >=
-		r.m_ptrb_end[0]) {
-		throw overflow(g_ns, k_clazz, method, __FILE__, __LINE__,
-			"destination");
-	}
-#endif // LIBTENSOR_DEBUG
-
-	blas::daxpby_trp(r.m_ptra[0], r.m_ptrb[0], m_daxpby_trp.m_ni,
-		m_daxpby_trp.m_nj, m_daxpby_trp.m_stepa, m_daxpby_trp.m_stepb,
-		m_daxpby_trp.m_k, 1.0);
-}
-*/
 
 } // namespace libtensor
