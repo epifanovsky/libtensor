6fb9a77976be418f0b94090ebd8c0a85768244ba
Evgeny Epifanovsky <epif@q-chem.com>
2010-06-11 14:40:27 +0000

    bugfix in contraction2

--- tests/contraction2_test.h
+++ tests/contraction2_test.h
@@ -20,6 +20,10 @@ private:
 	void test_4() throw(libtest::test_exception);
 	void test_5() throw(libtest::test_exception);
 	void test_6() throw(libtest::test_exception);
+	void test_7() throw(libtest::test_exception);
+	void test_8() throw(libtest::test_exception);
+	void test_9() throw(libtest::test_exception);
+	void test_10() throw(libtest::test_exception);
 };
 
 } // namespace libtensor
