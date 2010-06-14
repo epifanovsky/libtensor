11ddaf940f9618523d708f182f59b4844750746c
Michael Wormit <michael.wormit@iwr.uni-heidelberg.de>
2010-06-14 10:51:32 +0000

    tod_mult and tod_mult1 generalized for use with symmetry, extended tests respectively.

--- libtensor/tod/tod_mult1.h
+++ libtensor/tod/tod_mult1.h
@@ -4,6 +4,7 @@
 #include "../defs.h"
 #include "../timings.h"
 #include "../core/tensor_i.h"
+#include "loop_list_elem1.h"
 #include "bad_dimensions.h"
 
 namespace libtensor {
@@ -25,21 +26,36 @@ namespace libtensor {
 	\ingroup libtensor
  **/
 template<size_t N>
-class tod_mult1 : public timings< tod_mult1<N> > {
+class tod_mult1 :
+	public loop_list_elem1,
+	public timings< tod_mult1<N> > {
 public:
 	static const char *k_clazz; //!< Class name
 
 private:
 	tensor_i<N, double> &m_tb; //!< Second argument
+	permutation<N> m_pb; //!< Permutation of argument
 	bool m_recip; //!< Reciprocal (multiplication by 1/bi)
+	double m_c; //!< Scaling coefficient
 
 public:
 	/**	\brief Creates the operation
 		\param tb Second argument.
 		\param recip \c false (default) sets up multiplication and
 			\c true sets up element-wise division.
+		\param c Coefficient
 	 **/
-	tod_mult1(tensor_i<N, double> &tb, bool recip = false);
+	tod_mult1(tensor_i<N, double> &tb, bool recip = false, double c = 1.0);
+
+	/**	\brief Creates the operation
+		\param tb Second argument.
+		\param p Permutation of argument
+		\param recip \c false (default) sets up multiplication and
+			\c true sets up element-wise division.
+		\param c Coefficient
+	 **/
+	tod_mult1(tensor_i<N, double> &tb, const permutation<N> &p,
+			bool recip = false, double c = 1.0);
 
 	/**	\brief Performs the operation, replaces the output.
 		\param ta Tensor A.
@@ -51,6 +67,13 @@ public:
 		\param c Coefficient.
 	 **/
 	void perform(tensor_i<N, double> &ta, double c);
+
+private:
+	void do_perform(tensor_i<N, double> &ta, bool doadd, double c);
+
+	void build_loop(typename loop_list_elem1::list_t &loop,
+			const dimensions<N> &dimsa, const dimensions<N> &dimsb,
+			const permutation<N> &permb);
 };
 
 
@@ -59,8 +82,14 @@ const char *tod_mult1<N>::k_clazz = "tod_mult1<N>";
 
 
 template<size_t N>
-tod_mult1<N>::tod_mult1(tensor_i<N, double> &tb, bool recip) :
-	m_tb(tb), m_recip(recip) {
+tod_mult1<N>::tod_mult1(tensor_i<N, double> &tb, bool recip, double c) :
+	m_tb(tb), m_recip(recip), m_c(c) {
+}
+
+template<size_t N>
+tod_mult1<N>::tod_mult1(tensor_i<N, double> &tb, const permutation<N> &p,
+		bool recip, double c) :
+	m_tb(tb), m_pb(p), m_recip(recip), m_c(c) {
 }
 
 
@@ -69,32 +98,16 @@ void tod_mult1<N>::perform(tensor_i<N, double> &ta) {
 
 	static const char *method = "perform(tensor_i<N, double>&)";
 
-	if(!m_tb.get_dims().equals(ta.get_dims())) {
+	dimensions<N> dimsb(m_tb.get_dims());
+	dimsb.permute(m_pb);
+
+	if(! dimsb.equals(ta.get_dims())) {
 		throw bad_dimensions(g_ns, k_clazz, method, __FILE__, __LINE__,
 			"ta");
 	}
 
-	tod_mult1<N>::start_timer();
+	do_perform(ta, false, 1.0);
 
-	tensor_ctrl<N, double> ca(ta), cb(m_tb);
-	double *pa = ca.req_dataptr();
-	const double *pb = cb.req_const_dataptr();
-
-	size_t sz = ta.get_dims().get_size();
-	if(m_recip) {
-		for(size_t i = 0; i < sz; i++) {
-			pa[i] /= pb[i];
-		}
-	} else {
-		for(size_t i = 0; i < sz; i++) {
-			pa[i] *= pb[i];
-		}
-	}
-
-	cb.ret_dataptr(pb); pb = 0;
-	ca.ret_dataptr(pa); pa = 0;
-
-	tod_mult1<N>::stop_timer();
 }
 
 
@@ -103,32 +116,88 @@ void tod_mult1<N>::perform(tensor_i<N, double> &ta, double c) {
 
 	static const char *method = "perform(tensor_i<N, double>&, double)";
 
-	if(!m_tb.get_dims().equals(ta.get_dims())) {
+	dimensions<N> dimsb(m_tb.get_dims());
+	dimsb.permute(m_pb);
+
+	if(! dimsb.equals(ta.get_dims())) {
 		throw bad_dimensions(g_ns, k_clazz, method, __FILE__, __LINE__,
 			"ta");
 	}
 
+	do_perform(ta, true, c);
+}
+
+template<size_t N>
+void tod_mult1<N>::do_perform(tensor_i<N, double> &ta, bool doadd, double c) {
+
+	typedef typename loop_list_elem1::list_t list_t;
+	typedef typename loop_list_elem1::registers registers_t;
+	typedef typename loop_list_elem1::node node_t;
+
 	tod_mult1<N>::start_timer();
 
+	try {
+
 	tensor_ctrl<N, double> ca(ta), cb(m_tb);
+	ca.req_prefetch();
+	cb.req_prefetch();
+
+	const dimensions<N> &dimsa = ta.get_dims();
+	const dimensions<N> &dimsb = m_tb.get_dims();
+
+	list_t loop;
+	build_loop(loop, dimsa, dimsb, m_pb);
+
 	double *pa = ca.req_dataptr();
 	const double *pb = cb.req_const_dataptr();
 
-	size_t sz = ta.get_dims().get_size();
-	if(m_recip) {
-		for(size_t i = 0; i < sz; i++) {
-			pa[i] += c * pa[i] / pb[i];
-		}
-	} else {
-		for(size_t i = 0; i < sz; i++) {
-			pa[i] += c * pa[i] * pb[i];
-		}
-	}
+	registers_t r;
+	r.m_ptra[0] = pb;
+	r.m_ptrb[0] = pa;
+	r.m_ptra_end[0] = pb + dimsb.get_size();
+	r.m_ptrb_end[0] = pa + dimsa.get_size();
+
+	loop_list_elem1::run_loop(loop, r, m_c * c, doadd, m_recip);
 
 	cb.ret_dataptr(pb); pb = 0;
 	ca.ret_dataptr(pa); pa = 0;
 
+	} catch (...) {
+		tod_mult1<N>::stop_timer();
+		throw;
+	}
+
 	tod_mult1<N>::stop_timer();
+
+}
+
+
+template<size_t N>
+void tod_mult1<N>::build_loop(typename loop_list_elem1::list_t &loop,
+		const dimensions<N> &dimsa, const dimensions<N> &dimsb,
+		const permutation<N> &permb) {
+
+	typedef typename loop_list_elem1::iterator_t iterator_t;
+	typedef typename loop_list_elem1::node node_t;
+
+	size_t mapb[N];
+	for(register size_t i = 0; i < N; i++) mapb[i] = i;
+	permb.apply(mapb);
+
+	for (size_t idxa = 0; idxa < N; ) {
+		size_t len = 1;
+		size_t idxb = mapb[idxa];
+
+		do {
+			len *= dimsa.get_dim(idxa);
+			idxa++; idxb++;
+		} while (idxa < N &&  mapb[idxa] == idxb);
+
+		iterator_t inode = loop.insert(loop.end(), node_t(len));
+		inode->stepa(0) = dimsb.get_increment(idxb - 1);
+		inode->stepb(0) = dimsa.get_increment(idxa - 1);
+	}
+
 }
 
 
