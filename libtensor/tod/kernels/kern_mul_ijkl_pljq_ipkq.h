c5b70ee02fa81afaf20badd1733f445c058a1d32
Evgeny Epifanovsky <epif@q-chem.com>
2010-10-06 23:00:45 +0000

    level-6 tod mul kernels originating from ijk_pkiq_pjq

---
+++ libtensor/tod/kernels/kern_mul_ijkl_pljq_ipkq.h
@@ -0,0 +1,35 @@
+#ifndef LIBTENSOR_KERN_MUL_IJKL_PLJQ_IPKQ_H
+#define LIBTENSOR_KERN_MUL_IJKL_PLJQ_IPKQ_H
+
+#include "kern_mul_ijk_pkiq_pjq.h"
+
+namespace libtensor {
+
+
+class kern_mul_ijkl_pljq_ipkq : public kernel_base<2, 1> {
+public:
+	static const char *k_clazz; //!< Kernel name
+
+private:
+	double m_d;
+	size_t m_ni, m_nj, m_nk, m_nl, m_np, m_nq;
+	size_t m_spa, m_sla, m_sja, m_sib, m_spb, m_skb, m_sic, m_sjc, m_skc;
+
+public:
+	virtual ~kern_mul_ijkl_pljq_ipkq() { }
+
+	virtual const char *get_name() const {
+		return k_clazz;
+	}
+
+	virtual void run(const loop_registers<2, 1> &r);
+
+	static kernel_base<2, 1> *match(const kern_mul_ijk_pkiq_pjq &z,
+		list_t &in, list_t &out);
+
+};
+
+
+} // namespace libtensor
+
+#endif // LIBTENSOR_KERN_MUL_IJKL_PLJQ_IPKQ_H
