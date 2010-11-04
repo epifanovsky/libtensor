a86527876775c8699efef5854fe9c9b74f1af91b
Evgeny Epifanovsky <epif@q-chem.com>
2010-11-04 18:37:18 +0000

    test for a bug in so_symmetrize for se_part

--- tests/symmetry/so_symmetrize_test.h
+++ tests/symmetry/so_symmetrize_test.h
@@ -17,6 +17,8 @@ private:
 	void test_1() throw(libtest::test_exception);
 	void test_2() throw(libtest::test_exception);
 	void test_3() throw(libtest::test_exception);
+	void test_4() throw(libtest::test_exception);
+	void test_5() throw(libtest::test_exception);
 
 };
 
