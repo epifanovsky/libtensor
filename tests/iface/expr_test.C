54704ac022d903a08649e779c86ee634def02e52
Evgeny Epifanovsky <epif@q-chem.com>
2011-02-21 01:07:37 +0000

    uncommented tests in expr_test

--- tests/iface/expr_test.C
+++ tests/iface/expr_test.C
@@ -19,14 +19,14 @@ void expr_test::perform() throw(libtest::test_exception) {
 
 	try {
 
-//		test_1();
-//		test_2();
-//		test_3();
-//		test_4();
-//		test_5();
-//		test_6();
-//		test_7();
-//		test_8();
+		test_1();
+		test_2();
+		test_3();
+		test_4();
+		test_5();
+		test_6();
+		test_7();
+		test_8();
 		test_9();
 
 	} catch(...) {
