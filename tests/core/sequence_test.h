4cdfe31b0648c11fecbd97532b4f1385ce84a825
Evgeny Epifanovsky <epif@q-chem.com>
2011-08-08 21:58:40 +0000

    cleaned up class sequence, more tests and docs

--- tests/core/sequence_test.h
+++ tests/core/sequence_test.h
@@ -5,20 +5,37 @@
 
 namespace libtensor {
 
+
 /**	\brief Tests the libtensor::sequence class
 
-	\ingroup libtensor_tests_core
+    \sa sequence<N, T>
+
+    \ingroup libtensor_tests_core
  **/
 class sequence_test : public libtest::unit_test {
 public:
-	virtual void perform() throw(libtest::test_exception);
+    virtual void perform() throw(libtest::test_exception);
 
 private:
-	void test_1() throw(libtest::test_exception);
+    void test_ctor_1() throw(libtest::test_exception);
+    void test_ctor_2() throw(libtest::test_exception);
+    void test_ctor_3() throw(libtest::test_exception);
+    void test_ctor_4() throw(libtest::test_exception);
+    void test_ctor_5() throw(libtest::test_exception);
+    void test_ctor_6() throw(libtest::test_exception);
+    void test_ctor_7() throw(libtest::test_exception);
+    void test_ctor_8() throw(libtest::test_exception);
+    void test_ctor_9() throw(libtest::test_exception);
+
+    void test_at_1() throw(libtest::test_exception);
+    void test_at_2() throw(libtest::test_exception);
+
+    void test_exc_1() throw(libtest::test_exception);
+    void test_exc_2() throw(libtest::test_exception);
 
 };
+
 
 } // namespace libtensor
 
 #endif // LIBTENSOR_SEQUENCE_TEST_H
-
