3d870ce6bb4bab59db6a0e4c0dd7cf2b4529c675
Evgeny Epifanovsky <epif@q-chem.com>
2011-09-23 21:07:17 +0000

    removed unused class tod_symcontract2

--- tests/tod/libtensor_tod_suite.h
+++ tests/tod/libtensor_tod_suite.h
@@ -26,7 +26,6 @@
 #include "tod_set_diag_test.h"
 #include "tod_set_elem_test.h"
 #include "tod_sum_test.h"
-#include "tod_symcontract2_test.h"
 #include "tod_trace_test.h"
 
 using libtest::unit_test_factory;
@@ -68,7 +67,6 @@ namespace libtensor {
 	\li libtensor::tod_set_diag_test
 	\li libtensor::tod_set_elem_test
 	\li libtensor::tod_sum_test
-	\li libtensor::tod_symcontract2_test
 	\li libtensor::tod_trace_test
 **/
 class libtensor_tod_suite : public libtest::test_suite {
@@ -98,7 +96,6 @@ private:
 	unit_test_factory<tod_set_diag_test> m_utf_tod_set_diag;
 	unit_test_factory<tod_set_elem_test> m_utf_tod_set_elem;
 	unit_test_factory<tod_sum_test> m_utf_tod_sum;
-	unit_test_factory<tod_symcontract2_test> m_utf_tod_symcontract2;
 	unit_test_factory<tod_trace_test> m_utf_tod_trace;
 
 public:
