a2962cc478654ec487a3e672e84be724012747d5
Evgeny Epifanovsky <epif@q-chem.com>
2011-12-09 04:21:23 +0000

    libtensor version changed to 2.1

--- libtensor/version.C
+++ libtensor/version.C
@@ -5,22 +5,22 @@ namespace libtensor {
 
 const char *version::k_status = "trunk";
 const char *version::k_authors[] = {
-	"Evgeny Epifanovsky",
-	"Michael Wormit",
-	"Dmitry Zuev"
+    "Evgeny Epifanovsky",
+    "Michael Wormit",
+    "Dmitry Zuev"
 };
 
 
 version::version() : m_status(k_status) {
 
-	std::ostringstream ss;
-	ss << k_major << "." << k_minor << "-" << m_status;
-	m_string = ss.str();
+    std::ostringstream ss;
+    ss << k_major << "." << k_minor << "-" << m_status;
+    m_string = ss.str();
 
-	size_t nauthors = sizeof(k_authors)/sizeof(char*);
-	for(size_t i = 0; i < nauthors; i++) {
-		m_authors.push_back(std::string(k_authors[i]));
-	}
+    size_t nauthors = sizeof(k_authors)/sizeof(char*);
+    for(size_t i = 0; i < nauthors; i++) {
+        m_authors.push_back(std::string(k_authors[i]));
+    }
 }
 
 
--- libtensor/version.h
+++ libtensor/version.h
@@ -8,76 +8,76 @@
 namespace libtensor {
 
 
-/**	\brief Version of the %tensor library
+/** \brief Version of the %tensor library
 
-	The %version of the library is specified using the %version number and
-	status. The number consists of a major number and a minor number. The
-	status string describes the release status.
+    The %version of the library is specified using the %version number and
+    status. The number consists of a major number and a minor number. The
+    status string describes the release status.
 
-	For example, %version 2.0-alpha2 has major number 2, minor number 0,
-	and status "alpha2" meaning the second alpha release.
+    For example, %version 2.0-alpha2 has major number 2, minor number 0,
+    and status "alpha2" meaning the second alpha release.
 
-	\ingroup libtensor
+    \ingroup libtensor
  **/
 class version : public libvmm::singleton<version> {
-	friend class libvmm::singleton<version>;
+    friend class libvmm::singleton<version>;
 
 private:
-	static const unsigned k_major = 2; //!< Major %version number
-	static const unsigned k_minor = 0; //!< Minor %version number
-	static const char *k_status; //!< Version status
-	static const char *k_authors[]; //!< List of authors
+    static const unsigned k_major = 2; //!< Major %version number
+    static const unsigned k_minor = 1; //!< Minor %version number
+    static const char *k_status; //!< Version status
+    static const char *k_authors[]; //!< List of authors
 
 private:
-	std::string m_status; //!< Version status
-	std::string m_string; //!< Version string
-	std::list<std::string> m_authors; //!< List of authors
+    std::string m_status; //!< Version status
+    std::string m_string; //!< Version string
+    std::list<std::string> m_authors; //!< List of authors
 
 protected:
-	version();
+    version();
 
-	const std::string &get_status_impl() const {
-		return m_status;
-	}
+    const std::string &get_status_impl() const {
+        return m_status;
+    }
 
-	const std::string &get_string_impl() const {
-		return m_string;
-	}
+    const std::string &get_string_impl() const {
+        return m_string;
+    }
 
-	const std::list<std::string> &get_authors_impl() const {
-		return m_authors;
-	}
+    const std::list<std::string> &get_authors_impl() const {
+        return m_authors;
+    }
 
 public:
-	/**	\brief Returns the major %version number
-	 **/
-	static unsigned get_major() {
-		return k_major;
-	}
+    /**    \brief Returns the major %version number
+     **/
+    static unsigned get_major() {
+        return k_major;
+    }
 
-	/**	\brief Returns the minor %version number
-	 **/
-	static unsigned get_minor() {
-		return k_minor;
-	}
+    /**    \brief Returns the minor %version number
+     **/
+    static unsigned get_minor() {
+        return k_minor;
+    }
 
-	/**	\brief Returns the %version status
-	 **/
-	static const std::string &get_status() {
-		return version::get_instance().get_status_impl();
-	}
+    /**    \brief Returns the %version status
+     **/
+    static const std::string &get_status() {
+        return version::get_instance().get_status_impl();
+    }
 
-	/**	\brief Returns the string that corresponds to the %version
-	 **/
-	static const std::string &get_string() {
-		return version::get_instance().get_string_impl();
-	}
+    /**    \brief Returns the string that corresponds to the %version
+     **/
+    static const std::string &get_string() {
+        return version::get_instance().get_string_impl();
+    }
 
-	/**	\brief Returns the list of authors
-	 **/
-	static const std::list<std::string> &get_authors() {
-		return version::get_instance().get_authors_impl();
-	}
+    /**    \brief Returns the list of authors
+     **/
+    static const std::list<std::string> &get_authors() {
+        return version::get_instance().get_authors_impl();
+    }
 
 };
 
