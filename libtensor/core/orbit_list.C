a6fe712558cd363e787b15cee4b1d8781be4ce54
Evgeny Epifanovsky <epif@q-chem.com>
2012-01-20 23:46:16 +0000

    bto_set template

--- libtensor/core/orbit_list.C
+++ libtensor/core/orbit_list.C
@@ -13,6 +13,8 @@ template class orbit_list<3, double>;
 template class orbit_list<4, double>;
 template class orbit_list<5, double>;
 template class orbit_list<6, double>;
+template class orbit_list<7, double>;
+template class orbit_list<8, double>;
 
 
 } // namespace libtensor
