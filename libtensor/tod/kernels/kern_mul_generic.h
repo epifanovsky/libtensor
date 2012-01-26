21f208cf5e3685d85a11495a7f9f5069a7d65998
Evgeny Epifanovsky <epif@q-chem.com>
2012-01-26 01:06:15 +0000

    multiplication kernel based on i_i_i_x (elementwise vector multiply)

--- libtensor/tod/kernels/kern_mul_generic.h
+++ libtensor/tod/kernels/kern_mul_generic.h
@@ -7,29 +7,30 @@ namespace libtensor {
 
 /** \brief Generic kernel for multiplications
 
- 	\ingroup libtensor_tod_kernel
+     \ingroup libtensor_tod_kernel
  **/
 class kern_mul_generic : public kernel_base<2, 1> {
-	friend class kern_mul_i_i_x;
-	friend class kern_mul_i_x_i;
-	friend class kern_mul_x_p_p;
+    friend class kern_mul_i_i_i;
+    friend class kern_mul_i_i_x;
+    friend class kern_mul_i_x_i;
+    friend class kern_mul_x_p_p;
 
 public:
-	static const char *k_clazz; //!< Kernel name
+    static const char *k_clazz; //!< Kernel name
 
 private:
-	double m_d;
+    double m_d;
 
 public:
-	virtual ~kern_mul_generic() { }
+    virtual ~kern_mul_generic() { }
 
-	virtual const char *get_name() const {
-		return k_clazz;
-	}
+    virtual const char *get_name() const {
+        return k_clazz;
+    }
 
-	virtual void run(const loop_registers<2, 1> &r);
+    virtual void run(const loop_registers<2, 1> &r);
 
-	static kernel_base<2, 1> *match(double d, list_t &in, list_t &out);
+    static kernel_base<2, 1> *match(double d, list_t &in, list_t &out);
 
 };
 
---
+++ libtensor/tod/kernels/kern_mul_i_i_i.h
@@ -0,0 +1,38 @@
+#ifndef LIBTENSOR_KERN_MUL_I_I_I_H
+#define LIBTENSOR_KERN_MUL_I_I_I_H
+
+#include "kern_mul_generic.h"
+
+namespace libtensor {
+
+
+/**
+    \ingroup libtensor_tod_kernel
+ **/
+class kern_mul_i_i_i : public kernel_base<2, 1> {
+public:
+    static const char *k_clazz; //!< Kernel name
+
+private:
+    double m_d;
+    size_t m_ni;
+    size_t m_sia, m_sib, m_sic;
+
+public:
+    virtual ~kern_mul_i_i_i() { }
+
+    virtual const char *get_name() const {
+        return k_clazz;
+    }
+
+    virtual void run(const loop_registers<2, 1> &r);
+
+    static kernel_base<2, 1> *match(const kern_mul_generic &z,
+        list_t &in, list_t &out);
+
+};
+
+
+} // namespace libtensor
+
+#endif // LIBTENSOR_KERN_MUL_I_I_I_H
