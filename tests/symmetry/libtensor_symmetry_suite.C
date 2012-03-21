78e25a09b498da8c37fe68befc8419ae94e297bb
Michael Wormit <michael.wormit@iwr.uni-heidelberg.de>
2012-03-21 06:56:18 +0000

    Disable all se_label related classes and tests.

--- tests/symmetry/libtensor_symmetry_suite.C
+++ tests/symmetry/libtensor_symmetry_suite.C
@@ -5,13 +5,13 @@ namespace libtensor {
 libtensor_symmetry_suite::libtensor_symmetry_suite() :
 		libtest::test_suite("libtensor") {
 
-    add_test("block_labeling", m_utf_block_labeling);
+//    add_test("block_labeling", m_utf_block_labeling);
     add_test("combine_part", m_utf_combine_part);
-    add_test("evaluation_rule", m_utf_evaluation_rule);
+//    add_test("evaluation_rule", m_utf_evaluation_rule);
     add_test("permutation_group", m_utf_permutation_group);
-    add_test("point_group_table", m_utf_point_group_table);
-    add_test("product_table_container", m_utf_product_table_container);
-    add_test("se_label", m_utf_se_label);
+//    add_test("point_group_table", m_utf_point_group_table);
+//    add_test("product_table_container", m_utf_product_table_container);
+//    add_test("se_label", m_utf_se_label);
     add_test("se_part", m_utf_se_part);
     add_test("se_perm", m_utf_se_perm);
 //	add_test("so_apply", m_utf_so_apply);
@@ -20,18 +20,18 @@ libtensor_symmetry_suite::libtensor_symmetry_suite() :
 //	add_test("so_apply_impl_perm", m_utf_so_apply_impl_perm);
 	add_test("so_copy", m_utf_so_copy);
 //	add_test("so_dirprod", m_utf_so_dirprod);
-	add_test("so_dirprod_impl_label", m_utf_so_dirprod_impl_label);
+//	add_test("so_dirprod_impl_label", m_utf_so_dirprod_impl_label);
     add_test("so_dirprod_impl_part", m_utf_so_dirprod_impl_part);
     add_test("so_dirprod_impl_perm", m_utf_so_dirprod_impl_perm);
 	add_test("so_dirsum", m_utf_so_dirsum);
-    add_test("so_dirsum_impl_label", m_utf_so_dirsum_impl_label);
+//    add_test("so_dirsum_impl_label", m_utf_so_dirsum_impl_label);
     add_test("so_dirsum_impl_part", m_utf_so_dirsum_impl_part);
     add_test("so_dirsum_impl_perm", m_utf_so_dirsum_impl_perm);
 //	add_test("so_merge", m_utf_so_merge);
-	add_test("so_merge_impl_label", m_utf_so_merge_impl_label);
+//	add_test("so_merge_impl_label", m_utf_so_merge_impl_label);
     add_test("so_merge_impl_part", m_utf_so_merge_impl_part);
     add_test("so_merge_impl_perm", m_utf_so_merge_impl_perm);
-	add_test("so_permute_impl_label", m_utf_so_permute_impl_label);
+//	add_test("so_permute_impl_label", m_utf_so_permute_impl_label);
     add_test("so_permute_impl_part", m_utf_so_permute_impl_part);
     add_test("so_permute_impl_perm", m_utf_so_permute_impl_perm);
 //	add_test("so_reduce", m_utf_so_reduce);
@@ -41,7 +41,7 @@ libtensor_symmetry_suite::libtensor_symmetry_suite() :
 //	add_test("so_symmetrize", m_utf_so_symmetrize);
     add_test("symmetry_element_set_adapter",
              m_utf_symmetry_element_set_adapter);
-    add_test("transfer_rule", m_utf_transfer_rule);
+//    add_test("transfer_rule", m_utf_transfer_rule);
 }
 
 }
