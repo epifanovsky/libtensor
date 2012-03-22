514b945baae69a7985b9365a93d8065e392e0144
Evgeny Epifanovsky <epif@q-chem.com>
2012-03-22 03:38:07 +0000

    diag_to_contract2_space routine + tests

---
+++ libtensor/diag_tensor/diag_to_contract2_space.h
@@ -0,0 +1,61 @@
+#ifndef LIBTENSOR_DIAG_TO_CONTRACT2_SPACE_H
+#define LIBTENSOR_DIAG_TO_CONTRACT2_SPACE_H
+
+#include <libtensor/dense_tensor/to_contract2_dims.h>
+#include "diag_tensor_space.h"
+
+namespace libtensor {
+
+
+/** \brief Forms the space of the contraction of two diagonal tensors
+    \tparam N Order of first tensor less contraction order.
+    \tparam M Order of second tensor less contraction order.
+    \tparam K Order of contraction.
+
+    This algorithm takes the spaces of two input diagonal tensors and forms
+    the space of the output of the contraction operation.
+
+    \ingroup libtensor_diag_tensor
+ **/
+template<size_t N, size_t M, size_t K>
+class diag_to_contract2_space {
+public:
+    static const char *k_clazz; //!< Class name
+
+private:
+    to_contract2_dims<N, M, K> m_dimsc; //!< Dimensions of C
+    diag_tensor_space<N + M> m_dtsc; //!< Space of C
+
+public:
+    /** \brief Builds the result space
+     **/
+    diag_to_contract2_space(const contraction2<N, M, K> &contr,
+        const diag_tensor_space<N + K> &dtsa,
+        const diag_tensor_space<M + K> &dtsb);
+
+    /** \brief Returns the result space
+     **/
+    const diag_tensor_space<N + M> &get_dtsc() const {
+        return m_dtsc;
+    }
+
+private:
+    /** \brief Forms a subspace in C from subspaces A and B, adds it to
+            space of C
+     **/
+    void add_subspace(const contraction2<N, M, K> &contr,
+        const diag_tensor_subspace<N + K> &ssa,
+        const diag_tensor_subspace<M + K> &ssb);
+
+    /** \brief Returns true if the space contains the subspace
+     **/
+    bool contains(const diag_tensor_space<N + M> &dts,
+        const diag_tensor_subspace<N + M> &ss) const;
+
+};
+
+
+} // namespace libtensor
+
+#endif // LIBTENSOR_DIAG_TO_CONTRACT2_SPACE_H
+
--- tests/diag_tensor/libtensor_diag_tensor_suite.h
+++ tests/diag_tensor/libtensor_diag_tensor_suite.h
@@ -6,6 +6,7 @@
 #include "diag_tensor_space_test.h"
 #include "diag_tensor_subspace_test.h"
 #include "diag_to_add_space_test.h"
+#include "diag_to_contract2_space_test.h"
 #include "diag_tod_adjust_space_test.h"
 #include "diag_tod_set_test.h"
 #include "tod_conv_diag_tensor_test.h"
@@ -27,6 +28,7 @@ namespace libtensor {
      - libtensor::diag_tensor_space_test
      - libtensor::diag_tensor_subspace_test
      - libtensor::diag_to_add_space_test
+     - libtensor::diag_to_contract2_space_test
      - libtensor::diag_tod_adjust_space_test
      - libtensor::diag_tod_set_test
      - libtensor::tod_conv_diag_tensor_test
@@ -39,6 +41,8 @@ private:
     unit_test_factory<diag_tensor_space_test> m_utf_diag_tensor_space;
     unit_test_factory<diag_tensor_subspace_test> m_utf_diag_tensor_subspace;
     unit_test_factory<diag_to_add_space_test> m_utf_diag_to_add_space;
+    unit_test_factory<diag_to_contract2_space_test>
+        m_utf_diag_to_contract2_space;
     unit_test_factory<diag_tod_adjust_space_test> m_utf_diag_tod_adjust_space;
     unit_test_factory<diag_tod_set_test> m_utf_diag_tod_set;
     unit_test_factory<tod_conv_diag_tensor_test> m_utf_tod_conv_diag_tensor;
