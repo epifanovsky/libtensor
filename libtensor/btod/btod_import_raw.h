01f32582fc3d4c503d86bf97ea40e805d36c4fdf
Evgeny Epifanovsky <epif@q-chem.com>
2012-03-29 00:46:25 +0000

    compilation fix in btod_import_raw

--- libtensor/btod/btod_import_raw.h
+++ libtensor/btod/btod_import_raw.h
@@ -1,6 +1,7 @@
 #ifndef LIBTENSOR_BTOD_IMPORT_RAW_H
 #define LIBTENSOR_BTOD_IMPORT_RAW_H
 
+#include <libtensor/core/allocator.h>
 #include <libtensor/dense_tensor/tod_import_raw.h>
 #include <libtensor/dense_tensor/tod_set.h>
 #include "btod_import_raw_base.h"
