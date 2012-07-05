77828f1c31acd41a01a94fd6e83dc005ab0cbd6f
Michael Wormit <michael.wormit@iwr.uni-heidelberg.de>
2012-07-05 00:13:47 +0000

    Fixed so_symmetrize algorithm for se_label + new tests.

---
+++ tests/symmetry/so_symmetrize_se_label_test.h
@@ -0,0 +1,34 @@
+#ifndef LIBTENSOR_SO_SYMMETRIZE_SE_LABEL_TEST_H
+#define LIBTENSOR_SO_SYMMETRIZE_SE_LABEL_TEST_H
+
+#include "se_label_test_base.h"
+
+namespace libtensor {
+
+/** \brief Tests the libtensor::so_symmetrize_se_label class
+
+    \ingroup libtensor_tests_sym
+ **/
+class so_symmetrize_se_label_test : public se_label_test_base {
+public:
+    virtual void perform() throw(libtest::test_exception);
+
+private:
+    void test_empty(
+            const std::string &table_id) throw(libtest::test_exception);
+    void test_sym2_1(
+            const std::string &table_id) throw(libtest::test_exception);
+    void test_sym2_2(
+            const std::string &table_id) throw(libtest::test_exception);
+    void test_sym3_1(
+            const std::string &table_id) throw(libtest::test_exception);
+
+    using se_label_test_base::setup_pg_table;
+    using se_label_test_base::check_allowed;
+};
+
+
+} // namespace libtensor
+
+#endif // LIBTENSOR_SO_SYMMETRIZE_SE_LABEL_TEST_H
+
