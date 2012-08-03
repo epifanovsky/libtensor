79bbe0116d62dd28c2660de8198df91efb35a6c8
Michael Wormit <michael.wormit@iwr.uni-heidelberg.de>
2012-08-03 15:33:38 +0000

    Missing template instantiations for evaluation_rule

--- libtensor/symmetry/inst/evaluation_rule_inst.C
+++ libtensor/symmetry/inst/evaluation_rule_inst.C
@@ -425,9 +425,336 @@ template void evaluation_rule<12>::merge(evaluation_rule<11> &res,
         const sequence<12, size_t> &mmap, const mask<11> &smsk) const;
 
 template class evaluation_rule<13>;
+template void evaluation_rule<13>::reduce(evaluation_rule<12> &res,
+        const sequence<13, size_t> &rmap,
+        const sequence<1, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<13>::reduce(evaluation_rule<11> &res,
+        const sequence<13, size_t> &rmap,
+        const sequence<2, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<13>::reduce(evaluation_rule<10> &res,
+        const sequence<13, size_t> &rmap,
+        const sequence<3, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<13>::reduce(evaluation_rule<9> &res,
+        const sequence<13, size_t> &rmap,
+        const sequence<4, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<13>::reduce(evaluation_rule<8> &res,
+        const sequence<13, size_t> &rmap,
+        const sequence<5, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<13>::reduce(evaluation_rule<7> &res,
+        const sequence<13, size_t> &rmap,
+        const sequence<6, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<13>::reduce(evaluation_rule<6> &res,
+        const sequence<13, size_t> &rmap,
+        const sequence<7, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<13>::reduce(evaluation_rule<5> &res,
+        const sequence<13, size_t> &rmap,
+        const sequence<8, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<13>::reduce(evaluation_rule<4> &res,
+        const sequence<13, size_t> &rmap,
+        const sequence<9, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<13>::reduce(evaluation_rule<3> &res,
+        const sequence<13, size_t> &rmap,
+        const sequence<10, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<13>::reduce(evaluation_rule<2> &res,
+        const sequence<13, size_t> &rmap,
+        const sequence<11, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<13>::reduce(evaluation_rule<1> &res,
+        const sequence<13, size_t> &rmap,
+        const sequence<12, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<13>::merge(evaluation_rule<1> &res,
+        const sequence<13, size_t> &mmap, const mask<1> &smsk) const;
+template void evaluation_rule<13>::merge(evaluation_rule<2> &res,
+        const sequence<13, size_t> &mmap, const mask<2> &smsk) const;
+template void evaluation_rule<13>::merge(evaluation_rule<3> &res,
+        const sequence<13, size_t> &mmap, const mask<3> &smsk) const;
+template void evaluation_rule<13>::merge(evaluation_rule<4> &res,
+        const sequence<13, size_t> &mmap, const mask<4> &smsk) const;
+template void evaluation_rule<13>::merge(evaluation_rule<5> &res,
+        const sequence<13, size_t> &mmap, const mask<5> &smsk) const;
+template void evaluation_rule<13>::merge(evaluation_rule<6> &res,
+        const sequence<13, size_t> &mmap, const mask<6> &smsk) const;
+template void evaluation_rule<13>::merge(evaluation_rule<7> &res,
+        const sequence<13, size_t> &mmap, const mask<7> &smsk) const;
+template void evaluation_rule<13>::merge(evaluation_rule<8> &res,
+        const sequence<13, size_t> &mmap, const mask<8> &smsk) const;
+template void evaluation_rule<13>::merge(evaluation_rule<9> &res,
+        const sequence<13, size_t> &mmap, const mask<9> &smsk) const;
+template void evaluation_rule<13>::merge(evaluation_rule<10> &res,
+        const sequence<13, size_t> &mmap, const mask<10> &smsk) const;
+template void evaluation_rule<13>::merge(evaluation_rule<11> &res,
+        const sequence<13, size_t> &mmap, const mask<11> &smsk) const;
+template void evaluation_rule<13>::merge(evaluation_rule<12> &res,
+        const sequence<13, size_t> &mmap, const mask<12> &smsk) const;
+
 template class evaluation_rule<14>;
+template void evaluation_rule<14>::reduce(evaluation_rule<13> &res,
+        const sequence<14, size_t> &rmap,
+        const sequence<1, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<14>::reduce(evaluation_rule<12> &res,
+        const sequence<14, size_t> &rmap,
+        const sequence<2, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<14>::reduce(evaluation_rule<11> &res,
+        const sequence<14, size_t> &rmap,
+        const sequence<3, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<14>::reduce(evaluation_rule<10> &res,
+        const sequence<14, size_t> &rmap,
+        const sequence<4, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<14>::reduce(evaluation_rule<9> &res,
+        const sequence<14, size_t> &rmap,
+        const sequence<5, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<14>::reduce(evaluation_rule<8> &res,
+        const sequence<14, size_t> &rmap,
+        const sequence<6, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<14>::reduce(evaluation_rule<7> &res,
+        const sequence<14, size_t> &rmap,
+        const sequence<7, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<14>::reduce(evaluation_rule<6> &res,
+        const sequence<14, size_t> &rmap,
+        const sequence<8, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<14>::reduce(evaluation_rule<5> &res,
+        const sequence<14, size_t> &rmap,
+        const sequence<9, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<14>::reduce(evaluation_rule<4> &res,
+        const sequence<14, size_t> &rmap,
+        const sequence<10, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<14>::reduce(evaluation_rule<3> &res,
+        const sequence<14, size_t> &rmap,
+        const sequence<11, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<14>::reduce(evaluation_rule<2> &res,
+        const sequence<14, size_t> &rmap,
+        const sequence<12, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<14>::reduce(evaluation_rule<1> &res,
+        const sequence<14, size_t> &rmap,
+        const sequence<13, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<14>::merge(evaluation_rule<1> &res,
+        const sequence<14, size_t> &mmap, const mask<1> &smsk) const;
+template void evaluation_rule<14>::merge(evaluation_rule<2> &res,
+        const sequence<14, size_t> &mmap, const mask<2> &smsk) const;
+template void evaluation_rule<14>::merge(evaluation_rule<3> &res,
+        const sequence<14, size_t> &mmap, const mask<3> &smsk) const;
+template void evaluation_rule<14>::merge(evaluation_rule<4> &res,
+        const sequence<14, size_t> &mmap, const mask<4> &smsk) const;
+template void evaluation_rule<14>::merge(evaluation_rule<5> &res,
+        const sequence<14, size_t> &mmap, const mask<5> &smsk) const;
+template void evaluation_rule<14>::merge(evaluation_rule<6> &res,
+        const sequence<14, size_t> &mmap, const mask<6> &smsk) const;
+template void evaluation_rule<14>::merge(evaluation_rule<7> &res,
+        const sequence<14, size_t> &mmap, const mask<7> &smsk) const;
+template void evaluation_rule<14>::merge(evaluation_rule<8> &res,
+        const sequence<14, size_t> &mmap, const mask<8> &smsk) const;
+template void evaluation_rule<14>::merge(evaluation_rule<9> &res,
+        const sequence<14, size_t> &mmap, const mask<9> &smsk) const;
+template void evaluation_rule<14>::merge(evaluation_rule<10> &res,
+        const sequence<14, size_t> &mmap, const mask<10> &smsk) const;
+template void evaluation_rule<14>::merge(evaluation_rule<11> &res,
+        const sequence<14, size_t> &mmap, const mask<11> &smsk) const;
+template void evaluation_rule<14>::merge(evaluation_rule<12> &res,
+        const sequence<14, size_t> &mmap, const mask<12> &smsk) const;
+template void evaluation_rule<14>::merge(evaluation_rule<13> &res,
+        const sequence<14, size_t> &mmap, const mask<13> &smsk) const;
+
 template class evaluation_rule<15>;
+template void evaluation_rule<15>::reduce(evaluation_rule<14> &res,
+        const sequence<15, size_t> &rmap,
+        const sequence<1, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<15>::reduce(evaluation_rule<13> &res,
+        const sequence<15, size_t> &rmap,
+        const sequence<2, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<15>::reduce(evaluation_rule<12> &res,
+        const sequence<15, size_t> &rmap,
+        const sequence<3, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<15>::reduce(evaluation_rule<11> &res,
+        const sequence<15, size_t> &rmap,
+        const sequence<4, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<15>::reduce(evaluation_rule<10> &res,
+        const sequence<15, size_t> &rmap,
+        const sequence<5, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<15>::reduce(evaluation_rule<9> &res,
+        const sequence<15, size_t> &rmap,
+        const sequence<6, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<15>::reduce(evaluation_rule<8> &res,
+        const sequence<15, size_t> &rmap,
+        const sequence<7, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<15>::reduce(evaluation_rule<7> &res,
+        const sequence<15, size_t> &rmap,
+        const sequence<8, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<15>::reduce(evaluation_rule<6> &res,
+        const sequence<15, size_t> &rmap,
+        const sequence<9, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<15>::reduce(evaluation_rule<5> &res,
+        const sequence<15, size_t> &rmap,
+        const sequence<10, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<15>::reduce(evaluation_rule<4> &res,
+        const sequence<15, size_t> &rmap,
+        const sequence<11, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<15>::reduce(evaluation_rule<3> &res,
+        const sequence<15, size_t> &rmap,
+        const sequence<12, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<15>::reduce(evaluation_rule<2> &res,
+        const sequence<15, size_t> &rmap,
+        const sequence<13, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<15>::reduce(evaluation_rule<1> &res,
+        const sequence<15, size_t> &rmap,
+        const sequence<14, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<15>::merge(evaluation_rule<1> &res,
+        const sequence<15, size_t> &mmap, const mask<1> &smsk) const;
+template void evaluation_rule<15>::merge(evaluation_rule<2> &res,
+        const sequence<15, size_t> &mmap, const mask<2> &smsk) const;
+template void evaluation_rule<15>::merge(evaluation_rule<3> &res,
+        const sequence<15, size_t> &mmap, const mask<3> &smsk) const;
+template void evaluation_rule<15>::merge(evaluation_rule<4> &res,
+        const sequence<15, size_t> &mmap, const mask<4> &smsk) const;
+template void evaluation_rule<15>::merge(evaluation_rule<5> &res,
+        const sequence<15, size_t> &mmap, const mask<5> &smsk) const;
+template void evaluation_rule<15>::merge(evaluation_rule<6> &res,
+        const sequence<15, size_t> &mmap, const mask<6> &smsk) const;
+template void evaluation_rule<15>::merge(evaluation_rule<7> &res,
+        const sequence<15, size_t> &mmap, const mask<7> &smsk) const;
+template void evaluation_rule<15>::merge(evaluation_rule<8> &res,
+        const sequence<15, size_t> &mmap, const mask<8> &smsk) const;
+template void evaluation_rule<15>::merge(evaluation_rule<9> &res,
+        const sequence<15, size_t> &mmap, const mask<9> &smsk) const;
+template void evaluation_rule<15>::merge(evaluation_rule<10> &res,
+        const sequence<15, size_t> &mmap, const mask<10> &smsk) const;
+template void evaluation_rule<15>::merge(evaluation_rule<11> &res,
+        const sequence<15, size_t> &mmap, const mask<11> &smsk) const;
+template void evaluation_rule<15>::merge(evaluation_rule<12> &res,
+        const sequence<15, size_t> &mmap, const mask<12> &smsk) const;
+template void evaluation_rule<15>::merge(evaluation_rule<13> &res,
+        const sequence<15, size_t> &mmap, const mask<13> &smsk) const;
+template void evaluation_rule<15>::merge(evaluation_rule<14> &res,
+        const sequence<15, size_t> &mmap, const mask<14> &smsk) const;
+
 template class evaluation_rule<16>;
+template void evaluation_rule<16>::reduce(evaluation_rule<15> &res,
+        const sequence<16, size_t> &rmap,
+        const sequence<1, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<16>::reduce(evaluation_rule<14> &res,
+        const sequence<16, size_t> &rmap,
+        const sequence<2, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<16>::reduce(evaluation_rule<13> &res,
+        const sequence<16, size_t> &rmap,
+        const sequence<3, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<16>::reduce(evaluation_rule<12> &res,
+        const sequence<16, size_t> &rmap,
+        const sequence<4, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<16>::reduce(evaluation_rule<11> &res,
+        const sequence<16, size_t> &rmap,
+        const sequence<5, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<16>::reduce(evaluation_rule<10> &res,
+        const sequence<16, size_t> &rmap,
+        const sequence<6, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<16>::reduce(evaluation_rule<9> &res,
+        const sequence<16, size_t> &rmap,
+        const sequence<7, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<16>::reduce(evaluation_rule<8> &res,
+        const sequence<16, size_t> &rmap,
+        const sequence<8, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<16>::reduce(evaluation_rule<7> &res,
+        const sequence<16, size_t> &rmap,
+        const sequence<9, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<16>::reduce(evaluation_rule<6> &res,
+        const sequence<16, size_t> &rmap,
+        const sequence<10, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<16>::reduce(evaluation_rule<5> &res,
+        const sequence<16, size_t> &rmap,
+        const sequence<11, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<16>::reduce(evaluation_rule<4> &res,
+        const sequence<16, size_t> &rmap,
+        const sequence<12, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<16>::reduce(evaluation_rule<3> &res,
+        const sequence<16, size_t> &rmap,
+        const sequence<13, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<16>::reduce(evaluation_rule<2> &res,
+        const sequence<16, size_t> &rmap,
+        const sequence<14, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<16>::reduce(evaluation_rule<1> &res,
+        const sequence<16, size_t> &rmap,
+        const sequence<15, label_group_t> &rdims,
+        const product_table_i &pt) const;
+template void evaluation_rule<16>::merge(evaluation_rule<1> &res,
+        const sequence<16, size_t> &mmap, const mask<1> &smsk) const;
+template void evaluation_rule<16>::merge(evaluation_rule<2> &res,
+        const sequence<16, size_t> &mmap, const mask<2> &smsk) const;
+template void evaluation_rule<16>::merge(evaluation_rule<3> &res,
+        const sequence<16, size_t> &mmap, const mask<3> &smsk) const;
+template void evaluation_rule<16>::merge(evaluation_rule<4> &res,
+        const sequence<16, size_t> &mmap, const mask<4> &smsk) const;
+template void evaluation_rule<16>::merge(evaluation_rule<5> &res,
+        const sequence<16, size_t> &mmap, const mask<5> &smsk) const;
+template void evaluation_rule<16>::merge(evaluation_rule<6> &res,
+        const sequence<16, size_t> &mmap, const mask<6> &smsk) const;
+template void evaluation_rule<16>::merge(evaluation_rule<7> &res,
+        const sequence<16, size_t> &mmap, const mask<7> &smsk) const;
+template void evaluation_rule<16>::merge(evaluation_rule<8> &res,
+        const sequence<16, size_t> &mmap, const mask<8> &smsk) const;
+template void evaluation_rule<16>::merge(evaluation_rule<9> &res,
+        const sequence<16, size_t> &mmap, const mask<9> &smsk) const;
+template void evaluation_rule<16>::merge(evaluation_rule<10> &res,
+        const sequence<16, size_t> &mmap, const mask<10> &smsk) const;
+template void evaluation_rule<16>::merge(evaluation_rule<11> &res,
+        const sequence<16, size_t> &mmap, const mask<11> &smsk) const;
+template void evaluation_rule<16>::merge(evaluation_rule<12> &res,
+        const sequence<16, size_t> &mmap, const mask<12> &smsk) const;
+template void evaluation_rule<16>::merge(evaluation_rule<13> &res,
+        const sequence<16, size_t> &mmap, const mask<13> &smsk) const;
+template void evaluation_rule<16>::merge(evaluation_rule<14> &res,
+        const sequence<16, size_t> &mmap, const mask<14> &smsk) const;
+template void evaluation_rule<16>::merge(evaluation_rule<15> &res,
+        const sequence<16, size_t> &mmap, const mask<15> &smsk) const;
 
 
 } // namespace libtensor
