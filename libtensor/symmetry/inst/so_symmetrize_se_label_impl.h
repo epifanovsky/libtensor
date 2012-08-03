74cbd2b2ae133b33617b1961d6092e4e47c889b4
Michael Wormit <michael.wormit@iwr.uni-heidelberg.de>
2012-08-03 15:33:18 +0000

    Adapted symmetrize for se_label + tests

--- libtensor/symmetry/inst/so_symmetrize_se_label_impl.h
+++ libtensor/symmetry/inst/so_symmetrize_se_label_impl.h
@@ -69,10 +69,10 @@ symmetry_operation_impl< so_symmetrize<N, T>, se_label<N, T> >::do_perform(
          transfer_labeling(bl1, idmap, bl2);
 
          evaluation_rule<N> r2;
-         for (typename evaluation_rule<N>::const_iterator it = r1.begin();
-                 it != r1.end(); it++) {
+         for (typename evaluation_rule<N>::const_iterator ir = r1.begin();
+                 ir != r1.end(); ir++) {
 
-             const product_rule<N> &pr1 = r1.get_product(it);
+             const product_rule<N> &pr1 = r1.get_product(ir);
 
              permutation_generator<N> pg(msk);
              do {
