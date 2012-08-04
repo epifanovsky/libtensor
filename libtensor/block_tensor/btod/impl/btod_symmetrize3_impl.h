ebb9ecf59ea38e47b309e675a357d3ceccc144a8
Evgeny Epifanovsky <epif@q-chem.com>
2012-08-04 15:47:50 +0000

    move btod_symmetrize, btod_symmetrize3 to block_tensor/btod/

--- libtensor/block_tensor/btod/impl/btod_symmetrize3_impl.h
+++ libtensor/block_tensor/btod/impl/btod_symmetrize3_impl.h
@@ -1,135 +1,16 @@
-#ifndef LIBTENSOR_BTOD_SYMMETRIZE3_H
-#define LIBTENSOR_BTOD_SYMMETRIZE3_H
+#ifndef LIBTENSOR_BTOD_SYMMETRIZE3_IMPL_H
+#define LIBTENSOR_BTOD_SYMMETRIZE3_IMPL_H
 
-#include <algorithm>
-#include "../exception.h"
-#include "../timings.h"
-#include "../core/allocator.h"
-#include "../core/block_index_subspace_builder.h"
-#include "../core/permutation_builder.h"
-#include "../core/transf_list.h"
+#include <algorithm> // for std::swap
+#include <libtensor/core/allocator.h>
 #include <libtensor/core/orbit.h>
-#include "../symmetry/so_copy.h"
-#include "../symmetry/so_symmetrize.h"
 #include <libtensor/core/scalar_transf_double.h>
-#include <libtensor/block_tensor/bto/additive_bto.h>
-#include <libtensor/block_tensor/btod/btod_traits.h>
+#include <libtensor/dense_tensor/dense_tensor.h>
+#include <libtensor/symmetry/so_copy.h>
+#include <libtensor/symmetry/so_symmetrize.h>
+#include "../btod_symmetrize3.h"
 
 namespace libtensor {
-
-
-/** \brief (Anti-)symmetrizes the result of a block %tensor operation
-        over three groups of indexes
-    \tparam N Tensor order.
-
-    The operation symmetrizes or anti-symmetrizes the result of another
-    block %tensor operation over three indexes or groups of indexes.
-
-    \f[
-        b_{ijk} = P_{\pm} a_{ijk} = a_{ijk} \pm a_{jik} \pm a_{kji} \pm
-            a_{ikj} + a_{jki} + a_{kij}
-    \f]
-
-    The constructor takes three different indexes to be symmetrized.
-
-    \ingroup libtensor_btod
- **/
-template<size_t N>
-class btod_symmetrize3 :
-    public additive_bto<N, bto_traits<double> >,
-    public timings< btod_symmetrize3<N> > {
-
-public:
-    static const char *k_clazz; //!< Class name
-
-private:
-    struct schrec {
-        size_t ai;
-        tensor_transf<N, double> tr;
-        schrec() : ai(0) { }
-        schrec(size_t ai_, const tensor_transf<N, double> &tr_) :
-            ai(ai_), tr(tr_) { }
-    };
-    typedef std::pair<size_t, schrec> sym_schedule_pair_t;
-    typedef std::multimap<size_t, schrec> sym_schedule_t;
-
-private:
-    additive_bto<N, bto_traits<double> > &m_op; //!< Symmetrized operation
-    size_t m_i1; //!< First %index
-    size_t m_i2; //!< Second %index
-    size_t m_i3; //!< Third %index
-    bool m_symm; //!< Symmetrization/anti-symmetrization
-    symmetry<N, double> m_sym; //!< Symmetry of the result
-    assignment_schedule<N, double> m_sch; //!< Schedule
-
-public:
-    //!    \name Construction and destruction
-    //@{
-
-    /** \brief Initializes the operation
-        \param op Operation to be symmetrized.
-        \param i1 First %index.
-        \param i2 Second %index.
-        \param i3 Third %index.
-        \param symm True for symmetrization, false for
-            anti-symmetrization.
-     **/
-    btod_symmetrize3(additive_bto<N, bto_traits<double> > &op,
-            size_t i1, size_t i2, size_t i3, bool symm);
-
-    /** \brief Virtual destructor
-     **/
-    virtual ~btod_symmetrize3() { }
-
-    //@}
-
-
-    //!    \name Implementation of direct_block_tensor_operation<N, double>
-    //@{
-
-    virtual const block_index_space<N> &get_bis() const {
-        return m_op.get_bis();
-    }
-
-    virtual const symmetry<N, double> &get_symmetry() const {
-        return m_sym;
-    }
-
-    virtual const assignment_schedule<N, double> &get_schedule() const {
-        return m_sch;
-    }
-
-    virtual void sync_on() {
-        m_op.sync_on();
-    }
-
-    virtual void sync_off() {
-        m_op.sync_off();
-    }
-
-    //@}
-
-protected:
-    //!    \brief Implementation of additive_bto<N, bto_traits<double> >
-    //@{
-
-    virtual void compute_block(bool zero, dense_tensor_i<N, double> &blk,
-        const index<N> &i, const tensor_transf<N, double> &tr,
-        const double &c);
-
-    //@}
-
-private:
-    void make_symmetry();
-    void make_schedule();
-    void make_schedule_blk(const abs_index<N> &ai,
-        sym_schedule_t &sch) const;
-
-private:
-    btod_symmetrize3(const btod_symmetrize3<N>&);
-    const btod_symmetrize3<N> &operator=(const btod_symmetrize3<N>&);
-
-};
 
 
 template<size_t N>
@@ -372,4 +253,4 @@ void btod_symmetrize3<N>::make_schedule_blk(const abs_index<N> &ai,
 
 } // namespace libtensor
 
-#endif // LIBTENSOR_BTOD_SYMMETRIZE3_H
+#endif // LIBTENSOR_BTOD_SYMMETRIZE3_IMPL_H
