1c18e29f79ce6e1898e404a5880c55f6891a4247
Michael Wormit <michael.wormit@iwr.uni-heidelberg.de>
2012-08-13 20:10:12 +0000

    Printer operators for symmetry information.

---
+++ libtensor/symmetry/print_symmetry.h
@@ -0,0 +1,181 @@
+#ifndef LIBTENSOR_PRINT_SYMMETRY_H
+#define LIBTENSOR_PRINT_SYMMETRY_H
+
+#include <libtensor/core/print_dimensions.h>
+#include <libtensor/symmetry/se_label.h>
+#include <libtensor/symmetry/se_part.h>
+#include <libtensor/symmetry/se_perm.h>
+
+/** \file print_symmetry.h
+
+    Several operators for printing symmetry information.
+ **/
+
+namespace libtensor {
+
+//! \name Declaration of symmetry information print routines
+//@{
+
+template<size_t N, typename T>
+std::ostream &operator<<(std::ostream &os, const symmetry<N, T> &sym);
+
+template<size_t N, typename T>
+std::ostream &operator<<(std::ostream &os, const se_label<N, T> &se);
+
+template<size_t N, typename T>
+std::ostream &operator<<(std::ostream &os, const se_part<N, T> &se);
+
+template<size_t N, typename T>
+std::ostream &operator<<(std::ostream &os, const se_perm<N, T> &se);
+
+template<size_t N, typename T>
+std::ostream &operator<<(std::ostream &os, const block_labeling<N> &bl);
+
+template<size_t N, typename T>
+std::ostream &operator<<(std::ostream &os, const evaluation_rule<N> &er);
+
+template<size_t N, typename T>
+std::ostream &operator<<(std::ostream &os, const product_rule<N> &pr);
+
+template<typename T>
+std::ostream &operator<<(std::ostream &os, const scalar_transf<T> &tr);
+
+
+//@}
+
+
+template<size_t N, typename T>
+std::ostream &operator<<(std::ostream &os, const symmetry<N, T> &sym) {
+
+    typedef se_label<N, T> se1_t;
+    typedef se_part<N, T> se2_t;
+    typedef se_perm<N, T> se3_t;
+    typedef symmetry_element_set_adapter<N, T, se1_t> adapter1_t;
+    typedef symmetry_element_set_adapter<N, T, se2_t> adapter2_t;
+    typedef symmetry_element_set_adapter<N, T, se3_t> adapter3_t;
+
+    size_t i = 1;
+    for (typename symmetry<N, double>::iterator it = sym.begin();
+            it != sym.end(); it++, i++) {
+
+        const symmetry_element_set<N, double> &set = sym.get_subset(it);
+        os << " Set " << std::setw(2) << i << ": ";
+        os << set.get_id() << std::endl;
+
+        if (set.get_id().compare(se1_t::k_sym_type) == 0) {
+            adapter1_t g(set);
+            for (typename adapter1_t::iterator its = g.begin();
+                    its != g.end(); its++) {
+                os << g.get_elem(its);
+            }
+        }
+        else if (set.get_id().compare(se2_t::k_sym_type) == 0) {
+            adapter2_t g(set);
+            for (typename adapter2_t::iterator its = g.begin();
+                    its != g.end(); its++) {
+                os << g.get_elem(its);
+            }
+        }
+        else if (set.get_id().compare(se3_t::k_sym_type) == 0) {
+            adapter3_t g(set);
+            for (typename adapter3_t::iterator its = g.begin();
+                    its != g.end(); its++) {
+                os << g.get_elem(its);
+            }
+        }
+    }
+    return os;
+}
+
+
+template<size_t N, typename T>
+std::ostream &operator<<(std::ostream &os, const se_label<N, T> &se) {
+	
+	os << "Table ID: " << se.get_table_id() << std::endl;
+    os << "Block labels: " << se.get_labeling() << std::endl;
+    os << "Rule: " << std::endl << se.get_rule() << std::endl;
+    return os;
+}
+
+template<size_t N, typename T>
+std::ostream &operator<<(std::ostream &os, const se_part<N, T> &se) {
+	
+	const dimensions<N> &pdims = se.get_pdims();
+	os << "Partition dims: " << pdims << std::endl;
+	os << "Mappings:" << std::endl;
+	abs_index<N> ai(pdims);
+	do {
+	    if (se.is_forbidden(ai.get_index())) {
+	        os << ai.get_index() << " (x)";
+	        continue;
+	    }
+
+		abs_index<N> aix(se.get_direct_map(ai.get_index()), pdims);
+		if (aix.get_abs_index() <= ai.get_abs_index()) continue;
+		
+		os << " " << ai.get_index() << " -> " << aix.get_index() << " ("
+			<< se.get_transf(ai.get_index(), aix.get_index())
+			<< ")" << std::endl;
+	} while (ai.inc());
+
+	return os;
+}
+
+template<size_t N> 
+std::ostream &operator<<(std::ostream &out, const se_perm<N, double> &sp) {
+	
+	out << sp.get_perm() << " " << sp.get_transf() << std::endl;
+	return out;
+}
+
+
+template<size_t N>
+std::ostream &operator<<(std::ostream &os, const block_labeling<N> &bl) {
+
+    for (size_t i = 0; i < N; i++) {
+        size_t itype = bl.get_dim_type(i);
+        os << " [" << i << "(" << itype << "):";
+        for (size_t j = 0; j < bl.get_dim(itype); j++) {
+            os << " " << bl.get_label(itype, j);
+        }
+        os << "]";
+    }
+    return os;
+}
+
+template<size_t N>
+std::ostream &operator<<(std::ostream &os, const evaluation_rule<N> &er) {
+
+    for (typename evaluation_rule<N>::const_iterator it = er.begin();
+            it != er.end(); it++) {
+
+        os << er.get_product(it) << std::endl;
+    }
+    return os;
+}
+
+template<size_t N>
+std::ostream &operator<<(std::ostream &os, const product_rule<N> &pr) {
+
+    for (typename product_rule<N>::iterator it = pr.begin();
+            it != pr.end(); it++) {
+        os << "([";
+        const sequence<N, size_t> &seq = pr.get_sequence(it);
+        for (register size_t j = 0; j < N; j++) os << seq[j];
+        os << "]," << pr.get_intrinsic(it) << ")";
+    }
+    return os;
+}
+
+template<>
+std::ostream &operator<<(std::ostream &os, const scalar_transf<double> &tr) {
+
+    os << tr.get_coeff();
+    return os;
+}
+
+//@}
+
+} // namespace libtensor
+
+#endif // LIBTENSOR_PRINT_SYMMETRY
