3946306b22eba13591a021223a7104ee1afd2d71
Michael Wormit <michael.wormit@iwr.uni-heidelberg.de>
2012-09-19 08:36:31 +0000

    explicit instantiation of gen_bto_dirsum

--- libtensor/block_tensor/impl/btod_dirsum.C
+++ libtensor/block_tensor/impl/btod_dirsum.C
@@ -5,6 +5,23 @@
 namespace libtensor {
 
 
+template class gen_bto_dirsum<1, 1, btod_traits, btod_dirsum<1, 1> >;
+template class gen_bto_dirsum<1, 2, btod_traits, btod_dirsum<1, 2> >;
+template class gen_bto_dirsum<1, 3, btod_traits, btod_dirsum<1, 3> >;
+template class gen_bto_dirsum<1, 4, btod_traits, btod_dirsum<1, 4> >;
+template class gen_bto_dirsum<1, 5, btod_traits, btod_dirsum<1, 5> >;
+template class gen_bto_dirsum<2, 1, btod_traits, btod_dirsum<2, 1> >;
+template class gen_bto_dirsum<2, 2, btod_traits, btod_dirsum<2, 2> >;
+template class gen_bto_dirsum<2, 3, btod_traits, btod_dirsum<2, 3> >;
+template class gen_bto_dirsum<2, 4, btod_traits, btod_dirsum<2, 4> >;
+template class gen_bto_dirsum<3, 1, btod_traits, btod_dirsum<3, 1> >;
+template class gen_bto_dirsum<3, 2, btod_traits, btod_dirsum<3, 2> >;
+template class gen_bto_dirsum<3, 3, btod_traits, btod_dirsum<3, 3> >;
+template class gen_bto_dirsum<4, 1, btod_traits, btod_dirsum<4, 1> >;
+template class gen_bto_dirsum<4, 2, btod_traits, btod_dirsum<4, 2> >;
+template class gen_bto_dirsum<5, 1, btod_traits, btod_dirsum<5, 1> >;
+
+
 template<>
 struct btod_dirsum_clazz<0, 1> {
     static const char *k_clazz;
