6ef11970d287e6c5ee158dfad76354cce5a77e79
Evgeny Epifanovsky <epif@q-chem.com>
2012-09-20 04:49:27 +0000

    generalized block tensor class

--- libtensor/block_tensor/block_tensor.h
+++ libtensor/block_tensor/block_tensor.h
@@ -1,19 +1,15 @@
 #ifndef LIBTENSOR_BLOCK_TENSOR_H
 #define LIBTENSOR_BLOCK_TENSOR_H
 
-#include <libtensor/core/abs_index.h>
-#include <libtensor/core/block_index_space.h>
 #include <libtensor/core/immutable.h>
-#include <libtensor/core/orbit_list.h>
-#include <libtensor/mp/default_sync_policy.h>
-#include <libtensor/dense_tensor/dense_tensor.h>
-#include <libtensor/dense_tensor/dense_tensor_ctrl.h>
-#include <libtensor/block_tensor/block_tensor_i.h>
-#include "../gen_block_tensor/auto_rwlock.h"
-#include "../gen_block_tensor/impl/block_map_impl.h"
+#include <libtensor/core/noncopyable.h>
+#include <libtensor/gen_block_tensor/gen_block_tensor.h>
+#include <libtensor/gen_block_tensor/gen_block_tensor_ctrl.h>
+#include "block_tensor_i.h"
 #include "block_tensor_traits.h"
 
 namespace libtensor {
+
 
 /** \brief Block %tensor
     \tparam N Tensor order.
@@ -23,20 +19,21 @@ namespace libtensor {
     \ingroup libtensor_core
  **/
 template<size_t N, typename T, typename Alloc>
-class block_tensor : public block_tensor_i<N, T>, public immutable {
+class block_tensor :
+    public block_tensor_i<N, T>,
+    public immutable,
+    public noncopyable {
+
 public:
     static const char *k_clazz; //!< Class name
 
 public:
     typedef block_tensor_traits<T, Alloc> bt_traits;
+    typedef typename bt_traits::bti_traits bti_traits;
 
 public:
-    block_index_space<N> m_bis; //!< Block %index space
-    dimensions<N> m_bidims; //!< Block %index %dimensions
-    symmetry<N, T> m_symmetry; //!< Block %tensor symmetry
-    orbit_list<N, T> *m_orblst; //!< Orbit list
-    block_map<N, T, bt_traits> m_map; //!< Block map
-    libutil::rwlock m_lock; //!< Read-write lock
+    gen_block_tensor<N, bt_traits> m_bt;
+    gen_block_tensor_ctrl<N, bti_traits> m_ctrl;
 
 public:
     //!    \name Construction and destruction
@@ -81,11 +78,8 @@ const char *block_tensor<N, T, Alloc>::k_clazz = "block_tensor<N, T, Alloc>";
 
 template<size_t N, typename T, typename Alloc>
 block_tensor<N, T, Alloc>::block_tensor(const block_index_space<N> &bis) :
-    m_bis(bis),
-    m_bidims(bis.get_block_index_dims()),
-    m_symmetry(m_bis),
-    m_orblst(0),
-    m_map(m_bis) {
+
+    m_bt(bis), m_ctrl(m_bt) {
 
 }
 
@@ -93,11 +87,7 @@ block_tensor<N, T, Alloc>::block_tensor(const block_index_space<N> &bis) :
 template<size_t N, typename T, typename Alloc>
 block_tensor<N, T, Alloc>::block_tensor(const block_tensor<N, T, Alloc> &bt) :
 
-    m_bis(bt.get_bis()),
-    m_bidims(bt.m_bidims),
-    m_symmetry(bt.get_bis()),
-    m_orblst(0),
-    m_map(m_bis) {
+    m_bt(bt.get_bis()), m_ctrl(m_bt) {
 
 }
 
@@ -105,42 +95,27 @@ block_tensor<N, T, Alloc>::block_tensor(const block_tensor<N, T, Alloc> &bt) :
 template<size_t N, typename T, typename Alloc>
 block_tensor<N, T, Alloc>::~block_tensor() {
 
-    delete m_orblst;
 }
 
 
 template<size_t N, typename T, typename Alloc>
-const block_index_space<N> &block_tensor<N, T, Alloc>::get_bis()
-    const {
+const block_index_space<N> &block_tensor<N, T, Alloc>::get_bis() const {
 
-    return m_bis;
+    return m_bt.get_bis();
 }
 
 
 template<size_t N, typename T, typename Alloc>
 const symmetry<N, T> &block_tensor<N, T, Alloc>::on_req_const_symmetry() {
 
-    return m_symmetry;
+    return m_ctrl.req_const_symmetry();
 }
 
 
 template<size_t N, typename T, typename Alloc>
 symmetry<N, T> &block_tensor<N, T, Alloc>::on_req_symmetry() {
 
-    static const char *method = "on_req_symmetry()";
-
-    auto_rwlock lock(m_lock);
-
-    if(is_immutable()) {
-        throw immut_violation(g_ns, k_clazz, method, __FILE__, __LINE__,
-            "symmetry");
-    }
-
-    lock.upgrade();
-    delete m_orblst;
-    m_orblst = 0;
-
-    return m_symmetry;
+    return m_ctrl.req_symmetry();
 }
 
 
@@ -148,14 +123,14 @@ template<size_t N, typename T, typename Alloc>
 dense_tensor_i<N, T> &block_tensor<N, T, Alloc>::on_req_const_block(
     const index<N> &idx) {
 
-    return on_req_block(idx);
+    return m_ctrl.req_const_block(idx);
 }
 
 
 template<size_t N, typename T, typename Alloc>
 void block_tensor<N, T, Alloc>::on_ret_const_block(const index<N> &idx) {
 
-    on_ret_block(idx);
+    m_ctrl.ret_const_block(idx);
 }
 
 
@@ -163,26 +138,14 @@ template<size_t N, typename T, typename Alloc>
 dense_tensor_i<N, T> &block_tensor<N, T, Alloc>::on_req_block(
     const index<N> &idx) {
 
-    static const char *method = "on_req_block(const index<N>&)";
-
-    auto_rwlock lock(m_lock);
-
-    update_orblst(lock);
-    if(!m_orblst->contains(idx)) {
-        throw symmetry_violation(g_ns, k_clazz, method, __FILE__, __LINE__,
-            "Index does not correspond to a canonical block.");
-    }
-    if(!m_map.contains(idx)) {
-        lock.upgrade();
-        m_map.create(idx);
-    }
-    return m_map.get(idx);
+    return m_ctrl.req_block(idx);
 }
 
 
 template<size_t N, typename T, typename Alloc>
 void block_tensor<N, T, Alloc>::on_ret_block(const index<N> &idx) {
 
+    m_ctrl.ret_block(idx);
 }
 
 
@@ -190,77 +153,28 @@ template<size_t N, typename T, typename Alloc>
 bool block_tensor<N, T, Alloc>::on_req_is_zero_block(
     const index<N> &idx) {
 
-    static const char *method = "on_req_is_zero_block(const index<N>&)";
-
-    auto_rwlock lock(m_lock);
-
-    update_orblst(lock);
-    if(!m_orblst->contains(idx)) {
-        throw symmetry_violation(g_ns, k_clazz, method,
-            __FILE__, __LINE__,
-            "Index does not correspond to a canonical block.");
-    }
-    return !m_map.contains(idx);
+    return m_ctrl.req_is_zero_block(idx);
 }
 
 
 template<size_t N, typename T, typename Alloc>
 void block_tensor<N, T, Alloc>::on_req_zero_block(const index<N> &idx) {
 
-    static const char *method = "on_req_zero_block(const index<N>&)";
-
-    auto_rwlock lock(m_lock);
-
-    if(is_immutable()) {
-        throw immut_violation(g_ns, k_clazz, method, __FILE__, __LINE__,
-            "Immutable object cannot be modified.");
-    }
-    update_orblst(lock);
-    if(!m_orblst->contains(idx)) {
-        throw symmetry_violation(g_ns, k_clazz, method, __FILE__, __LINE__,
-            "Index does not correspond to a canonical block.");
-    }
-    lock.upgrade();
-    m_map.remove(idx);
+    m_ctrl.req_zero_block(idx);
 }
 
 
 template<size_t N, typename T, typename Alloc>
 void block_tensor<N, T, Alloc>::on_req_zero_all_blocks() {
 
-    static const char *method = "on_req_zero_all_blocks()";
-
-    auto_rwlock lock(m_lock);
-
-    if(is_immutable()) {
-        throw immut_violation(g_ns, k_clazz, method, __FILE__, __LINE__,
-            "Immutable object cannot be modified.");
-    }
-    lock.upgrade();
-    m_map.clear();
+    m_ctrl.req_zero_all_blocks();
 }
 
 
 template<size_t N, typename T, typename Alloc>
-inline void block_tensor<N, T, Alloc>::on_set_immutable() {
+void block_tensor<N, T, Alloc>::on_set_immutable() {
 
-    auto_rwlock lock(m_lock);
-
-    lock.upgrade();
-    m_map.set_immutable();
-}
-
-
-template<size_t N, typename T, typename Alloc>
-void block_tensor<N, T, Alloc>::update_orblst(auto_rwlock &lock) {
-
-    if(m_orblst == 0) {
-        lock.upgrade();
-        //  We may have waited long enough here that the orbit has been updated
-        //  in another thread already. Need to double check.
-        if(m_orblst == 0) m_orblst = new orbit_list<N, T>(m_symmetry);
-        lock.downgrade();
-    }
+    m_bt.set_immutable();
 }
 
 
