251b234e68d5dfe13a8616a3d2458006cff03152
Michael Wormit <michael.wormit@iwr.uni-heidelberg.de>
2012-10-02 11:04:07 +0000

    btod_random generalized

---
+++ libtensor/gen_block_tensor/gen_bto_random.h
@@ -0,0 +1,74 @@
+#ifndef LIBTENSOR_GEN_BTO_RANDOM_H
+#define LIBTENSOR_GEN_BTO_RANDOM_H
+
+#include <list>
+#include <map>
+#include <libtensor/defs.h>
+#include <libtensor/timings.h>
+#include <libtensor/exception.h>
+#include <libtensor/core/dimensions.h>
+#include "gen_block_tensor_i.h"
+#include "gen_block_tensor_ctrl.h"
+
+namespace libtensor {
+
+
+/** \brief Fills a block %tensor with random data without affecting its
+        %symmetry
+    \tparam T Block %tensor order.
+
+    \ingroup libtensor_btod
+ **/
+template<size_t N, typename Traits, typename Timed>
+class gen_bto_random : public timings<Timed>, public noncopyable {
+public:
+    //! Type of tensor elements
+    typedef typename Traits::element_type element_type;
+
+    //! Block tensor interface traits
+    typedef typename Traits::bti_traits bti_traits;
+
+    //! Type of read-only block
+    typedef typename bti_traits::template rd_block_type<N>::type rd_block_type;
+
+    //! Type of write-only block
+    typedef typename bti_traits::template wr_block_type<N>::type wr_block_type;
+
+    //! Type of tensor transformation
+    typedef tensor_transf<N, element_type> tensor_transf_type;
+
+private:
+    typedef std::list<tensor_transf_type> transf_list_t;
+    typedef std::map<size_t, transf_list_t> transf_map_t;
+
+public:
+    static const char *k_clazz; //!< Class name
+
+public:
+    /** \brief Fills a block %tensor with random values preserving
+            symmetry
+        \param bt Block %tensor.
+     **/
+    void perform(gen_block_tensor_wr_i<N, bti_traits> &bt) throw(exception);
+
+    /** \brief Fills one block of a block %tensor with random values
+            preserving symmetry
+        \param bt Block %tensor.
+        \param idx Block %index in the block %tensor.
+     **/
+    void perform(gen_block_tensor_wr_i<N, bti_traits> &bt,
+            const index<N> &idx) throw(exception);
+
+private:
+    bool make_transf_map(const symmetry<N, element_type> &sym,
+        const dimensions<N> &bidims, const index<N> &idx,
+        const tensor_transf_type &tr, transf_map_t &alltransf);
+
+    void make_random_blk(gen_block_tensor_wr_ctrl<N, bti_traits> &ctrl,
+        const dimensions<N> &bidims, const index<N> &idx);
+};
+
+
+} // namespace libtensor
+
+#endif // LIBTENSOR_GEN_BTO_RANDOM_H
---
+++ libtensor/gen_block_tensor/impl/gen_bto_random_impl.h
@@ -0,0 +1,188 @@
+#ifndef LIBTENSOR_GEN_BTO_RANDOM_IMPL_H
+#define LIBTENSOR_GEN_BTO_RANDOM_IMPL_H
+
+#include <utility>
+#include <libtensor/core/abs_index.h>
+
+namespace libtensor {
+
+
+template<size_t N, typename Traits, typename Timed>
+const char *gen_bto_random<N, Traits, Timed>::k_clazz =
+        "gen_bto_random<N, Traits, Timed>";
+
+
+template<size_t N, typename Traits, typename Timed>
+void gen_bto_random<N, Traits, Timed>::perform(
+        gen_block_tensor_wr_i<N, bti_traits> &bt) throw(exception) {
+
+    gen_bto_random::start_timer();
+
+    dimensions<N> bidims(bt.get_bis().get_block_index_dims());
+    gen_block_tensor_wr_ctrl<N, bti_traits> ctrl(bt);
+
+    orbit_list<N, element_type> orblist(ctrl.req_symmetry());
+    typename orbit_list<N, element_type>::iterator iorbit = orblist.begin();
+    for(; iorbit != orblist.end(); iorbit++) {
+        make_random_blk(ctrl, bidims, orblist.get_index(iorbit));
+    }
+
+    gen_bto_random::stop_timer();
+}
+
+template<size_t N, typename Traits, typename Timed>
+void gen_bto_random<N, Traits, Timed>::perform(
+        gen_block_tensor_wr_i<N, bti_traits> &bt, const index<N> &idx)
+        throw(exception) {
+
+    gen_bto_random::start_timer();
+
+    dimensions<N> bidims(bt.get_bis().get_block_index_dims());
+    gen_block_tensor_wr_ctrl<N, bti_traits> ctrl(bt);
+    make_random_blk(ctrl, bidims, idx);
+
+    gen_bto_random::stop_timer();
+}
+
+
+template<size_t N, typename Traits, typename Timed>
+bool gen_bto_random<N, Traits, Timed>::make_transf_map(
+        const symmetry<N, element_type> &sym, const dimensions<N> &bidims,
+        const index<N> &idx, const tensor_transf_type &tr,
+        transf_map_t &alltransf) {
+
+    size_t absidx = abs_index<N>::get_abs_index(idx, bidims);
+    typename transf_map_t::iterator ilst = alltransf.find(absidx);
+    if(ilst == alltransf.end()) {
+        ilst = alltransf.insert(std::pair<size_t, transf_list_t>(
+            absidx, transf_list_t())).first;
+    }
+    typename transf_list_t::iterator itr = ilst->second.begin();
+    bool done = false;
+    for(; itr != ilst->second.end(); itr++) {
+        if(*itr == tr) {
+            done = true;
+            break;
+        }
+    }
+    if(done) return true;
+    ilst->second.push_back(tr);
+
+    bool allowed = true;
+    for(typename symmetry<N, element_type>::iterator iset = sym.begin();
+            iset != sym.end(); iset++) {
+
+        const symmetry_element_set<N, element_type> &eset =
+                sym.get_subset(iset);
+        for(typename symmetry_element_set<N, element_type>::const_iterator
+                ielem = eset.begin(); ielem != eset.end(); ielem++) {
+
+            const symmetry_element_i<N, element_type> &elem =
+                eset.get_elem(ielem);
+            index<N> idx2(idx);
+            tensor_transf_type tr2(tr);
+            if(elem.is_allowed(idx2)) {
+                elem.apply(idx2, tr2);
+                allowed = make_transf_map(sym, bidims,
+                    idx2, tr2, alltransf);
+            } else {
+                allowed = false;
+            }
+        }
+    }
+    return allowed;
+}
+
+
+template<size_t N, typename Traits, typename Timed>
+void gen_bto_random<N, Traits, Timed>::make_random_blk(
+        gen_block_tensor_wr_ctrl<N, bti_traits> &ctrl,
+        const dimensions<N> &bidims, const index<N> &idx) {
+
+    typedef typename Traits::template to_random_type<N>::type to_random_type;
+    typedef typename Traits::template temp_block_tensor_type<N>::type
+            temp_block_tensor_type;
+
+    const symmetry<N, element_type> &sym = ctrl.req_const_symmetry();
+    size_t absidx = abs_index<N>::get_abs_index(idx, bidims);
+
+    to_random_type randop;
+
+    tensor_transf_type tr0;
+    transf_map_t transf_map;
+
+    bool allowed = make_transf_map(sym, bidims, idx, tr0, transf_map);
+    typename transf_map_t::iterator ilst = transf_map.find(absidx);
+    if(!allowed || ilst == transf_map.end()) {
+        ctrl.req_zero_block(idx);
+        return;
+    }
+
+
+    typename transf_list_t::iterator itr = ilst->second.begin();
+    if(itr == ilst->second.end()) {
+        wr_block_type &blk = ctrl.req_block(idx);
+        gen_bto_random::start_timer("randop");
+        randop.perform(true, blk);
+        gen_bto_random::stop_timer("randop");
+        ctrl.ret_block(idx);
+    } else {
+        temp_block_tensor_type btrnd(sym.get_bis()), btsymrnd;
+        gen_block_tensor_ctrl<N, bti_traits> crnd(btrnd), csymrnd(btsymrnd);
+
+        {
+        wr_block_type &rnd = crnd.req_block(idx);
+
+        gen_bto_random::start_timer("randop");
+        randop.perform(true, rnd);
+        gen_bto_random::stop_timer("randop");
+
+        crnd.ret_block(idx);
+        }
+
+        scalar_transf_sum<element_type> tottr;
+        {
+        rd_block_type &rnd = crnd.req_const_block();
+        wr_block_type &symrnd = csymrnd.req_block();
+
+        tottr.add(itr->get_scalar_tr());
+        to_add symop(rnd, *itr);
+
+        for(itr++; itr != ilst->second.end(); itr++) {
+            symop.add_op(rnd, *itr);
+            tottr.add(itr->get_scalar_tr());
+        }
+        gen_bto_random::start_timer("symop");
+        symop.perform(true, symrnd);
+        gen_bto_random::stop_timer("symop");
+
+        crnd.ret_const_block(idx);
+        csymrnd.ret_block(idx);
+        }
+        crnd.req_zero_block(idx);
+
+        {
+        rd_block_type &symrnd = csymrnd.req_const_block(idx);
+        wr_block_type &blk = ctrl.req_block(idx);
+
+        scalar_transf<element_type> tr(tottr.get_transf());
+        if (tr.is_zero()) tr = scalar_transf<element_type>();
+        else tr.invert();
+
+        gen_bto_random::start_timer("copy");
+        to_copy(symrnd, tr).perform(true, blk);
+        gen_bto_random::stop_timer("copy");
+
+        csymrnd.ret_const_block(idx);
+        ctrl.ret_block(idx);
+        }
+
+        csymrnd.req_zero_block(idx);
+    }
+
+}
+
+
+} // namespace libtensor
+
+#endif // LIBTENSOR_BTOD_RANDOM_H
