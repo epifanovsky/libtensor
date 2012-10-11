9465c9d6d93c0d61398cf65edba0b9ea03238e0b
Michael Wormit <michael.wormit@iwr.uni-heidelberg.de>
2012-10-11 19:26:14 +0000

    New kernels kern_apply and kern_applyadd for tod_apply

--- libtensor/dense_tensor/impl/tod_apply_impl.h
+++ libtensor/dense_tensor/impl/tod_apply_impl.h
@@ -1,60 +1,14 @@
 #ifndef LIBTENSOR_TOD_APPLY_IMPL_H
 #define LIBTENSOR_TOD_APPLY_IMPL_H
 
-#include <libtensor/kernels/kernel_base.h>
+#include <libtensor/kernels/kern_apply.h>
+#include <libtensor/kernels/kern_applyadd.h>
 #include <libtensor/kernels/loop_list_runner.h>
-#include <libtensor/linalg/linalg.h>
 #include <libtensor/tod/bad_dimensions.h>
 #include "../dense_tensor_ctrl.h"
 #include "../tod_apply.h"
 
 namespace libtensor {
-
-
-template<typename Functor>
-class kern_apply : public kernel_base<linalg, 1, 1> {
-public:
-    static const char *k_clazz; //!< Kernel name
-
-private:
-    Functor *m_fn; //!< Functor
-    double m_c1, m_c2;
-
-public:
-    virtual ~kern_apply() { }
-
-    virtual const char *get_name() const {
-        return k_clazz;
-    }
-
-    virtual void run(void *, const loop_registers<1, 1> &r);
-
-    static kernel_base<linalg, 1, 1> *match(Functor &fn,
-            double c1, double c2, list_t &in, list_t &out);
-};
-
-
-template<typename Functor>
-class kern_applyadd : public kernel_base<linalg, 1, 1> {
-public:
-    static const char *k_clazz; //!< Kernel name
-
-private:
-    Functor *m_fn; //!< Functor
-    double m_c1, m_c2;
-
-public:
-    virtual ~kern_applyadd() { }
-
-    virtual const char *get_name() const {
-        return k_clazz;
-    }
-
-    virtual void run(void *, const loop_registers<1, 1> &r);
-
-    static kernel_base<linalg, 1, 1> *match(Functor &fn,
-            double c1, double c2, list_t &in, list_t &out);
-};
 
 
 template<size_t N, typename Functor>
@@ -178,57 +132,6 @@ dimensions<N> tod_apply<N, Functor>::mk_dimsb(dense_tensor_rd_i<N, double> &ta,
     return dims;
 }
 
-
-template<typename Functor>
-const char *kern_apply<Functor>::k_clazz = "kern_apply";
-
-
-template<typename Functor>
-void kern_apply<Functor>::run(void *, const loop_registers<1, 1> &r) {
-
-    r.m_ptrb[0][0] = m_c2 * (*m_fn)(m_c1 * r.m_ptra[0][0]);
-}
-
-
-template<typename Functor>
-kernel_base<linalg, 1, 1> *kern_apply<Functor>::match(Functor &fn,
-        double c1, double c2, list_t &in, list_t &out) {
-
-    kernel_base<linalg, 1, 1> *kern = 0;
-
-    kern_apply zz;
-    zz.m_fn = &fn;
-    zz.m_c1 = c1;
-    zz.m_c2 = c2;
-
-    return new kern_apply(zz);
-}
-
-
-template<typename Functor>
-const char *kern_applyadd<Functor>::k_clazz = "kern_apply";
-
-
-template<typename Functor>
-void kern_applyadd<Functor>::run(void *, const loop_registers<1, 1> &r) {
-
-    r.m_ptrb[0][0] = r.m_ptrb[0][0] + m_c2 * (*m_fn)(m_c1 * r.m_ptra[0][0]);
-}
-
-
-template<typename Functor>
-kernel_base<linalg, 1, 1> *kern_applyadd<Functor>::match(Functor &fn,
-        double c1, double c2, list_t &in, list_t &out) {
-
-    kernel_base<linalg, 1, 1> *kern = 0;
-
-    kern_applyadd zz;
-    zz.m_fn = &fn;
-    zz.m_c1 = c1;
-    zz.m_c2 = c2;
-
-    return new kern_applyadd(zz);
-}
 
 } // namespace libtensor
 
