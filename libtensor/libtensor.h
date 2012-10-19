77dd386ce8f4218a048646a82b0834d198827052
Michael Wormit <michael.wormit@iwr.uni-heidelberg.de>
2012-10-19 21:23:29 +0000

    Remove obsolete class processor

--- libtensor/libtensor.h
+++ libtensor/libtensor.h
@@ -19,6 +19,7 @@
 #include "dense_tensor/dense_tensor_ctrl.h"
 #include "dense_tensor/dense_tensor.h"
 
+#include "core/contraction2.h"
 #include "core/scalar_transf.h"
 #include "core/scalar_transf_double.h"
 #include "core/tensor_transf.h"
@@ -28,22 +29,15 @@
 #include "core/symmetry_element_i.h"
 #include "core/symmetry.h"
 
-
-#include "tod/contraction2.h"
-#include "tod/processor.h"
-
-
 #include "btod/btod_import_raw.h"
 #include "btod/btod_print.h"
 #include "btod/btod_read.h"
-
 
 #include "symmetry/point_group_table.h"
 #include "symmetry/product_table_container.h"
 #include "symmetry/se_label.h"
 #include "symmetry/se_part.h"
 #include "symmetry/se_perm.h"
-
 
 #include "iface/bispace.h"
 #include "iface/btensor_i.h"
