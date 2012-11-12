05ad4c9931c56846caaf9f69383f2d0118e2cfe7
Evgeny Epifanovsky <epif@q-chem.com>
2012-11-12 01:19:07 +0000

    new algorithm: subgroup_orbits

---
+++ libtensor/core/impl/subgroup_orbits.C
@@ -0,0 +1,16 @@
+#include "subgroup_orbits_impl.h"
+
+namespace libtensor {
+
+
+template class subgroup_orbits<1, double>;
+template class subgroup_orbits<2, double>;
+template class subgroup_orbits<3, double>;
+template class subgroup_orbits<4, double>;
+template class subgroup_orbits<5, double>;
+template class subgroup_orbits<6, double>;
+template class subgroup_orbits<7, double>;
+template class subgroup_orbits<8, double>;
+
+
+} // namespace libtensor
