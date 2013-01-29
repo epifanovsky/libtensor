7ee7c535e15f22e00042288c340d21d537076fa0
Kirill Khistyaev <kirhist@gmail.com>
2013-01-29 21:07:49 +0000

    additional tests in cuda_btod_contract

--- tests/cuda_block_tensor/cuda_btod_contract2_test.h
+++ tests/cuda_block_tensor/cuda_btod_contract2_test.h
@@ -2,6 +2,8 @@
 #define LIBTENSOR_CUDA_BTOD_CONTRACT2_TEST_H
 
 #include <libtest/unit_test.h>
+#include <libtensor/core/allocator.h>
+#include <libvmm/cuda_allocator.h>
 
 namespace libtensor {
 
@@ -11,6 +13,10 @@ namespace libtensor {
     \ingroup libtensor_tests_cuda_btod
 **/
 class cuda_btod_contract2_test : public libtest::unit_test {
+
+	 typedef std_allocator<double> allocator_t;
+	 typedef libvmm::cuda_allocator<double> cuda_allocator_t;
+
 public:
     virtual void perform() throw(libtest::test_exception);
 
@@ -28,7 +34,7 @@ private:
 //    void test_zeroblk_5() throw(libtest::test_exception);
 //    void test_zeroblk_6() throw(libtest::test_exception);
 //
-//    void test_contr_1() throw(libtest::test_exception);
+    void test_contr_1() throw(libtest::test_exception);
 //    void test_contr_2() throw(libtest::test_exception);
 //    void test_contr_3() throw(libtest::test_exception);
 //    void test_contr_4() throw(libtest::test_exception);
@@ -41,11 +47,11 @@ private:
 //    void test_contr_11() throw(libtest::test_exception);
 //    void test_contr_12() throw(libtest::test_exception);
 //    void test_contr_13() throw(libtest::test_exception);
-//    void test_contr_14(double c) throw(libtest::test_exception);
+    void test_contr_14(double c) throw(libtest::test_exception);
 //    void test_contr_15(double c) throw(libtest::test_exception);
 //    void test_contr_16(double c) throw(libtest::test_exception);
 //    void test_contr_17(double c) throw(libtest::test_exception);
-//    void test_contr_18(double c) throw(libtest::test_exception);
+    void test_contr_18(double c) throw(libtest::test_exception);
 //    void test_contr_19() throw(libtest::test_exception);
 //    void test_contr_20a() throw(libtest::test_exception);
 //    void test_contr_20b() throw(libtest::test_exception);
@@ -56,11 +62,11 @@ private:
 //    void test_contr_25();
 //    void test_contr_26();
 //
-//    void test_self_1();
+    void test_self_1();
 //    void test_self_2();
 //    void test_self_3();
 //
-//    void test_batch_1();
+    void test_batch_1();
 //    void test_batch_2();
 //    void test_batch_3();
 
