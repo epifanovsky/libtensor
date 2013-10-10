a8ba9c39eb89d661c037e453b452ecf2d73e7773
Sam Manzer <samuel.manzer1990@gmail.com>
2013-10-10 21:09:35 +0000

    block_loop now formulated as iterative with friend functions for less verbose use

--- libtensor/block_sparse/sparse_btensor.h
+++ libtensor/block_sparse/sparse_btensor.h
@@ -12,67 +12,17 @@
 
 namespace libtensor {
 
-//forward declaration for labeled_sparse_btensor
-template<size_t N,typename T = double>
-class sparse_btensor; 
 
-#if 0
-//TODO: Make templated labeledXXX classs to unify tensor types
-//For labeling general tensors
-template<size_t N,typename T = double> 
-class labeled_sparse_btensor
-{
-private:
-    sparse_btensor<N,T>& m_tensor; 
-    letter_expr<N> m_le;
-public:
-
-    labeled_sparse_btensor(spare_btensor<N,T>& tensor,letter_expr<N>& le) : m_tensor(tensor),m_le(le) { };
-
-    //TODO - could template this later to allow assignment across tensor formats
-    //labeled_sparse_btensor<N,T>& operator=(const labeled_sparse_btensor<N,T>& rhs);
-};
-
-
-template<size_t N,typename T> 
-labeled_sparse_btensor<N,T>& labeled_sparse_btensor<N,T>::operator=(const labeled_sparse_btensor<N,T>& rhs)
-{
-    //Create the outer loop for this operation
-    letter a = m_le[0];
-    sequence<1,size_t> output_bispace_indices()
-    block_loop<1,1> outer(sequence<1,size_t>(),sequence<1,size_t>(0),sequence<1,bool>(),sequence<1,bool>(false));
-
-    //Determine the permutation of indices between the two tensors
-    //Permutation is defined as acting on the RHS tensor to produce the THIS tensor
-    permute_map pm;
-    for(size_t i = 0; i < N; ++i)
-    {
-        letter a = m_le[i];
-        size_t rhs_idx = rhs.m_le.index_of(a);
-        if(rhs_idx != i)
-        {
-            pm.insert(std::make_pair(rhs_idx,i));
-        }
-
-        //Populate the loop for this index
-        output_in
-        for(size_t i = 1; i < N; ++i)
-        {
-            outer.nest(sequence<0,size_t>(),sequence<1,size_t>(i),sequence<0,bool>(),sequence<1,bool>(false));
-        }
-    }
-
-    block_permute_kernel<T> bpk(pm);
-}
-#endif
-
+//Forward declaration for operator()(...) 
+template<size_t N,typename T>
+class labeled_sparse_btensor;
 
 template<size_t N,typename T>
 class sparse_btensor {
 public:
     static const char *k_clazz; //!< Class name
 private:
-    T* m_data;
+    T* m_data_ptr;
     sparse_bispace<N> m_bispace;
 public:
     /** \brief Constructs a sparse block tensor object and populates it with the entries from mem if specified
@@ -89,6 +39,13 @@ public:
      **/
     bool operator==(const sparse_btensor<N,T>& rhs) const;
     bool operator!=(const sparse_btensor<N,T>& rhs) const;
+
+    const T* get_data_ptr() const { return m_data_ptr; }
+    
+    /** \brief Returns a labeled_sparse_btensor object for use in expressions
+     *
+     */
+    labeled_btensor<N,T> operator()(letter_expr<N>& le) const;
 
     /** \brief Returns a string representation of the tensor in row-major order 
      **/
@@ -109,7 +66,7 @@ sparse_btensor<N,T>::sparse_btensor(const sparse_bispace<N>& the_bispace,T* mem,
     }
 
     //Alloc storage
-    m_data = new T[size];
+    m_data_ptr = new T[size];
 
     //Create loops
 
@@ -117,7 +74,7 @@ sparse_btensor<N,T>::sparse_btensor(const sparse_bispace<N>& the_bispace,T* mem,
     {
         if(already_block_major)
         {
-            memcpy(m_data,mem,the_bispace.get_nnz()*sizeof(T));
+            memcpy(m_data_ptr,mem,the_bispace.get_nnz()*sizeof(T));
         }
         else
         {
@@ -130,7 +87,7 @@ sparse_btensor<N,T>::sparse_btensor(const sparse_bispace<N>& the_bispace,T* mem,
             //TODO: Make all input sequences for bispaces use const, or some other way of eliminating awkward const
             //from this method being const
             block_load_kernel<T> blk(m_bispace,mem);
-            run_loop_list(loop_list,blk,sequence<1,T*>(m_data),
+            run_loop_list(loop_list,blk,sequence<1,T*>(m_data_ptr),
                           sequence<0,T*>(),
                           sequence<1,sparse_bispace_generic_i*>((sparse_bispace<2>*)&m_bispace),
                           sequence<0,sparse_bispace_generic_i*>());
@@ -141,7 +98,7 @@ sparse_btensor<N,T>::sparse_btensor(const sparse_bispace<N>& the_bispace,T* mem,
 template<size_t N,typename T>
 sparse_btensor<N,T>::~sparse_btensor()
 {
-    delete [] m_data;
+    delete [] m_data_ptr;
 }
 
 template<size_t N,typename T>
@@ -163,7 +120,7 @@ bool sparse_btensor<N,T>::operator==(const sparse_btensor<N,T>& rhs) const
 
     for(size_t i = 0; i < m_bispace.get_nnz(); ++i)
     {
-        if(m_data[i] != rhs.m_data[i])
+        if(m_data_ptr[i] != rhs.m_data_ptr[i])
         {
             return false;
         }
@@ -177,6 +134,12 @@ bool sparse_btensor<N,T>::operator!=(const sparse_btensor<N,T>& rhs) const
     return !(*this == rhs);
 }
 
+
+template<size_t N,typename T>
+labeled_btensor<N,T> sparse_btensor<N,T>::operator()(letter_expr<N>& le) const
+{
+    return labeled_btensor<N,T>(*this,le);
+}
 template<size_t N,typename T>
 std::string sparse_btensor<N,T>::str() const
 {
@@ -193,7 +156,7 @@ std::string sparse_btensor<N,T>::str() const
     block_printer<T> bp;
     run_loop_list(loop_list,bp,
                   sequence<0,T*>(),
-                  sequence<1,T*>(m_data),
+                  sequence<1,T*>(m_data_ptr),
                   sequence<0,sparse_bispace_generic_i*>(NULL),
                   sequence<1,sparse_bispace_generic_i*>((sparse_bispace<2>*)&m_bispace));
     return bp.str();
