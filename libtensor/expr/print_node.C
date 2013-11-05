f4f5c1a17b790502c2eb0235018fa18b002bdb83
Evgeny Epifanovsky <epif@q-chem.com>
2013-11-05 15:19:28 +0000

    printing of node_transf

--- libtensor/expr/print_node.C
+++ libtensor/expr/print_node.C
@@ -1,6 +1,8 @@
 #include <string>
+#include <libtensor/core/scalar_transf_double.h>
 #include "node_assign.h"
 #include "node_ident.h"
+#include "node_transform.h"
 #include "unary_node_base.h"
 #include "nary_node_base.h"
 #include "print_node.h"
@@ -25,6 +27,23 @@ void print_node(const node &n, std::ostream &os, size_t indent) {
                 << std::endl;
             print_node(na->get_rhs(), os, indent + 2);
         } else if(n1) {
+            if(n1->get_op().compare("transform") == 0) {
+                const node_transform_base *ntr0 =
+                    dynamic_cast<const node_transform_base*>(n1);
+                if(ntr0) {
+                    const std::vector<size_t> &p = ntr0->get_perm();
+                    os << "  [";
+                    for(size_t i = 0; i + 1 < p.size(); i++) os << p[i] << ",";
+                    if(p.size() > 0) os << p[p.size() - 1];
+                    else os << "*";
+                    os << "]";
+                    const node_transform<double> *ntrd =
+                        dynamic_cast< const node_transform<double>* >(ntr0);
+                    if(ntrd) {
+                        os << " " << ntrd->get_coeff().get_coeff();
+                    }
+                }
+            }
             os << std::endl;
             print_node(n1->get_arg(), os, indent + 2);
         } else if(nn) {
