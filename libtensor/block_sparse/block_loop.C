32be8b93cc8efd6112adf2c4a003ffb8b4e161ae
Sam Manzer <samuel.manzer1990@gmail.com>
2013-12-03 16:16:28 +0000

    Refactored block_loop to not use std::map to reduce overhead

--- libtensor/block_sparse/block_loop.C
+++ libtensor/block_sparse/block_loop.C
@@ -16,12 +16,20 @@ bool count_flops = false;
 const char* block_loop::k_clazz = "block_loop";
 
 block_loop::block_loop(const std::vector< sparse_bispace_any_order >& bispaces) : m_bispaces(bispaces)
+    
 {
 	if(bispaces.size() == 0)
 	{
 		throw bad_parameter(g_ns, k_clazz,"block_loop_new(...)",
 				__FILE__, __LINE__, "loop must access at least one bispace");
 	}
+    size_t n_bispaces = m_bispaces.size();
+    m_subspaces_looped.resize(n_bispaces);
+    m_bispaces_ignored.resize(n_bispaces);
+    for(size_t bispace_idx = 0; bispace_idx  < n_bispaces; ++bispace_idx)
+    {
+        m_bispaces_ignored[bispace_idx] = true;
+    }
 }
 
 void block_loop::set_subspace_looped(size_t bispace_idx, size_t subspace_idx)
@@ -39,20 +47,24 @@ void block_loop::set_subspace_looped(size_t bispace_idx, size_t subspace_idx)
 	}
 
 	//Verify that all of the subspaces accessed by this loop are identical
-	if(m_subspace_map.size() > 0)
+    const sparse_bispace_any_order& cur_subspace = m_bispaces[bispace_idx][subspace_idx];
+    for(size_t ref_bispace_idx = 0; ref_bispace_idx < m_bispaces.size(); ++ref_bispace_idx)
 	{
-		std::map<size_t,size_t>::iterator sp_it = m_subspace_map.begin();
-		const sparse_bispace_any_order& ref_bispace = m_bispaces[sp_it->first][sp_it->second];
-		const sparse_bispace_any_order& cur_bispace = m_bispaces[bispace_idx][subspace_idx];
-		if(ref_bispace != cur_bispace)
-		{
-			throw bad_parameter(g_ns, k_clazz,"set_subspace_looped(...)",
-					__FILE__, __LINE__, "subspaces specified are incompatible");
-		}
+        if(!m_bispaces_ignored[ref_bispace_idx] && ref_bispace_idx != bispace_idx)
+        {
+            size_t ref_subspace_idx = m_subspaces_looped[ref_bispace_idx];
+            const sparse_bispace_any_order& ref_subspace = m_bispaces[ref_bispace_idx][ref_subspace_idx];
+            if(ref_subspace != cur_subspace)
+            {
+                throw bad_parameter(g_ns, k_clazz,"set_subspace_looped(...)",
+                                    __FILE__, __LINE__, "subspaces specified are incompatible");
+            }
+        }
 	}
 
 	//Finally, record that the loop touches the specified subspace of the given bispace
-	m_subspace_map[bispace_idx] = subspace_idx;
+	m_subspaces_looped[bispace_idx] = subspace_idx;
+    m_bispaces_ignored[bispace_idx] = false;
 }
 
 size_t block_loop::get_subspace_looped(size_t bispace_idx) const
@@ -63,14 +75,14 @@ size_t block_loop::get_subspace_looped(size_t bispace_idx) const
 		throw out_of_bounds(g_ns, k_clazz,"get_subspace_looped(...)",
 				__FILE__, __LINE__, "bispace_idx is out of bounds");
 	}
-	else if(m_subspace_map.find(bispace_idx) == m_subspace_map.end())
+	else if(m_bispaces_ignored[bispace_idx] == true)
 	{
 		throw bad_parameter(g_ns, k_clazz,"get_subspace_looped(...)",
 				__FILE__, __LINE__, "bispace_idx is not looped over");
 	}
 #endif
 
-	return m_subspace_map.at(bispace_idx);
+	return m_subspaces_looped[bispace_idx];
 }
 
 bool block_loop::is_bispace_ignored(size_t bispace_idx) const
@@ -83,7 +95,7 @@ bool block_loop::is_bispace_ignored(size_t bispace_idx) const
 	}
 #endif
 
-	return m_subspace_map.find(bispace_idx) == m_subspace_map.end();
+	return m_bispaces_ignored[bispace_idx];
 }
 
 } /* namespace libtensor */
--- libtensor/block_sparse/block_loop.h
+++ libtensor/block_sparse/block_loop.h
@@ -19,7 +19,8 @@ class block_loop
 {
 private:
     static const char* k_clazz; //!< Class name
-    std::map<size_t,size_t> m_subspace_map; ///!< What subspaces of what bispaces does this loop span?
+    std::vector<size_t> m_subspaces_looped; //!< What subspaces of what bispaces does this loop span?
+    std::vector<bool> m_bispaces_ignored; //!< What bispaces are completely ignored by this loop?
     std::vector< sparse_bispace_any_order > m_bispaces; //!< What bispaces does this loop access
 public:
 	block_loop(const std::vector< sparse_bispace_any_order >& bispaces);
