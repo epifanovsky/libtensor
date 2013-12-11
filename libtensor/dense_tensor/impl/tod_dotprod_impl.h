9be93e7f1d0607706eef9d489f14887a2126eef9
Evgeny Epifanovsky <epif@q-chem.com>
2013-12-11 16:33:48 +0000

    group indices in tod_dotprod

--- libtensor/dense_tensor/impl/tod_dotprod_impl.h
+++ libtensor/dense_tensor/impl/tod_dotprod_impl.h
@@ -13,7 +13,7 @@ namespace libtensor {
 
 
 template<size_t N>
-const char *tod_dotprod<N>::k_clazz = "tod_dotprod<N>";
+const char tod_dotprod<N>::k_clazz[] = "tod_dotprod<N>";
 
 
 template<size_t N>
@@ -22,7 +22,7 @@ tod_dotprod<N>::tod_dotprod(dense_tensor_rd_i<N, double> &ta,
 
     m_ta(ta), m_tb(tb), m_c(1.0) {
 
-    static const char *method = "tod_dotprod(dense_tensor_rd_i<N, double>&, "
+    static const char method[] = "tod_dotprod(dense_tensor_rd_i<N, double>&, "
         "dense_tensor_rd_i<N, double>&)";
 
     if(!verify_dims()) {
@@ -39,7 +39,7 @@ tod_dotprod<N>::tod_dotprod(dense_tensor_rd_i<N, double> &ta,
 
     m_ta(ta), m_tb(tb), m_perma(perma), m_permb(permb), m_c(1.0) {
 
-    static const char *method = "tod_dotprod(dense_tensor_rd_i<N, double>&, "
+    static const char method[] = "tod_dotprod(dense_tensor_rd_i<N, double>&, "
         "const permutation<N>&, dense_tensor_rd_i<N, double>&, "
         "const permutation<N>&)";
 
@@ -60,7 +60,7 @@ tod_dotprod<N>::tod_dotprod(
     m_ta(ta), m_tb(tb), m_perma(tra.get_perm()), m_permb(trb.get_perm()),
     m_c(tra.get_scalar_tr().get_coeff() * trb.get_scalar_tr().get_coeff()){
 
-    static const char *method = "tod_dotprod(dense_tensor_rd_i<N, double>&, "
+    static const char method[] = "tod_dotprod(dense_tensor_rd_i<N, double>&, "
         "const tensor_transf<N, double>&, dense_tensor_rd_i<N, double>&, "
         "const tensor_transf<N, double>&)";
 
@@ -92,10 +92,12 @@ double tod_dotprod<N>::calculate() {
         ca.req_prefetch();
         cb.req_prefetch();
 
-        sequence<N, size_t> seqa(0), seqb(0);
-        for(size_t i = 0; i < N; i++) seqa[i] = seqb[i] = i;
+        permutation<N> pinvb(m_permb, true);
+
+        sequence<N, size_t> seqa(0);
+        for(size_t i = 0; i < N; i++) seqa[i] = i;
         m_perma.apply(seqa);
-        m_permb.apply(seqb);
+        pinvb.apply(seqa);
 
         const dimensions<N> &dimsa(m_ta.get_dims());
         const dimensions<N> &dimsb(m_tb.get_dims());
@@ -103,14 +105,20 @@ double tod_dotprod<N>::calculate() {
         std::list< loop_list_node<2, 1> > loop_in, loop_out;
         typename std::list< loop_list_node<2, 1> >::iterator inode =
             loop_in.end();
-        for(size_t i = 0; i < N; i++) {
-            size_t ia = seqa[i], ib = seqb[i];
-            size_t w = dimsa[ia];
-            inode = loop_in.insert(loop_in.end(), loop_list_node<2, 1>(w));
-            inode->stepa(0) = dimsa.get_increment(ia);
-            inode->stepa(1) = dimsb.get_increment(ib);
+        for(size_t ib = 0; ib < N;) {
+            size_t len = 1;
+            size_t ia = seqa[ib];
+            do {
+                len *= dimsa.get_dim(ia);
+                ia++; ib++;
+            } while(ib < N && seqa[ib] == ia);
+
+            inode = loop_in.insert(loop_in.end(), loop_list_node<2, 1>(len));
+            inode->stepa(0) = dimsa.get_increment(ia - 1);
+            inode->stepa(1) = dimsb.get_increment(ib - 1);
             inode->stepb(0) = 0;
         }
+
 
         const double *pa = ca.req_const_dataptr();
         const double *pb = cb.req_const_dataptr();
