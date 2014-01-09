0753c816cbbd926b7e1fd2027648c73e6a46fd7d
Evgeny Epifanovsky <epif@q-chem.com>
2014-01-09 01:31:19 +0000

    3-index symm

--- libtensor/iface/btensor/impl/eval_btensor_double_symm.C
+++ libtensor/iface/btensor/impl/eval_btensor_double_symm.C
@@ -23,6 +23,18 @@ private:
     };
 
 private:
+    template<size_t N>
+    struct dispatch_symm {
+        eval_symm_impl &eval;
+        const tensor_transf<N, double> &tr;
+        const node &t;
+        template<size_t M> void dispatch();
+    };
+
+    template<size_t M>
+    struct tag { };
+
+private:
     const expr_tree &m_tree; //!< Expression tree
     expr_tree::node_id_t m_id; //!< ID of copy node
     bool m_add; //!< True if add
@@ -35,12 +47,17 @@ public:
     template<size_t N>
     void evaluate(const tensor_transf<N, double> &trc, const node &t);
 
-    void evaluate(const tensor_transf<1, double> &tr, const node &t);
+    template<size_t N, size_t M>
+    void do_evaluate(const tensor_transf<N, double> &trc, const node &t,
+        const tag<M>&);
 
-private:
     template<size_t N>
-    expr_tree::node_id_t gather_info(expr_tree::node_id_t id,
-        tensor_transf<N, double> &tr);
+    void do_evaluate(const tensor_transf<N, double> &trc, const node &t,
+        const tag<2>&);
+
+    template<size_t N>
+    void do_evaluate(const tensor_transf<N, double> &trc, const node &t,
+        const tag<3>&);
 
 };
 
@@ -49,94 +66,108 @@ template<size_t N>
 void eval_symm_impl::evaluate(const tensor_transf<N, double> &tr,
     const node &t) {
 
+    const node_symm<double> &n =
+        m_tree.get_vertex(m_id).recast_as< node_symm<double> >();
+
+    dispatch_symm<N> d = { *this, tr, t };
+    dispatch_1<2, N>::dispatch(d, n.get_nsym());
+}
+
+
+template<size_t N>
+void eval_symm_impl::do_evaluate(const tensor_transf<N, double> &tr,
+    const node &t, const tag<2>&) {
+
     const expr_tree::edge_list_t &e = m_tree.get_edges_out(m_id);
     if (e.size() != 1) throw "More than one child node";
 
     const node &n = m_tree.get_vertex(m_id);
     const node_symm<double> &nn = n.recast_as< node_symm<double> >();
-    if(nn.get_nsym() == 2) {
 
-        // Need to convert
-        // T2 S T1 A -> S' T' A, where S = I + Ts and S' = I + Ts'
-        //
-        // T2 (I + Ts) T1 A =
-        // (T2 T1 + T2 Ts T1) A =
-        // (T2 T1 + T2 Ts T2(inv) T2 T1) A =
-        // [I + T2 Ts T2(inv)] T2 T1 A
-        //
-        // => Ts' = T2 Ts T2(inv); T' = T2 T1
+    // Need to convert
+    // T2 S T1 A -> S' T' A, where S = I + Ts and S' = I + Ts'
+    //
+    // T2 (I + Ts) T1 A =
+    // (T2 T1 + T2 Ts T1) A =
+    // (T2 T1 + T2 Ts T2(inv) T2 T1) A =
+    // [I + T2 Ts T2(inv)] T2 T1 A
+    //
+    // => Ts' = T2 Ts T2(inv); T' = T2 T1
 
-        tensor_transf<N, double> tr1;
-        expr_tree::node_id_t id1 = gather_info(e[0], tr1);
-        const node &n1 = m_tree.get_vertex(id1);
-        btensor_i<N, double> &bta = tensor_from_node<N>(n1);
+    btensor_from_node<N, double> bta(m_tree, e[0]);
 
-        if(nn.get_sym().size() % 2 != 0) {
-            throw "Wrong size of symmetrization sequence";
-        }
-        size_t nsymidx = nn.get_sym().size() / 2;
-        permutation<N> symperm;
-        for(size_t i = 0; i < nsymidx; i++) {
-            symperm.permute(nn.get_sym()[i], nn.get_sym()[nsymidx + i]);
-        }
-
-        tensor_transf<N, double> tr2(tr), tr2inv(tr2, true);
-        tensor_transf<N, double> tpr(tr1);
-        tpr.transform(tr2);
-        tensor_transf<N, double> trs(symperm, nn.get_pair_tr());
-        tensor_transf<N, double> tspr(tr2inv);
-        tspr.transform(trs);
-        tspr.transform(tr2);
-
-        btod_copy<N> op(bta, tpr.get_perm(), tpr.get_scalar_tr().get_coeff());
-        btod_symmetrize2<N> symop(op, tspr.get_perm(), tspr.get_scalar_tr().get_coeff() == 1.0);
-        btensor<N, double> &bt = tensor_from_node(t, symop.get_bis());
-        if(m_add) {
-            symop.perform(bt, 1.0);
-        } else {
-            symop.perform(bt);
-        }
-
-    } else if(nn.get_nsym() == 3) {
-        throw "Third-order symmetrizations not implemented";
-    } else {
-        throw "High-order symmetrizations not implemented";
+    if(nn.get_sym().size() % 2 != 0) {
+        throw "Wrong size of symmetrization sequence";
     }
-}
+    size_t nsymidx = nn.get_sym().size() / 2;
+    permutation<N> symperm;
+    for(size_t i = 0; i < nsymidx; i++) {
+        symperm.permute(nn.get_sym()[i], nn.get_sym()[nsymidx + i]);
+    }
 
+    tensor_transf<N, double> tr2(tr), tr2inv(tr2, true);
+    tensor_transf<N, double> tpr(bta.get_transf());
+    tpr.transform(tr2);
+    tensor_transf<N, double> trs(symperm, nn.get_pair_tr());
+    tensor_transf<N, double> tspr(tr2inv);
+    tspr.transform(trs);
+    tspr.transform(tr2);
 
-void eval_symm_impl::evaluate(const tensor_transf<1, double> &tr, const node &t) {
-
-    throw "Should not be here";
+    btod_copy<N> op(bta.get_btensor(), tpr.get_perm(),
+        tpr.get_scalar_tr().get_coeff());
+    btod_symmetrize2<N> symop(op, tspr.get_perm(),
+        tspr.get_scalar_tr().get_coeff() == 1.0);
+    btensor<N, double> &bt = tensor_from_node(t, symop.get_bis());
+    if(m_add) {
+        symop.perform(bt, 1.0);
+    } else {
+        symop.perform(bt);
+    }
 }
 
 
 template<size_t N>
-expr_tree::node_id_t eval_symm_impl::gather_info(
-    expr_tree::node_id_t id, tensor_transf<N, double> &tr) {
+void eval_symm_impl::do_evaluate(const tensor_transf<N, double> &tr,
+    const node &t, const tag<3>&) {
 
-    const node &n = m_tree.get_vertex(id);
-    if (n.get_op().compare(node_transform_base::k_op_type) != 0) {
-        return id;
+    const expr_tree::edge_list_t &e = m_tree.get_edges_out(m_id);
+    if (e.size() != 1) throw "More than one child node";
+
+    const node &n = m_tree.get_vertex(m_id);
+    const node_symm<double> &nn = n.recast_as< node_symm<double> >();
+
+    btensor_from_node<N, double> bta(m_tree, e[0]);
+
+    if(nn.get_sym().size() != 3) {
+        throw "Wrong size of symmetrization sequence";
     }
 
-    const node_transform<double> &ntr =
-            n.recast_as< node_transform<double> >();
-
-    const std::vector<size_t> &p = ntr.get_perm();
-    if(p.size() != N) {
-        throw "Bad transform node";
+    btod_copy<N> op(bta.get_btensor(), bta.get_transf().get_perm(),
+        bta.get_transf().get_scalar_tr().get_coeff());
+    btod_symmetrize3<N> symop(op, nn.get_sym().at(0), nn.get_sym().at(1),
+        nn.get_sym().at(2), nn.get_pair_tr().get_coeff() == 1.0);
+    btensor<N, double> &bt = tensor_from_node(t, symop.get_bis());
+    if(m_add) {
+        symop.perform(bt, 1.0);
+    } else {
+        symop.perform(bt);
     }
-    sequence<N, size_t> s0(0), s1(0);
-    for(size_t i = 0; i < N; i++) {
-        s0[i] = i;
-        s1[i] = p[i];
-    }
-    permutation_builder<N> pb(s1, s0);
-    tr.permute(pb.get_perm());
-    tr.transform(ntr.get_coeff());
+}
 
-    return m_tree.get_edges_out(id).front();
+
+template<size_t N, size_t M>
+void eval_symm_impl::do_evaluate(const tensor_transf<N, double> &tr,
+    const node &t, const tag<M>&) {
+
+    std:: cout << "do_evaluate<" << N << ", " << M << ">" << std::endl;
+    throw "High-order symmetrizations not implemented";
+}
+
+
+template<size_t N> template<size_t M>
+void eval_symm_impl::dispatch_symm<N>::dispatch() {
+
+    eval.do_evaluate(tr, t, tag<M>());
 }
 
 
