980fdea7d46795918b7930a8adb864547882f2c7
Evgeny Epifanovsky <epif@q-chem.com>
2014-02-19 22:15:06 +0000

    break down tod_contract2.C into multiple files

---
+++ libtensor/dense_tensor/impl/tod_contract2_5.C
@@ -0,0 +1,36 @@
+#include "tod_contract2_impl.h"
+
+namespace libtensor {
+
+
+template class tod_contract2<0, 5, 1>;
+template class tod_contract2<0, 5, 2>;
+template class tod_contract2<0, 5, 3>;
+template class tod_contract2<1, 4, 0>;
+template class tod_contract2<1, 4, 1>;
+template class tod_contract2<1, 4, 2>;
+template class tod_contract2<1, 4, 3>;
+template class tod_contract2<1, 4, 4>;
+template class tod_contract2<2, 3, 0>;
+template class tod_contract2<2, 3, 1>;
+template class tod_contract2<2, 3, 2>;
+template class tod_contract2<2, 3, 3>;
+template class tod_contract2<2, 3, 4>;
+template class tod_contract2<2, 3, 5>;
+template class tod_contract2<3, 2, 0>;
+template class tod_contract2<3, 2, 1>;
+template class tod_contract2<3, 2, 2>;
+template class tod_contract2<3, 2, 3>;
+template class tod_contract2<3, 2, 4>;
+template class tod_contract2<3, 2, 5>;
+template class tod_contract2<4, 1, 0>;
+template class tod_contract2<4, 1, 1>;
+template class tod_contract2<4, 1, 2>;
+template class tod_contract2<4, 1, 3>;
+template class tod_contract2<4, 1, 4>;
+template class tod_contract2<5, 0, 1>;
+template class tod_contract2<5, 0, 2>;
+template class tod_contract2<5, 0, 3>;
+
+
+} // namespace libtensor
