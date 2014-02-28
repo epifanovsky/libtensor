b9f7ceb70cb44eb2b048bf92cc2ba4daad296636
Michael Wormit <michael.wormit@iwr.uni-heidelberg.de>
2014-02-28 16:30:31 +0000

    Test + fix for bug in so_dirsum of se_part

--- libtensor/symmetry/inst/so_dirsum_se_part_impl.h
+++ libtensor/symmetry/inst/so_dirsum_se_part_impl.h
@@ -26,8 +26,6 @@ symmetry_operation_impl< so_dirsum<N, M, T>, se_part<N + M, T> >::do_perform(
     // Nothing needs to be done if both sets are empty
     if (params.g1.is_empty() && params.g2.is_empty()) return;
 
-    // [120] map2[
-
     permutation<N + M> pinv(params.perm, true);
     sequence<N, size_t> map1(0);
     sequence<M, size_t> map2(0);
@@ -154,24 +152,23 @@ symmetry_operation_impl< so_dirsum<N, M, T>, se_part<N + M, T> >::do_perform(
         se_part<N + M, T> se3(params.bis,
                 dimensions<N + M>(index_range<N + M>(i3a, i3b)));
 
-        abs_index<N + M> ai3a(se3.get_pdims());
+        abs_index<N + M> ai3(se3.get_pdims());
         do {
-            const index<N + M> &i3a = ai3a.get_index();
+            const index<N + M> &i3 = ai3.get_index();
 
             index<N> i1a;
-            for (register size_t i = 0; i < N; i++) i1a[i] = i3a[map1[i]];
+            for (register size_t i = 0; i < N; i++) i1a[i] = i3[map1[i]];
             index<M> i2a;
-            for (register size_t i = 0; i < M; i++) i2a[i] = i3a[map2[i]];
+            for (register size_t i = 0; i < M; i++) i2a[i] = i3[map2[i]];
 
             bool fb1 = se1.is_forbidden(i1a);
             bool fb2 = se2.is_forbidden(i2a);
 
             if (fb1 && fb2) { // Both forbidden
-                se3.mark_forbidden(i3a);
+                se3.mark_forbidden(i3);
                 continue;
             }
-
-            if (fb1) { // se1 forbidden
+            else if (fb1) { // se1 forbidden
                 // Try to create a map leaving se1 indexes fixed
                 index<M> i2b = se2.get_direct_map(i2a);
                 if (i2a < i2b) {
@@ -179,114 +176,104 @@ symmetry_operation_impl< so_dirsum<N, M, T>, se_part<N + M, T> >::do_perform(
                         i3b[map1[i]] = i1a[i];
                     for (register size_t i = 0; i < M; i++)
                         i3b[map2[i]] = i2b[i];
-                    se3.add_map(i3a, i3b, se2.get_transf(i2a, i2b));
-
+                    se3.add_map(i3, i3b, se2.get_transf(i2a, i2b));
                     continue;
                 }
 
                 // Otherwise check, if there is another forbidden index in se1
                 abs_index<N> ai1b(i1a, se1.get_pdims());
-                while (ai1b.inc()) {
-                    if (se1.is_forbidden(ai1b.get_index())) break;
-                }
+                if (! ai1b.is_last()) {
+                    while (ai1b.inc()) {
+                        if (se1.is_forbidden(ai1b.get_index())) break;
+                    }
 
-                if (se1.is_forbidden(ai1b.get_index())) {
-                    const index<N> &i1b = ai1b.get_index();
-                    for (register size_t i = 0; i < N; i++)
-                        i3b[map1[i]] = i1b[i];
-                    for (register size_t i = 0; i < M; i++)
-                        i3b[map2[i]] = i2a[i];
-                    se3.add_map(i3a, i3b, tr0);
+                    if (se1.is_forbidden(ai1b.get_index())) {
+                        const index<N> &i1b = ai1b.get_index();
+                        for (register size_t i = 0; i < N; i++)
+                            i3b[map1[i]] = i1b[i];
+                        for (register size_t i = 0; i < M; i++)
+                            i3b[map2[i]] = i2a[i];
+                        se3.add_map(i3, i3b, tr0);
+                    }
                 }
-
-                continue;
             }
-
-            if (fb2) { // se2 forbidden
+            else if (fb2) { // se2 forbidden
                 // Check, if there is another forbidden index in se2
                 abs_index<M> ai2b(i2a, se2.get_pdims());
-                while (ai2b.inc()) {
-                    if (se2.is_forbidden(ai2b.get_index())) break;
+                if (! ai2b.is_last()) {
+                    while (ai2b.inc()) {
+                        if (se2.is_forbidden(ai2b.get_index())) break;
+                    }
+
+                    if (se2.is_forbidden(ai2b.get_index())) {
+                        const index<M> &i2b = ai2b.get_index();
+                        for (register size_t i = 0; i < N; i++)
+                            i3b[map1[i]] = i1a[i];
+                        for (register size_t i = 0; i < M; i++)
+                            i3b[map2[i]] = i2b[i];
+                        se3.add_map(i3, i3b, tr0);
+                        continue;
+                    }
                 }
-
-                // If there is, create a map
-                if (se2.is_forbidden(ai2b.get_index())) {
-                    const index<M> &i2b = ai2b.get_index();
-                    for (register size_t i = 0; i < N; i++)
-                        i3b[map1[i]] = i1a[i];
-                    for (register size_t i = 0; i < M; i++)
-                        i3b[map2[i]] = i2b[i];
-                    se3.add_map(i3a, i3b, tr0);
-
-                    continue;
-                }
-
-                // Try to create a map leaving se2 indexes fixed
+                // Otherwise, try to create a map leaving se2 indexes fixed
                 index<N> i1b = se1.get_direct_map(i1a);
                 if (i1a < i1b) {
                     for (register size_t i = 0; i < N; i++)
                         i3b[map1[i]] = i1b[i];
                     for (register size_t i = 0; i < M; i++)
                         i3b[map2[i]] = i2a[i];
-                    se3.add_map(i3a, i3b, se1.get_transf(i1a, i1b));
+                    se3.add_map(i3, i3b, se1.get_transf(i1a, i1b));
                 }
-
-                continue;
             }
-
-            // Both allowed
-            // First try to create a map which leaves indexes of 1 fixed
-            index<M> i2b = se2.get_direct_map(i2a);
-            scalar_transf<T> tr2 = se2.get_transf(i2a, i2b);
-            while (i2a < i2b && ! tr2.is_identity()) {
-                i2b = se2.get_direct_map(i2b);
-                tr2 = se2.get_transf(i2a, i2b);
-            }
-            if (i2a < i2b) {
-                for (register size_t i = 0; i < N; i++)
-                    i3b[map1[i]] = i1a[i];
-                for (register size_t i = 0; i < M; i++)
-                    i3b[map2[i]] = i2b[i];
-                se3.add_map(i3a, i3b, tr2);
-
-                continue;
-            }
-
-            // Then try to find the map that changes the indexes of 1 by the
-            // least amount
-            index<N> i1b = se1.get_direct_map(i1a);
-            while (i1a < i1b) {
-                scalar_transf<T> tr1 = se1.get_transf(i1a, i1b);
-                // This can either be changeing only indexes of 1
-                if (tr1.is_identity()) {
-                    for (register size_t i = 0; i < N; i++)
-                        i3b[map1[i]] = i1b[i];
-                    for (register size_t i = 0; i < M; i++)
-                        i3b[map2[i]] = i2a[i];
-                    se3.add_map(i3a, i3b, tr1);
-
-                    break;
-                }
-
-                // Or of 1 and 2
+            else { // both allowed
+                // First try to create a map which leaves indexes of 1 fixed
                 index<M> i2b = se2.get_direct_map(i2a);
-                while (i2a != i2b) {
-                    if (se2.get_transf(i2a, i2b) == tr1) break;
+                scalar_transf<T> tr2 = se2.get_transf(i2a, i2b);
+                while (i2a < i2b && ! tr2.is_identity()) {
                     i2b = se2.get_direct_map(i2b);
+                    tr2 = se2.get_transf(i2a, i2b);
                 }
-                if (i2a != i2b) {
+                if (i2a < i2b) {
                     for (register size_t i = 0; i < N; i++)
-                        i3b[map1[i]] = i1b[i];
+                        i3b[map1[i]] = i1a[i];
                     for (register size_t i = 0; i < M; i++)
                         i3b[map2[i]] = i2b[i];
-                    se3.add_map(i3a, i3b, tr1);
-
-                    break;
+                    se3.add_map(i3, i3b, tr2);
+                    continue;
                 }
 
-                i1b = se1.get_direct_map(i1b);
+                // Then try to find the map that changes the indexes of 1 by the
+                // least amount
+                index<N> i1b = se1.get_direct_map(i1a);
+                while (i1a < i1b) {
+                    scalar_transf<T> tr1 = se1.get_transf(i1a, i1b);
+                    // This can either be changing only indexes of 1
+                    if (tr1.is_identity()) {
+                        for (register size_t i = 0; i < N; i++)
+                            i3b[map1[i]] = i1b[i];
+                        for (register size_t i = 0; i < M; i++)
+                            i3b[map2[i]] = i2a[i];
+                        se3.add_map(i3, i3b, tr1);
+                        break;
+                    }
+                    // Or of 1 and 2
+                    index<M> i2b = se2.get_direct_map(i2a);
+                    while (i2a != i2b) {
+                        if (se2.get_transf(i2a, i2b) == tr1) break;
+                        i2b = se2.get_direct_map(i2b);
+                    }
+                    if (i2a != i2b) {
+                        for (register size_t i = 0; i < N; i++)
+                            i3b[map1[i]] = i1b[i];
+                        for (register size_t i = 0; i < M; i++)
+                            i3b[map2[i]] = i2b[i];
+                        se3.add_map(i3, i3b, tr1);
+                        break;
+                    }
+                    i1b = se1.get_direct_map(i1b);
+                }
             }
-         } while (ai3a.inc());
+         } while (ai3.inc());
 
         params.g3.insert(se3);
     }
