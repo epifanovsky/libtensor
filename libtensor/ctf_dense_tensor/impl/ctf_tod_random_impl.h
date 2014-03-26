f433301ceb50b03825e6afce0fe7cc8288b7d2f2
Evgeny Epifanovsky <epif@q-chem.com>
2014-03-26 00:21:28 +0000

    fixed batching in ctf_tod_random

--- libtensor/ctf_dense_tensor/impl/ctf_tod_random_impl.h
+++ libtensor/ctf_dense_tensor/impl/ctf_tod_random_impl.h
@@ -28,26 +28,41 @@ ctf_tod_random<N>::ctf_tod_random(double c) : m_c(c) {
 template<size_t N>
 void ctf_tod_random<N>::perform(bool zero, ctf_dense_tensor_i<N, double> &ta) {
 
+    const size_t batsz = 1024 * 1024;
+
     size_t totsz = ta.get_dims().get_size();
 
+    // Total number of batches
+    size_t totnb = totsz / batsz;
+    if(totnb * batsz < totsz) totnb++;
+
     unsigned iproc = ctf::get_rank(), nproc = ctf::get_size();
-    size_t off = iproc * totsz / nproc;
-    size_t sz = std::min((iproc + 1) * totsz / nproc, totsz) - off;
+
+    // Number of batches per processor
+    size_t nbpp = totnb / nproc;
+    if(nbpp * nproc < totnb) nbpp++;
 
     ctf_dense_tensor_ctrl<N, double> ca(ta);
     tCTF_Tensor<double> &dta = ca.req_ctf_tensor();
 
-    const size_t bufsz = 1024 * 1024;
-    std::vector<long_int> idx(bufsz);
-    std::vector<double> data(bufsz);
-    for(size_t i = 0; i < sz; i += bufsz) {
-        size_t n = std::min(sz - i, bufsz);
-        for(size_t j = 0; j < n; j++) idx[j] = long_int(off + i + j);
+    std::vector<long_int> idx(batsz);
+    std::vector<double> data(batsz);
+
+    for(size_t i = 0; i < nbpp; i++) {
+        size_t off = (iproc * nbpp + i) * batsz;
+        size_t n = batsz;
+        if(iproc * nbpp + i + 1 == totnb) {
+            n = totsz - off;
+        } else if(iproc * nbpp + i >= totnb) {
+            n = 0;
+        }
+
+        for(size_t j = 0; j < n; j++) idx[j] = long_int(off + j);
         if(zero) {
-            linalg::rng_set_i_x(0, n, &data[0], 1, m_c);
+            if(n > 0) linalg::rng_set_i_x(0, n, &data[0], 1, m_c);
         } else {
             dta.read(n, &idx[0], &data[0]);
-            linalg::rng_add_i_x(0, n, &data[0], 1, m_c);
+            if(n > 0) linalg::rng_add_i_x(0, n, &data[0], 1, m_c);
         }
         dta.write(n, &idx[0], &data[0]);
     }
--- tests/ctf_dense_tensor/ctf_tod_random_test.C
+++ tests/ctf_dense_tensor/ctf_tod_random_test.C
@@ -20,6 +20,7 @@ void ctf_tod_random_test::perform() throw(libtest::test_exception) {
 
         test_1();
         test_2();
+        test_3();
 
     } catch(...) {
         ctf::exit();
@@ -120,6 +121,35 @@ void ctf_tod_random_test::test_2() {
     for(size_t i = 0; i < sz; i++) if(fabs(pa[i] - pb[i]) < 1e-14) ndup++;
     if(ndup > 0) {
         fail_test(testname, __FILE__, __LINE__, "Too many values are similar.");
+    }
+}
+
+
+void ctf_tod_random_test::test_3() {
+
+    static const char testname[] = "ctf_tod_random_test::test_3()";
+
+    typedef std_allocator<double> allocator_t;
+
+    index<1> i1a, i1b;
+    i1b[0] = 2;
+    dimensions<1> dims(index_range<1>(i1a, i1b));
+
+    dense_tensor<1, double, allocator_t> ta(dims);
+    ctf_dense_tensor<1, double> dta(dims);
+
+    tod_set<1>(100.0).perform(ta);
+    ctf_tod_distribute<1>(ta).perform(dta);
+    ctf_tod_random<1>().perform(true, dta);
+    ctf_tod_collect<1>(dta).perform(ta);
+
+    dense_tensor_rd_ctrl<1, double> ca(ta);
+    const double *pa = ca.req_const_dataptr();
+    size_t sz = dims.get_size();
+
+    for(size_t i = 0; i < sz; i++) if(pa[i] < 0.0 || pa[i] > 1.0) {
+        fail_test(testname, __FILE__, __LINE__,
+            "Random value (A) is out of range.");
     }
 }
 
