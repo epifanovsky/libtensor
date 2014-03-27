8ba112c72e66ca9cb0fe4365abe04297b49fddb7
Evgeny Epifanovsky <epif@q-chem.com>
2014-03-27 18:37:28 +0000

    ctf_tod_random without batching

--- libtensor/ctf_dense_tensor/impl/ctf_tod_random_impl.h
+++ libtensor/ctf_dense_tensor/impl/ctf_tod_random_impl.h
@@ -28,44 +28,16 @@ ctf_tod_random<N>::ctf_tod_random(double c) : m_c(c) {
 template<size_t N>
 void ctf_tod_random<N>::perform(bool zero, ctf_dense_tensor_i<N, double> &ta) {
 
-    const size_t batsz = 1024 * 1024;
-
-    size_t totsz = ta.get_dims().get_size();
-
-    // Total number of batches
-    size_t totnb = totsz / batsz;
-    if(totnb * batsz < totsz) totnb++;
-
-    unsigned iproc = ctf::get_rank(), nproc = ctf::get_size();
-
-    // Number of batches per processor
-    size_t nbpp = totnb / nproc;
-    if(nbpp * nproc < totnb) nbpp++;
-
     ctf_dense_tensor_ctrl<N, double> ca(ta);
     tCTF_Tensor<double> &dta = ca.req_ctf_tensor();
 
-    std::vector<long_int> idx(batsz);
-    std::vector<double> data(batsz);
+    long_int np, *idx;
+    double *p;
 
-    for(size_t i = 0; i < nbpp; i++) {
-        size_t off = (iproc * nbpp + i) * batsz;
-        size_t n = batsz;
-        if(iproc * nbpp + i + 1 == totnb) {
-            n = totsz - off;
-        } else if(iproc * nbpp + i >= totnb) {
-            n = 0;
-        }
-
-        for(size_t j = 0; j < n; j++) idx[j] = long_int(off + j);
-        if(zero) {
-            if(n > 0) linalg::rng_set_i_x(0, n, &data[0], 1, m_c);
-        } else {
-            dta.read(n, &idx[0], &data[0]);
-            if(n > 0) linalg::rng_add_i_x(0, n, &data[0], 1, m_c);
-        }
-        dta.write(n, &idx[0], &data[0]);
-    }
+    dta.read_local(&np, &idx, &p);
+    if(zero) linalg::rng_set_i_x(0, np, p, 1, m_c);
+    else linalg::rng_add_i_x(0, np, p, 1, m_c);
+    dta.write(np, idx, p);
 }
 
 
