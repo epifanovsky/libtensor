8b1866ffe348fed3b2a60c5911e7663d5181fcba
Evgeny Epifanovsky <epif@q-chem.com>
2014-03-27 18:09:44 +0000

    ctf_tod_set via assignment

--- libtensor/ctf_dense_tensor/impl/ctf_tod_set_impl.h
+++ libtensor/ctf_dense_tensor/impl/ctf_tod_set_impl.h
@@ -19,18 +19,7 @@ void ctf_tod_set<N>::perform(ctf_dense_tensor_i<N, double> &ta) {
     ctf_dense_tensor_ctrl<N, double> ca(ta);
     tCTF_Tensor<double> &dta = ca.req_ctf_tensor();
 
-    const size_t bufsz = 1024 * 1024;
-    std::vector<long_int> idx(bufsz);
-    std::vector<double> data(bufsz, m_v);
-    for(size_t i = 0; i < sz; i += bufsz) {
-        size_t n = std::min(sz - i, bufsz);
-        for(size_t j = 0; j < n; j++) idx[j] = long_int(i + j);
-        if(ctf::is_master()) {
-            dta.write(n, &idx[0], &data[0]);
-        } else {
-            dta.write(0, 0, 0);
-        }
-    }
+    dta = m_v;
 }
 
 
