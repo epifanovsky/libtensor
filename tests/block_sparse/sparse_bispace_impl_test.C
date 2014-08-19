b203b9f56b9784fd945bfff650eb5390828835f2
Sam Manzer <samuel.manzer1990@gmail.com>
2014-08-19 22:10:47 +0000

    First sparse_bispace_impl test passes

--- tests/block_sparse/sparse_bispace_impl_test.C
+++ tests/block_sparse/sparse_bispace_impl_test.C
@@ -1,9 +1,44 @@
 #include "sparse_bispace_impl_test.h"
+#include <libtensor/block_sparse/sparse_bispace_impl.h>
+
+using namespace std;
 
 namespace libtensor {
 
 void sparse_bispace_impl_test::perform() throw(libtest::test_exception)
 {
+    test_nd_equality_true();
+}
+
+
+/* Tests equality operator for multidimensional block index spaces
+ *
+ */
+void sparse_bispace_impl_test::test_nd_equality_true() throw(libtest::test_exception) {
+
+    static const char *test_name = "sparse_bispace_test::test_nd_equality_true()";
+
+    //First two
+    vector<subspace> subspaces(1,subspace(5));
+    vector<size_t> split_points_0; 
+    split_points_0.push_back(1);
+    split_points_0.push_back(3);
+    subspaces[0].split(split_points_0);
+
+    subspaces.push_back(subspace(6));
+    vector<size_t> split_points_1; 
+    split_points_1.push_back(2);
+    split_points_1.push_back(5);
+    subspaces[1].split(split_points_1);
+
+    sparse_bispace_impl spb_i_0(subspaces);
+    sparse_bispace_impl spb_i_1(subspaces);
+
+    if(!(spb_i_0 == spb_i_1))
+    {
+        fail_test(test_name,__FILE__,__LINE__,
+                "sparse_bispace_impl::operator==(...) returned false");
+    }
 }
 
 } // namespace libtensor
--- tests/block_sparse/sparse_bispace_impl_test.h
+++ tests/block_sparse/sparse_bispace_impl_test.h
@@ -10,6 +10,69 @@ class sparse_bispace_impl_test : public libtest::unit_test
 public:
     virtual void perform() throw(libtest::test_exception);
 private:
+
+    /*
+     * operator==() tests
+     */
+    void test_nd_equality_true() throw(libtest::test_exception);
+
+    /*
+     * sparse_bispace<N>::operator[](...) tests
+     */
+    //Negative tests (throw correct exception)
+    void test_nd_subscript_operator_gt_upper_bound() throw(libtest::test_exception);
+
+    /* 
+     * sparse_bispace<N>::get_nnz(...) tests
+     */
+    //Positive tests (produce correct result)
+    void test_get_nnz_dense() throw(libtest::test_exception);
+    void test_get_nnz_2d_sparsity() throw(libtest::test_exception);
+    void test_get_nnz_3d_dense_sparse() throw(libtest::test_exception);
+    void test_get_nnz_3d_sparse_dense() throw(libtest::test_exception);
+    void test_get_nnz_3d_fully_sparse() throw(libtest::test_exception);
+
+    /*
+     * sparse_bispace<N>::permute(...) tests
+     */
+    void test_permute_2d_10() throw(libtest::test_exception);
+    void test_permute_3d_dense_sparse_021() throw(libtest::test_exception);
+    void test_permute_3d_non_contiguous_sparsity() throw(libtest::test_exception);
+    void test_permute_3d_fully_sparse_210() throw(libtest::test_exception);
+
+    /*
+     * sparse_bispace<N>::contract(...) tests
+     */
+    void test_contract_3d_dense() throw(libtest::test_exception);
+    void test_contract_3d_sparse_2() throw(libtest::test_exception);
+    void test_contract_3d_sparse_2_nnz() throw(libtest::test_exception);
+    void test_contract_3d_sparse_destroy_all_sparsity() throw(libtest::test_exception);
+
+    void test_truncate_subspace() throw(libtest::test_exception);
+
+    /*
+     * sparse_bispace<N>::fuse(...) tests
+     */
+    void test_fuse_2d_2d() throw(libtest::test_exception);
+    void test_fuse_3d_3d_no_overlap() throw(libtest::test_exception);
+    void test_fuse_3d_3d_invalid_no_match() throw(libtest::test_exception);
+
+    /*
+     * index_group tests
+     */
+    void test_get_n_index_groups() throw(libtest::test_exception);
+    void test_get_index_group_offset() throw(libtest::test_exception);
+    void test_get_index_group_order() throw(libtest::test_exception);
+    void test_get_index_group_dim() throw(libtest::test_exception);
+    void test_get_index_group_containing_subspace() throw(libtest::test_exception);
+
+    void test_get_batch_size() throw(libtest::test_exception);
+
+    /*  ALL TESTS INVOLVING SPARSITY!!
+     *
+     */
+    void test_equality_false_sparsity_2d() throw(libtest::test_exception);
+    void test_equality_true_sparsity_2d() throw(libtest::test_exception);
 };
 
 } // namespace libtensor
