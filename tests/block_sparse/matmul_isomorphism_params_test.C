9403f8f77dd831555495dd69ab5288f782036b23
Sam Manzer <samuel.manzer1990@gmail.com>
2015-01-22 22:23:57 +0000

    Finished refactoring all matmul_isomorphism_params tests

--- tests/block_sparse/matmul_isomorphism_params_test.C
+++ tests/block_sparse/matmul_isomorphism_params_test.C
@@ -7,15 +7,13 @@ namespace libtensor {
 
 void blas_isomorphism_test::perform() throw(libtest::test_exception) {
     test_matmul_isomorphism_params_identity_NN();
-#if 0
     test_matmul_isomorphism_params_identity_NT();
     test_matmul_isomorphism_params_identity_TN();
     test_matmul_isomorphism_params_identity_TT();
     test_matmul_isomorphism_params_3d_2d_A_perm();
     test_matmul_isomorphism_params_permuted_ioc();
+    //Not sure what correct result was intended to be for this test
     /*test_matmul_isomorphism_params_3d_3d_A_perm_B_perm();*/
-    /*test_matmul_isomorphism_params_3d_3d_A_perm_B_perm();*/
-#endif
 }
 
 //Give it NN,NT,TN,TT matrix multiply cases, it should return identity permutation for
@@ -60,7 +58,6 @@ void blas_isomorphism_test::test_matmul_isomorphism_params_identity_NN() throw(l
     }
 }
 
-#if 0
 void blas_isomorphism_test::test_matmul_isomorphism_params_identity_NT() throw(libtest::test_exception)
 {
     static const char *test_name = "blas_isomorphism_test::test_matmul_isomorphism_params_identity_NT()";
@@ -70,24 +67,22 @@ void blas_isomorphism_test::test_matmul_isomorphism_params_identity_NT() throw(l
     sparse_bispace<1> spb_j(3);
     sparse_bispace<1> spb_k(4);
 
-    vector<sparse_bispace_any_order> bispaces(1,spb_i | spb_j);
+    vector<sparse_bispace_impl> bispaces(1,spb_i | spb_j);
     bispaces.push_back(spb_i|spb_k);
     bispaces.push_back(spb_j|spb_k);
 
-    vector<block_loop> loops(3,block_loop(bispaces));
+    vector<idx_pair_list> ts_groups(3);
     //i loop
-    loops[0].set_subspace_looped(0,0);
-    loops[0].set_subspace_looped(1,0);
+    ts_groups[0].push_back(idx_pair(0,0));
+    ts_groups[0].push_back(idx_pair(1,0));
     //j loop
-    loops[1].set_subspace_looped(0,1);
-    loops[1].set_subspace_looped(2,0);
+    ts_groups[1].push_back(idx_pair(0,1));
+    ts_groups[1].push_back(idx_pair(2,0));
     //k loop
-    loops[2].set_subspace_looped(1,1);
-    loops[2].set_subspace_looped(2,1);
+    ts_groups[2].push_back(idx_pair(1,1));
+    ts_groups[2].push_back(idx_pair(2,1));
 
-    sparse_loop_list sll(loops,bispaces);
-
-    matmul_isomorphism_params<double> mip(sll);
+    matmul_isomorphism_params mip(bispaces,ts_groups);
 
     runtime_permutation perm_A = mip.get_A_perm();
     runtime_permutation perm_B = mip.get_B_perm();
@@ -112,24 +107,22 @@ void blas_isomorphism_test::test_matmul_isomorphism_params_identity_TN() throw(l
     sparse_bispace<1> spb_j(3);
     sparse_bispace<1> spb_k(4);
 
-    vector<sparse_bispace_any_order> bispaces(1,spb_i | spb_j);
+    vector<sparse_bispace_impl> bispaces(1,spb_i | spb_j);
     bispaces.push_back(spb_k|spb_i);
     bispaces.push_back(spb_k|spb_j);
 
-    vector<block_loop> loops(3,block_loop(bispaces));
+    vector<idx_pair_list> ts_groups(3);
     //i loop
-    loops[0].set_subspace_looped(0,0);
-    loops[0].set_subspace_looped(1,1);
+    ts_groups[0].push_back(idx_pair(0,0));
+    ts_groups[0].push_back(idx_pair(1,1));
     //j loop
-    loops[1].set_subspace_looped(0,1);
-    loops[1].set_subspace_looped(2,1);
+    ts_groups[1].push_back(idx_pair(0,1));
+    ts_groups[1].push_back(idx_pair(2,1));
     //k loop
-    loops[2].set_subspace_looped(1,0);
-    loops[2].set_subspace_looped(2,0);
+    ts_groups[2].push_back(idx_pair(1,0));
+    ts_groups[2].push_back(idx_pair(2,0));
 
-    sparse_loop_list sll(loops,bispaces);
-
-    matmul_isomorphism_params<double> mip(sll);
+    matmul_isomorphism_params mip(bispaces,ts_groups);
 
     runtime_permutation perm_A = mip.get_A_perm();
     runtime_permutation perm_B = mip.get_B_perm();
@@ -154,25 +147,22 @@ void blas_isomorphism_test::test_matmul_isomorphism_params_identity_TT() throw(l
     sparse_bispace<1> spb_j(3);
     sparse_bispace<1> spb_k(4);
 
-    vector< sparse_bispace_any_order > bispaces(1,spb_i | spb_j);
+    vector<sparse_bispace_impl> bispaces(1,spb_i | spb_j);
     bispaces.push_back(spb_k|spb_i);
     bispaces.push_back(spb_j|spb_k);
 
-    vector<block_loop> loops(3,block_loop(bispaces));
+    vector<idx_pair_list> ts_groups(3);
     //i loop
-    loops[0].set_subspace_looped(0,0);
-    loops[0].set_subspace_looped(1,1);
+    ts_groups[0].push_back(idx_pair(0,0));
+    ts_groups[0].push_back(idx_pair(1,1));
     //j loop
-    loops[1].set_subspace_looped(0,1);
-    loops[1].set_subspace_looped(2,0);
+    ts_groups[1].push_back(idx_pair(0,1));
+    ts_groups[1].push_back(idx_pair(2,0));
     //k loop
-    loops[2].set_subspace_looped(1,0);
-    loops[2].set_subspace_looped(2,1);
+    ts_groups[2].push_back(idx_pair(1,0));
+    ts_groups[2].push_back(idx_pair(2,1));
 
-    sparse_loop_list sll(loops,bispaces);
-
-
-    matmul_isomorphism_params<double> mip(sll);
+    matmul_isomorphism_params mip(bispaces,ts_groups);
 
     runtime_permutation perm_A = mip.get_A_perm();
     runtime_permutation perm_B = mip.get_B_perm();
@@ -201,31 +191,25 @@ void blas_isomorphism_test::test_matmul_isomorphism_params_3d_2d_A_perm() throw(
     sparse_bispace<1> spb_k(4);
     sparse_bispace<1> spb_l(5);
 
-    vector< sparse_bispace_any_order > bispaces(1,spb_i|spb_j|spb_l);
+    vector<sparse_bispace_impl> bispaces(1,spb_i|spb_j|spb_l);
     bispaces.push_back(spb_i|spb_k|spb_j);
     bispaces.push_back(spb_k|spb_l);
 
-    vector<block_loop> loops(4,block_loop(bispaces));
+    vector<idx_pair_list> ts_groups(4);
     //i loop
-    loops[0].set_subspace_looped(0,0);
-    loops[0].set_subspace_looped(1,0);
-
+    ts_groups[0].push_back(idx_pair(0,0));
+    ts_groups[0].push_back(idx_pair(1,0));
     //j loop
-    loops[1].set_subspace_looped(0,1);
-    loops[1].set_subspace_looped(1,2);
-
+    ts_groups[1].push_back(idx_pair(0,1));
+    ts_groups[1].push_back(idx_pair(1,2));
     //l loop
-    loops[2].set_subspace_looped(0,2);
-    loops[2].set_subspace_looped(2,1);
-
+    ts_groups[2].push_back(idx_pair(0,2));
+    ts_groups[2].push_back(idx_pair(2,1));
     //k loop
-    loops[3].set_subspace_looped(1,1);
-    loops[3].set_subspace_looped(2,0);
+    ts_groups[3].push_back(idx_pair(1,1));
+    ts_groups[3].push_back(idx_pair(2,0));
 
-    sparse_loop_list sll(loops,bispaces);
-
-
-    matmul_isomorphism_params<double> mip(sll);
+    matmul_isomorphism_params mip(bispaces,ts_groups);
 
     runtime_permutation perm_A = mip.get_A_perm();
     runtime_permutation perm_B = mip.get_B_perm();
@@ -255,27 +239,25 @@ void blas_isomorphism_test::test_matmul_isomorphism_params_permuted_ioc() throw(
     sparse_bispace<1> spb_k(4);
     sparse_bispace<1> spb_l(5);
 
-    vector< sparse_bispace_any_order > bispaces(1,spb_i|spb_l);
+    vector<sparse_bispace_impl> bispaces(1,spb_i|spb_l);
     bispaces.push_back(spb_i|spb_j|spb_k);
     bispaces.push_back(spb_l|spb_k|spb_j);
 
-    vector<block_loop> loops(4,block_loop(bispaces));
+    vector<idx_pair_list> ts_groups(4);
     //i loop
-    loops[0].set_subspace_looped(0,0);
-    loops[0].set_subspace_looped(1,0);
+    ts_groups[0].push_back(idx_pair(0,0));
+    ts_groups[0].push_back(idx_pair(1,0));
     //l loop
-    loops[1].set_subspace_looped(0,1);
-    loops[1].set_subspace_looped(2,0);
+    ts_groups[1].push_back(idx_pair(0,1));
+    ts_groups[1].push_back(idx_pair(2,0));
     //j loop
-    loops[2].set_subspace_looped(1,1);
-    loops[2].set_subspace_looped(2,2);
+    ts_groups[2].push_back(idx_pair(1,1));
+    ts_groups[2].push_back(idx_pair(2,2));
     //k loop
-    loops[3].set_subspace_looped(1,2);
-    loops[3].set_subspace_looped(2,1);
+    ts_groups[3].push_back(idx_pair(1,2));
+    ts_groups[3].push_back(idx_pair(2,1));
 
-    sparse_loop_list sll(loops,bispaces);
-
-    matmul_isomorphism_params<double> mip(sll);
+    matmul_isomorphism_params mip(bispaces,ts_groups);
 
     runtime_permutation perm_A = mip.get_A_perm();
     runtime_permutation perm_B = mip.get_B_perm();
@@ -292,6 +274,7 @@ void blas_isomorphism_test::test_matmul_isomorphism_params_permuted_ioc() throw(
     }
 }
 
+#if 0
 //Requires A permutation 021, B permutation 201
 //Could also permute C additionally, but shouldn't, and therefore should reflect that
 //C_ijml = \sum_k A_ikj B_lkm
@@ -307,51 +290,44 @@ void blas_isomorphism_test::test_matmul_isomorphism_params_3d_3d_A_perm_B_perm()
     sparse_bispace<1> spb_l(5);
     sparse_bispace<1> spb_m(6);
 
-    vector< sparse_bispace_any_order > bispaces(1,spb_i|spb_j|spb_m|spb_l);
+    vector<sparse_bispace_impl> bispaces(1,spb_i|spb_j|spb_m|spb_l);
     bispaces.push_back(spb_i|spb_k|spb_j);
     bispaces.push_back(spb_l|spb_k|spb_m);
 
-    vector<block_loop> loops(5,block_loop(bispaces));
+    vector<idx_pair_list> ts_groups(5);
     //i loop
-    loops[0].set_subspace_looped(0,0);
-    loops[0].set_subspace_looped(1,0);
-
+    ts_groups[0].push_back(idx_pair(0,0));
+    ts_groups[0].push_back(idx_pair(1,0));
     //j loop
-    loops[1].set_subspace_looped(0,1);
-    loops[1].set_subspace_looped(1,2);
-
+    ts_groups[1].push_back(idx_pair(0,1));
+    ts_groups[1].push_back(idx_pair(1,2));
     //l loop
-    loops[2].set_subspace_looped(0,3);
-    loops[2].set_subspace_looped(2,0);
-
+    ts_groups[2].push_back(idx_pair(0,3));
+    ts_groups[2].push_back(idx_pair(2,0));
     //m loop
-    loops[3].set_subspace_looped(0,2);
-    loops[3].set_subspace_looped(2,2);
-
+    ts_groups[3].push_back(idx_pair(0,2));
+    ts_groups[3].push_back(idx_pair(2,2));
     //k loop
-    loops[4].set_subspace_looped(1,1);
-    loops[4].set_subspace_looped(2,1);
+    ts_groups[4].push_back(idx_pair(1,1));
+    ts_groups[4].push_back(idx_pair(2,1));
 
-
-    sparse_loop_list sll(loops,bispaces);
-
-
-    matmul_isomorphism_params<double> mip(sll);
+    matmul_isomorphism_params mip(bispaces,ts_groups);
 
     runtime_permutation perm_A = mip.get_A_perm();
     runtime_permutation perm_B = mip.get_B_perm();
-    matmul_isomorphism_params<double>::dgemm_fp_t dgemm_fp = mip.get_dgemm_fp();
-
+    bool A_trans = mip.get_A_trans();
+    bool B_trans = mip.get_B_trans();
 
     runtime_permutation correct_A_perm(3);
     correct_A_perm.permute(1,2);
-    runtime_permutation correct_B_perm(2);
+    runtime_permutation correct_B_perm(3);
     correct_B_perm.permute(0,2);
     correct_B_perm.permute(1,2);
-    if(perm_A !=  correct_A_perm || perm_B != correct_B_perm || dgemm_fp != &linalg::mul2_ij_ip_jp_x)
+
+    if(perm_A !=  correct_A_perm || perm_B != correct_B_perm || A_trans != false || B_trans != true)
     {
         fail_test(test_name,__FILE__,__LINE__,
-                "matmul_isomorphism_params returned incorrect value for 3d_2d_A_perm case");
+                "matmul_isomorphism_params returned incorrect value for 3d_3d_A_perm_B_perm case");
     }
 }
 #endif
