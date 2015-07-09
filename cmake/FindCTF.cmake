9aeaf43200e54657f38773067417d457944d1196
Evgeny Epifanovsky <epif@q-chem.com>
2015-07-09 18:36:57 +0000

    print ctf version in FindCTF.cmake

--- cmake/FindCTF.cmake
+++ cmake/FindCTF.cmake
@@ -27,6 +27,17 @@ if(CTF_INCLUDE AND CTF_LIB)
 
     if(CTF_HPP AND CTF_A)
 
+        file(READ "${CTF_HPP}" CTF_HPP_CONTENTS)
+        string(REGEX REPLACE ".*define CTF_VERSION ([0-9]+).*" "\\1"
+            CTF_VERSION "${CTF_HPP_CONTENTS}")
+        if(CTF_VERSION)
+            math(EXPR CTF_VERSION_MAJOR "${CTF_VERSION} / 100")
+            math(EXPR CTF_VERSION_MINOR "(${CTF_VERSION} % 100) / 10")
+            math(EXPR CTF_VERSION_PATCH "${CTF_VERSION} % 10")
+            set(CTF_VERSION
+                "${CTF_VERSION_MAJOR}.${CTF_VERSION_MINOR}${CTF_VERSION_PATCH}")
+        endif(CTF_VERSION)
+        unset(CTF_HPP_CONTENTS)
         set(CTF_INCLUDE_PATH "${CTF_INCLUDE}")
         set(CTF_LIBRARY_PATH "${CTF_LIB}")
         add_library(ctf UNKNOWN IMPORTED)
@@ -34,7 +45,7 @@ if(CTF_INCLUDE AND CTF_LIB)
         set(CTF_LIBRARIES ctf)
         set(CTF_FOUND TRUE)
         if(NOT CTF_FIND_QUIETLY)
-            message(STATUS "Found CTF: ${CTF_A}")
+            message(STATUS "Found CTF ${CTF_VERSION}: ${CTF_A}")
         endif(NOT CTF_FIND_QUIETLY)
 
     endif(CTF_HPP AND CTF_A)
