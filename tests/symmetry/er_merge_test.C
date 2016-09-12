04bf8500ee5812a63b46c04d18a62177aeb3e8b7
Evgeny Epifanovsky <epif@q-chem.com>
2016-09-12 07:06:35 -0700

    fix some symmetry tests

--- tests/symmetry/er_merge_test.C
+++ tests/symmetry/er_merge_test.C
@@ -2,51 +2,17 @@
 #include <libtensor/symmetry/inst/er_optimize.h>
 #include <libtensor/symmetry/point_group_table.h>
 #include <libtensor/symmetry/print_symmetry.h>
-#include "er_merge_test.h"
+#include "se_label_test_base.h"
+#include "../test_utils.h"
 
-namespace libtensor {
-
-
-void er_merge_test::perform() throw(libtest::test_exception) {
-
-    std::string s6 = "S6", c2v = "C2v";
-    setup_pg_table(c2v);
-
-    try {
-
-        test_1(c2v);
-        test_4(c2v);
-
-    } catch (libtest::test_exception &e) {
-        clear_pg_table(c2v);
-        throw;
-    }
-
-    clear_pg_table(c2v);
-
-    setup_pg_table(s6);
-
-    try {
-
-        test_2(s6);
-        test_3(s6);
-
-    } catch (libtest::test_exception &e) {
-        clear_pg_table(s6);
-        throw;
-    }
-
-    clear_pg_table(s6);
-
-}
+using namespace libtensor;
 
 
 /** \brief Merge 4 dimensions in 2 steps (merge dims can be simplified)
  **/
-void er_merge_test::test_1(
-        const std::string &id) throw(libtest::test_exception) {
+int test_1(const std::string &id) {
 
-    static const char *testname = "er_merge_test::test_1()";
+    static const char testname[] = "er_merge_test::test_1()";
 
     typedef product_table_i::label_set_t label_set_t;
     typedef product_table_i::label_group_t label_group_t;
@@ -76,46 +42,47 @@ void er_merge_test::test_1(
     // Check sequence list
     const eval_sequence_list<2> &sl = r2.get_sequences();
     if (sl.size() != 1) {
-        fail_test(testname, __FILE__, __LINE__, "# seq.");
+        return fail_test(testname, __FILE__, __LINE__, "# seq.");
     }
     if (sl[0][0] != 1 || sl[0][1] != 1) {
-        fail_test(testname, __FILE__, __LINE__, "seq.");
+        return fail_test(testname, __FILE__, __LINE__, "seq.");
     }
 
     // Check product list
     evaluation_rule<2>::iterator it = r2.begin();
     if (it == r2.end()) {
-        fail_test(testname, __FILE__, __LINE__, "Empty product list.");
+        return fail_test(testname, __FILE__, __LINE__, "Empty product list.");
     }
     const product_rule<2> &pr1 = r2.get_product(it);
     it++;
     if (it != r2.end()) {
-        fail_test(testname, __FILE__, __LINE__, "More than one products.");
+        return fail_test(testname, __FILE__, __LINE__, "More than one products.");
     }
 
     product_rule<2>::iterator ip1 = pr1.begin();
     if (ip1 == pr1.end()) {
-        fail_test(testname, __FILE__, __LINE__, "Empty product pr1.");
+        return fail_test(testname, __FILE__, __LINE__, "Empty product pr1.");
     }
     if (pr1.get_intrinsic(ip1) != product_table_i::k_invalid)
-        fail_test(testname, __FILE__, __LINE__, "Intrinsic label.");
+        return fail_test(testname, __FILE__, __LINE__, "Intrinsic label.");
     ip1++;
     if (ip1 != pr1.end()) {
-        fail_test(testname, __FILE__, __LINE__, "Too many terms in pr1.");
+        return fail_test(testname, __FILE__, __LINE__, "Too many terms in pr1.");
     }
 
     } catch(exception &e) {
-        fail_test(testname, __FILE__, __LINE__, e.what());
+        return fail_test(testname, __FILE__, __LINE__, e.what());
     }
+
+    return 0;
 }
 
 
 /** \brief Merge 4 dimensions in 2 steps (merge dims can be simplified)
  **/
-void er_merge_test::test_2(
-        const std::string &id) throw(libtest::test_exception) {
+int test_2(const std::string &id) {
 
-    static const char *testname = "er_merge_test::test_2()";
+    static const char testname[] = "er_merge_test::test_2()";
 
     typedef product_table_i::label_set_t label_set_t;
     typedef product_table_i::label_group_t label_group_t;
@@ -143,53 +110,53 @@ void er_merge_test::test_2(
     // Check sequence list
     const eval_sequence_list<2> &sl = r2.get_sequences();
     if (sl.size() != 1) {
-        fail_test(testname, __FILE__, __LINE__, "# seq.");
+        return fail_test(testname, __FILE__, __LINE__, "# seq.");
     }
     if (sl[0][0] != 1 || sl[0][1] != 1) {
-        fail_test(testname, __FILE__, __LINE__, "seq.");
+        return fail_test(testname, __FILE__, __LINE__, "seq.");
     }
 
     // Check product list
     evaluation_rule<2>::iterator it = r2.begin();
     if (it == r2.end()) {
-        fail_test(testname, __FILE__, __LINE__, "Empty product list.");
+        return fail_test(testname, __FILE__, __LINE__, "Empty product list.");
     }
     const product_rule<2> &pr1 = r2.get_product(it);
     it++;
     if (it != r2.end()) {
-        fail_test(testname, __FILE__, __LINE__, "More than one products.");
+        return fail_test(testname, __FILE__, __LINE__, "More than one products.");
     }
 
     product_rule<2>::iterator ip1 = pr1.begin(), ip2 = pr1.begin();
     if (ip1 == pr1.end()) {
-        fail_test(testname, __FILE__, __LINE__, "Empty product pr1.");
+        return fail_test(testname, __FILE__, __LINE__, "Empty product pr1.");
     }
     ip2++;
     if (ip2 == pr1.end()) {
-        fail_test(testname, __FILE__, __LINE__, "# terms in pr1.");
+        return fail_test(testname, __FILE__, __LINE__, "# terms in pr1.");
     }
     if ((pr1.get_intrinsic(ip1) != 1 || pr1.get_intrinsic(ip2) != 2) &&
             (pr1.get_intrinsic(ip1) != 2 || pr1.get_intrinsic(ip2) != 1)) {
-        fail_test(testname, __FILE__, __LINE__, "Intrinsic label.");
+        return fail_test(testname, __FILE__, __LINE__, "Intrinsic label.");
     }
     ip2++;
     if (ip2 != pr1.end()) {
-        fail_test(testname, __FILE__, __LINE__, "Too many terms in pr1.");
+        return fail_test(testname, __FILE__, __LINE__, "Too many terms in pr1.");
     }
 
     } catch(exception &e) {
-        fail_test(testname, __FILE__, __LINE__, e.what());
+        return fail_test(testname, __FILE__, __LINE__, e.what());
     }
+
+    return 0;
 }
 
 
 /** \brief Merge 4 dimensions in 2 steps (1 merge cannot be simplified)
  **/
-void er_merge_test::test_3(
-        const std::string &id) throw(libtest::test_exception) {
+int test_3(const std::string &id) {
 
-
-    static const char *testname = "er_merge_test::test_3()";
+    static const char testname[] = "er_merge_test::test_3()";
 
     typedef product_table_i::label_set_t label_set_t;
     typedef product_table_i::label_group_t label_group_t;
@@ -221,70 +188,71 @@ void er_merge_test::test_3(
     // Check sequence list
     const eval_sequence_list<3> &sl = r2.get_sequences();
     if (sl.size() != 3) {
-        fail_test(testname, __FILE__, __LINE__, "# seq.");
+        return fail_test(testname, __FILE__, __LINE__, "# seq.");
     }
     if (sl[0][0] != 0 || sl[0][1] != 1 || sl[0][2] != 0) {
-        fail_test(testname, __FILE__, __LINE__, "seq[0].");
+        return fail_test(testname, __FILE__, __LINE__, "seq[0].");
     }
     if (sl[1][0] != 0 || sl[1][1] != 0 || sl[1][2] != 2) {
-        fail_test(testname, __FILE__, __LINE__, "seq[1].");
+        return fail_test(testname, __FILE__, __LINE__, "seq[1].");
     }
     if (sl[2][0] != 1 || sl[2][1] != 0 || sl[2][2] != 1) {
-        fail_test(testname, __FILE__, __LINE__, "seq[2].");
+        return fail_test(testname, __FILE__, __LINE__, "seq[2].");
     }
 
     // Check product list
     evaluation_rule<3>::iterator it = r2.begin();
     if (it == r2.end()) {
-        fail_test(testname, __FILE__, __LINE__, "Empty product list.");
+        return fail_test(testname, __FILE__, __LINE__, "Empty product list.");
     }
     const product_rule<3> &pr1 = r2.get_product(it);
     it++;
     if (it == r2.end()) {
-        fail_test(testname, __FILE__, __LINE__, "Only one product.");
+        return fail_test(testname, __FILE__, __LINE__, "Only one product.");
     }
     const product_rule<3> &pr2 = r2.get_product(it);
     it++;
     if (it != r2.end()) {
-        fail_test(testname, __FILE__, __LINE__, "More than one products.");
+        return fail_test(testname, __FILE__, __LINE__, "More than one products.");
     }
 
     product_rule<3>::iterator ip = pr1.begin();
     if (ip == pr1.end()) {
-        fail_test(testname, __FILE__, __LINE__, "Empty product pr1.");
+        return fail_test(testname, __FILE__, __LINE__, "Empty product pr1.");
     }
     if (pr1.get_intrinsic(ip) != 1)
-        fail_test(testname, __FILE__, __LINE__, "Intrinsic label.");
+        return fail_test(testname, __FILE__, __LINE__, "Intrinsic label.");
     ip++;
     if (ip != pr1.end()) {
-        fail_test(testname, __FILE__, __LINE__, "Too many terms in pr1.");
+        return fail_test(testname, __FILE__, __LINE__, "Too many terms in pr1.");
     }
 
     ip = pr2.begin();
     if (ip == pr2.end()) {
-        fail_test(testname, __FILE__, __LINE__, "Empty product pr2.");
+        return fail_test(testname, __FILE__, __LINE__, "Empty product pr2.");
     }
     if (pr2.get_intrinsic(ip) != 2)
-        fail_test(testname, __FILE__, __LINE__, "Intrinsic label.");
+        return fail_test(testname, __FILE__, __LINE__, "Intrinsic label.");
     ip++;
     if (pr2.get_intrinsic(ip) != 3)
-        fail_test(testname, __FILE__, __LINE__, "Intrinsic label.");
+        return fail_test(testname, __FILE__, __LINE__, "Intrinsic label.");
     ip++;
     if (ip != pr2.end()) {
-        fail_test(testname, __FILE__, __LINE__, "Too many terms in pr2.");
+        return fail_test(testname, __FILE__, __LINE__, "Too many terms in pr2.");
     }
 
 
     } catch(exception &e) {
-        fail_test(testname, __FILE__, __LINE__, e.what());
+        return fail_test(testname, __FILE__, __LINE__, e.what());
     }
+
+    return 0;
 }
 
 
-void er_merge_test::test_4(
-        const std::string &id) throw(libtest::test_exception) {
+int test_4(const std::string &id) {
 
-    static const char *testname = "er_merge_test::test_4()";
+    static const char testname[] = "er_merge_test::test_4()";
 
     typedef product_table_i::label_set_t label_set_t;
     typedef product_table_i::label_group_t label_group_t;
@@ -314,38 +282,64 @@ void er_merge_test::test_4(
     // Check sequence list
     const eval_sequence_list<3> &sl = r2.get_sequences();
     if (sl.size() != 1) {
-        fail_test(testname, __FILE__, __LINE__, "# seq.");
+        return fail_test(testname, __FILE__, __LINE__, "# seq.");
     }
     if (sl[0][0] != 1 || sl[0][2] != 1) {
-        fail_test(testname, __FILE__, __LINE__, "seq.");
+        return fail_test(testname, __FILE__, __LINE__, "seq.");
     }
 
     // Check product list
     evaluation_rule<3>::iterator it = r2.begin();
     if (it == r2.end()) {
-        fail_test(testname, __FILE__, __LINE__, "Empty product list.");
+        return fail_test(testname, __FILE__, __LINE__, "Empty product list.");
     }
     const product_rule<3> &pr1 = r2.get_product(it);
     it++;
     if (it != r2.end()) {
-        fail_test(testname, __FILE__, __LINE__, "More than one product.");
+        return fail_test(testname, __FILE__, __LINE__, "More than one product.");
     }
 
     product_rule<3>::iterator ip1 = pr1.begin();
     if (ip1 == pr1.end()) {
-        fail_test(testname, __FILE__, __LINE__, "Empty product pr1.");
+        return fail_test(testname, __FILE__, __LINE__, "Empty product pr1.");
     }
     if (pr1.get_intrinsic(ip1) != product_table_i::k_identity)
-        fail_test(testname, __FILE__, __LINE__, "Intrinsic label.");
+        return fail_test(testname, __FILE__, __LINE__, "Intrinsic label.");
     ip1++;
     if (ip1 != pr1.end()) {
-        fail_test(testname, __FILE__, __LINE__, "Too many terms in pr1.");
+        return fail_test(testname, __FILE__, __LINE__, "Too many terms in pr1.");
     }
 
     } catch(exception &e) {
-        fail_test(testname, __FILE__, __LINE__, e.what());
+        return fail_test(testname, __FILE__, __LINE__, e.what());
     }
+
+    return 0;
 }
 
 
-} // namespace libtensor
+int main() {
+
+    std::string s6 = "S6", c2v = "C2v";
+    setup_pg_table(c2v);
+
+    int rc =
+        test_1(c2v) ||
+        test_4(c2v) ||
+        0;
+
+    clear_pg_table(c2v);
+
+    setup_pg_table(s6);
+
+    rc =
+        rc ||
+        test_2(s6) ||
+        test_3(s6) ||
+        0;
+
+    clear_pg_table(s6);
+
+    return rc;
+}
+
