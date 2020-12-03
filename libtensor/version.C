e1ccf83acc6ebfa5319b02d8192c1e82ce089273
Michael F. Herbst <info@michael-herbst.com>
2020-12-03 21:51:26 +0100

    Bump version

--- libtensor/version.C
+++ libtensor/version.C
@@ -1,29 +1,24 @@
-#include <sstream>
 #include "version.h"
+#include <sstream>
 
 namespace libtensor {
 
-const char version::k_status[] = "trunk";
-const char *version::k_authors[] = {
-    "Evgeny Epifanovsky",
-    "Michael Wormit",
-    "Dmitry Zuev",
-    "Sam Manzer",
-    "Ilya Kaliman"
+const char version::k_status[]   = "trunk";
+const char* version::k_authors[] = {
+      "Evgeny Epifanovsky", "Michael Wormit",      "Dmitry Zuev",       "Sam Manzer",
+      "Ilya Kaliman",       "Maximilian Scheurer", "Michael F. Herbst",
 };
-
 
 version::version() : m_status(k_status) {
 
-    std::ostringstream ss;
-    ss << k_major << "." << k_minor << "-" << m_status;
-    m_string = ss.str();
+  std::ostringstream ss;
+  ss << k_major << "." << k_minor << "-" << m_status;
+  m_string = ss.str();
 
-    size_t nauthors = sizeof(k_authors)/sizeof(char*);
-    for(size_t i = 0; i < nauthors; i++) {
-        m_authors.push_back(std::string(k_authors[i]));
-    }
+  size_t nauthors = sizeof(k_authors) / sizeof(char*);
+  for (size_t i = 0; i < nauthors; i++) {
+    m_authors.push_back(std::string(k_authors[i]));
+  }
 }
 
-
-}
+}  // namespace libtensor
--- libtensor/version.h
+++ libtensor/version.h
@@ -1,12 +1,11 @@
 #ifndef LIBTENSOR_VERSION_H
 #define LIBTENSOR_VERSION_H
 
+#include <libutil/singleton.h>
 #include <list>
 #include <string>
-#include <libutil/singleton.h>
 
 namespace libtensor {
-
 
 /** \brief Version of the %tensor library
 
@@ -20,68 +19,56 @@ namespace libtensor {
     \ingroup libtensor
  **/
 class version : public libutil::singleton<version> {
-    friend class libutil::singleton<version>;
+  friend class libutil::singleton<version>;
 
-private:
-    static const unsigned k_major = 2; //!< Major %version number
-    static const unsigned k_minor = 5; //!< Minor %version number
-    static const char k_status[]; //!< Version status
-    static const char *k_authors[]; //!< List of authors
+ private:
+  static const unsigned k_major = 3;  //!< Major %version number
+  static const unsigned k_minor = 0;  //!< Minor %version number
+  static const char k_status[];       //!< Version status
+  static const char* k_authors[];     //!< List of authors
 
-private:
-    std::string m_status; //!< Version status
-    std::string m_string; //!< Version string
-    std::list<std::string> m_authors; //!< List of authors
+ private:
+  std::string m_status;              //!< Version status
+  std::string m_string;              //!< Version string
+  std::list<std::string> m_authors;  //!< List of authors
 
-protected:
-    version();
+ protected:
+  version();
 
-    const std::string &get_status_impl() const {
-        return m_status;
-    }
+  const std::string& get_status_impl() const { return m_status; }
 
-    const std::string &get_string_impl() const {
-        return m_string;
-    }
+  const std::string& get_string_impl() const { return m_string; }
 
-    const std::list<std::string> &get_authors_impl() const {
-        return m_authors;
-    }
+  const std::list<std::string>& get_authors_impl() const { return m_authors; }
 
-public:
-    /**    \brief Returns the major %version number
-     **/
-    static unsigned get_major() {
-        return k_major;
-    }
+ public:
+  /**    \brief Returns the major %version number
+   **/
+  static unsigned get_major() { return k_major; }
 
-    /**    \brief Returns the minor %version number
-     **/
-    static unsigned get_minor() {
-        return k_minor;
-    }
+  /**    \brief Returns the minor %version number
+   **/
+  static unsigned get_minor() { return k_minor; }
 
-    /**    \brief Returns the %version status
-     **/
-    static const std::string &get_status() {
-        return version::get_instance().get_status_impl();
-    }
+  /**    \brief Returns the %version status
+   **/
+  static const std::string& get_status() {
+    return version::get_instance().get_status_impl();
+  }
 
-    /**    \brief Returns the string that corresponds to the %version
-     **/
-    static const std::string &get_string() {
-        return version::get_instance().get_string_impl();
-    }
+  /**    \brief Returns the string that corresponds to the %version
+   **/
+  static const std::string& get_string() {
+    return version::get_instance().get_string_impl();
+  }
 
-    /**    \brief Returns the list of authors
-     **/
-    static const std::list<std::string> &get_authors() {
-        return version::get_instance().get_authors_impl();
-    }
-
+  /**    \brief Returns the list of authors
+   **/
+  static const std::list<std::string>& get_authors() {
+    return version::get_instance().get_authors_impl();
+  }
 };
 
+}  // namespace libtensor
 
-} // namespace libtensor
-
-#endif // LIBTENSOR_VERSION_H
+#endif  // LIBTENSOR_VERSION_H
